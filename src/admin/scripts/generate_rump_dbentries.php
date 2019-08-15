753fbda6d0714a5058c1784b726afeee31b60ae0
Daniel Jakob <wolverine@usox.org>
2019-08-15 12:50:10 +0200

    Update shiprump database entry creator
    Fixes #14

--- src/admin/scripts/generate_rump_dbentries.php
+++ src/admin/scripts/generate_rump_dbentries.php
@@ -1,30 +1,25 @@
 <?php
 
 use Stu\Orm\Repository\DatabaseEntryRepositoryInterface;
+use Stu\Orm\Repository\DatabaseTypeRepositoryInterface;
 
 include_once(__DIR__.'/../../inc/config.inc.php');
 
-// @todo rebuild
-
-$db_category = '00'.DATABASE_TYPE_SHIPRUMP;
-
 $repository = $container->get(DatabaseEntryRepositoryInterface::class);
+$type = $container->get(DatabaseTypeRepositoryInterface::class)->find(DATABASE_TYPE_SHIPRUMP);
 
-//DatabaseEntry::truncate('WHERE category_id='.$db_category);
-
-$result = Shiprump::getBy('WHERE is_buildable=1');
+$result = Shiprump::getBy('WHERE is_buildable=1 AND id NOT IN (select object_id from stu_database_entrys where type='.DATABASE_TYPE_SHIPRUMP.')');
 foreach ($result as $key => $obj) {
     $db = $repository->prototype();
-	$db->setCategoryId($db_category);
+	$db->setCategoryId(DATABASE_TYPE_SHIPRUMP);
 	$db->setDescription($obj->getName());
 	$db->setData('');
 	$db->setSort($obj->getSort());
 	$db->setObjectId($obj->getId());
-	$db->setType(DATABASE_TYPE_RUMP);
+	$db->setTypeObject($type);
 
 	$repository->save($db);
 
-	//$db->forceId($obj->getId().$db_category);
 	$obj->setDatabaseId($db->getId());
 	$obj->save();
 }
