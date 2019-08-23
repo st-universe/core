09c8f529cddd7dbc2c9a7498760f4080f9888e64
Daniel Jakob <wolverine@usox.org>
2019-08-23 10:39:12 +0200

    add script to generate tradenpost db entries

---
+++ src/admin/scripts/generate_tradepost_dbentries.php
@@ -0,0 +1,27 @@
+<?php
+
+use Stu\Orm\Repository\DatabaseCategoryRepositoryInterface;
+use Stu\Orm\Repository\DatabaseEntryRepositoryInterface;
+use Stu\Orm\Repository\DatabaseTypeRepositoryInterface;
+
+include_once(__DIR__.'/../../inc/config.inc.php');
+
+$repository = $container->get(DatabaseEntryRepositoryInterface::class);
+$type = $container->get(DatabaseTypeRepositoryInterface::class)->find(DATABASE_TYPE_TRADEPOSTS);
+$category = $container->get(DatabaseCategoryRepositoryInterface::class)->find(DATABASE_CATEGORY_TRADEPOST);
+
+$result = Ship::getObjectsBy('WHERE database_id = 0 and trade_post_id > 0');
+foreach ($result as $key => $obj) {
+    $db = $repository->prototype();
+	$db->setCategory($category);
+	$db->setDescription($obj->getName());
+	$db->setData('');
+    $db->setTypeObject($type);
+	$db->setSort($obj->getId());
+	$db->setObjectId($obj->getId());
+
+	$repository->save($db);
+
+	$obj->setDatabaseId($db->getId());
+	$obj->save();
+}
