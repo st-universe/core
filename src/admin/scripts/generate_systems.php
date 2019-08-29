be198dde3af3fbb15b70094422535a56d60a09a8
Daniel Jakob <wolverine@usox.org>
2019-08-29 21:55:39 +0200

    Extract the starsystem generator into an own package

--- src/admin/scripts/generate_systems.php
+++ src/admin/scripts/generate_systems.php
@@ -1,12 +1,16 @@
 <?php
 
+use Stu\StarsystemGenerator\StarsystemGenerator;
+
 include_once(__DIR__.'/../../inc/config.inc.php');
 
 DB()->beginTransaction();
 
+$starSystemGenerator = new StarsystemGenerator();
+
 $result = MapField::getListBy('WHERE field_id=99');
 foreach ($result as $key => $obj) {
-	$system = generate(getSystemType());
+	$system = $starSystemGenerator->generate(901);
 	$system->setCX($obj->getCX());
 	$system->setCY($obj->getCY());
 	$system->save();
--- src/inc/sysgen/zonePlanets.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	if ($zones[$i][$j] == 1) $t = pdraw(array(111 => 45,115 => 45,105 => 10));
-	if ($zones[$i][$j] == 2) $t = pdraw(array(101 => 40,103 => 35,105 => 25));
-	if ($zones[$i][$j] == 3) $t = pdraw(array(113 => 60,107 => 40));
-	if ($zones[$i][$j] == 4) $t = 113;
-		
-?>
