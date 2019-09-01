5716354ae45e9643e8863b4673ad4d40832c0b2a
Daniel Jakob <wolverine@usox.org>
2019-09-01 10:27:20 +0200

    Repair duplicate variable name

--- src/Module/Ship/Action/ActivateTractorBeam/ActivateTractorBeam.php
+++ src/Module/Ship/Action/ActivateTractorBeam/ActivateTractorBeam.php
@@ -53,55 +53,55 @@ final class ActivateTractorBeam implements ActionControllerInterface
             $game->addInformation($wrapper->getError());
             return;
         }
-        $ship = $this->shipLoader->getById(request::getIntFatal('target'));
-        if ($ship->getRump()->isTrumfield()) {
+        $target = $this->shipLoader->getById(request::getIntFatal('target'));
+        if ($target->getRump()->isTrumfield()) {
             $game->addInformation("Das Trümmerfeld kann nicht erfasst werden");
             return;
         }
-        if (!checkPosition($ship, $ship)) {
+        if (!checkPosition($ship, $target)) {
             return;
         }
-        if ($ship->isBase()) {
-            $game->addInformation("Die " . $ship->getName() . " kann nicht erfasst werden");
+        if ($target->isBase()) {
+            $game->addInformation("Die " . $target->getName() . " kann nicht erfasst werden");
             return;
         }
-        if ($ship->traktorbeamToShip()) {
-            $game->addInformation("Das Schiff wird bereits vom Traktorstrahl der " . $ship->getTraktorShip()->getName() . " gehalten");
+        if ($target->traktorbeamToShip()) {
+            $game->addInformation("Das Schiff wird bereits vom Traktorstrahl der " . $target->getTraktorShip()->getName() . " gehalten");
             return;
         }
-        if ($ship->isInFleet() && $ship->getFleetId() == $ship->getFleetId()) {
-            $game->addInformation("Die " . $ship->getName() . " befindet sich in der selben Flotte wie die " . $ship->getName());
+        if ($target->isInFleet() && $target->getFleetId() == $ship->getFleetId()) {
+            $game->addInformation("Die " . $target->getName() . " befindet sich in der selben Flotte wie die " . $ship->getName());
             return;
         }
-        if (($ship->getAlertState() == ALERT_YELLOW || $ship->getAlertState() == ALERT_RED) && !$ship->getUser()->isFriend($userId)) {
-            if ($ship->isInFleet()) {
-                $attacker = $ship->getFleet()->getShips();
+        if (($target->getAlertState() == ALERT_YELLOW || $target->getAlertState() == ALERT_RED) && !$target->getUser()->isFriend($userId)) {
+            if ($target->isInFleet()) {
+                $attacker = $target->getFleet()->getShips();
             } else {
-                $attacker = &$ship;
+                $attacker = $target;
             }
-            $obj = new ShipSingleAttackCycle($attacker, $ship, $ship->getFleetId(),$ship->getFleetId());
+            $obj = new ShipSingleAttackCycle($attacker, $ship, $target->getFleetId(),$ship->getFleetId());
             $game->addInformationMergeDown($obj->getMessages());
-            PM::sendPM($userId, $ship->getUserId(),
-                "Die " . $ship->getName() . " versucht die " . $ship->getName() . " in Sektor " . $ship->getSectorString() . " mit dem Traktorstrahl zu erfassen. Folgende Aktionen wurden ausgeführt:\n" . infoToString($obj->getMessages()),
+            PM::sendPM($userId, $target->getUserId(),
+                "Die " . $ship->getName() . " versucht die " . $target->getName() . " in Sektor " . $ship->getSectorString() . " mit dem Traktorstrahl zu erfassen. Folgende Aktionen wurden ausgeführt:\n" . infoToString($obj->getMessages()),
                 PM_SPECIAL_SHIP);
         }
-        if ($ship->shieldIsActive()) {
-            $game->addInformation("Die " . $ship->getName() . " kann aufgrund der aktiven Schilde nicht erfasst werden");
+        if ($target->shieldIsActive()) {
+            $game->addInformation("Die " . $target->getName() . " kann aufgrund der aktiven Schilde nicht erfasst werden");
             return;
         }
-        $ship->deactivateTraktorBeam();
+        $target->deactivateTraktorBeam();
         $ship->setTraktorMode(1);
-        $ship->setTraktorShipId($ship->getId());
-        $ship->setTraktorMode(2);
-        $ship->setTraktorShipId($ship->getId());
+        $ship->setTraktorShipId($target->getId());
+        $target->setTraktorMode(2);
+        $target->setTraktorShipId($ship->getId());
+        $target->save();
         $ship->save();
-        $ship->save();
-        if ($userId != $ship->getUserId()) {
-            PM::sendPM($userId, $ship->getUserId(),
-                "Die " . $ship->getName() . " wurde in SeKtor " . $ship->getSectorString() . " vom Traktorstrahl der " . $ship->getName() . " erfasst",
+        if ($userId != $target->getUserId()) {
+            PM::sendPM($userId, $target->getUserId(),
+                "Die " . $target->getName() . " wurde in SeKtor " . $ship->getSectorString() . " vom Traktorstrahl der " . $ship->getName() . " erfasst",
                 PM_SPECIAL_SHIP);
         }
-        $game->addInformation("Der Traktorstrahl wurde auf die " . $ship->getName() . " gerichtet");
+        $game->addInformation("Der Traktorstrahl wurde auf die " . $target->getName() . " gerichtet");
     }
 
     public function performSessionCheck(): bool
