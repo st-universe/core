67cb3854acc8aa94c1f73e5ab88f996a11554821
Daniel Jakob <wolverine@usox.org>
2019-09-15 10:38:34 +0200

    Add middleware for jwt authentication

--- src/Module/Api/Middleware/Action.php
+++ src/Module/Api/Middleware/Action.php
@@ -130,8 +130,10 @@ abstract class Action
             ->withHeader('Content-Type', 'application/json')
             ->withHeader('Access-Control-Allow-Credentials', 'true')
             ->withHeader('Access-Control-Allow-Origin', '*')
-            ->withHeader('Access-Control-Allow-Headers',
-                'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader(
+                'Access-Control-Allow-Headers',
+                'X-Requested-With, Content-Type, Accept, Origin, Authorization'
+            )
             ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
             ->withAddedHeader('Cache-Control', 'post-check=0, pre-check=0')
--- src/api.php
+++ src/api.php
@@ -2,6 +2,8 @@
 
 declare(strict_types=1);
 
+use Noodlehaus\ConfigInterface;
+use Psr\Http\Message\ResponseInterface;
 use Slim\Factory\AppFactory;
 use Slim\Routing\RouteCollectorProxy;
 use Stu\Module\Api\V1\Common\Login\Login;
@@ -12,6 +14,22 @@ require_once __DIR__.'/inc/config.inc.php';
 AppFactory::setContainer($container);
 
 $app = AppFactory::create();
+
+$app->add(new Tuupola\Middleware\JwtAuthentication([
+    'secret' => $container->get(ConfigInterface::class)->get('api.jwt_secret'),
+    'secure' => true,
+    'relaxed' => ['localhost'],
+    'ignore' => [
+        '/api/v1/common'
+    ],
+    'error' => function (ResponseInterface $response, array $arguments): void {
+        $data['status'] = 'error';
+        $data['message'] = $arguments['message'];
+        $response->withHeader('Content-Type', 'application/json')
+            ->getBody()
+            ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    }
+]));
 
 $app->group('/api/v1', function (RouteCollectorProxy $group): void {
     $group->get('/common/news', GetNews::class);
