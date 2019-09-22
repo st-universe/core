7d3476d0838ba988a54a221d53fd0993151cb394
Daniel Jakob <wolverine@usox.org>
2019-09-22 11:56:08 +0200

    Wrap the maintenance script in a transaction

--- src/admin/cron/maintenance.php
+++ src/admin/cron/maintenance.php
@@ -1,12 +1,19 @@
 <?php
 
+use Doctrine\ORM\EntityManagerInterface;
 use Stu\Module\Tick\Maintenance\Maintenance;
 use Stu\Orm\Repository\GameConfigRepositoryInterface;
 
 require_once __DIR__.'/../../inc/config.inc.php';
+
+$entityManager = $container->get(EntityManagerInterface::class);
+
+$entityManager->beginTransaction();
 
 $maintenance = new Maintenance(
     $container->get(GameConfigRepositoryInterface::class),
     $container->get('maintenance_handler')
 );
 $maintenance->handle();
+
+$entityManager->commit();
