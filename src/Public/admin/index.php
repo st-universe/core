a2c0d1f4c47a079fb62a4926f78d2acb42262c0c
Daniel Jakob <wolverine@usox.org>
2019-12-18 20:37:40 +0100

    Remove old static files and move admin script

--- src/Public/admin/index.php
+++ src/Public/admin/index.php
@@ -7,7 +7,7 @@ use Stu\Module\Control\GameControllerInterface;
 
 @session_start();
 
-require_once __DIR__ . '/../Config/Bootstrap.php';
+require_once __DIR__ . '/../../Config/Bootstrap.php';
 
 $em = $container->get(EntityManagerInterface::class);
 $em->beginTransaction();
