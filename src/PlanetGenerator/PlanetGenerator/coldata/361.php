125e239c480106746dc884329ffa3222b36e8d24
Huxinator <nicotintel@gmail.com>
2020-03-28 19:21:29 +0100

    add planetgenerator to repo

---
+++ src/PlanetGenerator/PlanetGenerator/coldata/361.php
@@ -0,0 +1,43 @@
+<?php
+
+use Stu\PlanetGenerator\PlanetGenerator;
+
+$data[PlanetGenerator::COLGEN_DETAILS] = "Klasse I/J";
+
+	$data[PlanetGenerator::COLGEN_SIZEW] = 10;
+	$data[PlanetGenerator::COLGEN_SIZEH] = 6;
+
+	$hasground = 0;
+	$bonusfields = 0;
+	
+	
+	$data[PlanetGenerator::COLGEN_BASEFIELD] = 201;
+	$odata[PlanetGenerator::COLGEN_BASEFIELD] = 900;
+	$udata[PlanetGenerator::COLGEN_BASEFIELD] = 802;
+
+	$phases = 0;
+	$ophases = 0;
+	$uphases = 0;
+		
+	// Surface Phases
+		
+	$phase[$phases][PlanetGenerator::COLGEN_MODE] = "fullsurface";
+	$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Komplett-Oberfläche";
+	$phase[$phases][PlanetGenerator::COLGEN_TYPE] = 61;
+	$phases++;
+	
+	// Orbit Phases
+	
+	$ophase[$ophases][PlanetGenerator::COLGEN_MODE] = "upper orbit";
+	$ophase[$ophases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lower Orbit";
+	$ophase[$ophases][PlanetGenerator::COLGEN_NUM] = 10;
+	$ophase[$ophases][PlanetGenerator::COLGEN_FROM] = array("0" => "900");
+	$ophase[$ophases][PlanetGenerator::COLGEN_TO]   = array("0" => "961");
+	$ophase[$ophases][PlanetGenerator::COLGEN_ADJACENT] = 0;
+	$ophase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+	$ophase[$ophases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+	$ophase[$ophases][PlanetGenerator::COLGEN_FRAGMENTATION] = 2;
+	$ophases++;		
+	
+	
+?>
