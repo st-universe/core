35e91bd220a267a1bf18f004e185e08bdd76e173
Huxinator <nicotintel@gmail.com>
2020-04-06 23:59:07 +0200

    update x

--- src/PlanetGenerator/PlanetGenerator/coldata/217.php
+++ src/PlanetGenerator/PlanetGenerator/coldata/217.php
@@ -24,7 +24,7 @@ $magma = rand(5, 9);
 
 
 
-$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "equatorial";
 $phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava rechtsrunter";
 $phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
 $phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
@@ -71,7 +71,7 @@ $phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
 $phases++;
 
 
-$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "polar seeding south";
 $phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan rechts";
 $phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
 $phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
@@ -95,7 +95,7 @@ $phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
 $phases++;
 
 
-$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "polar seeding north";
 $phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan runter";
 $phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
 $phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
@@ -130,7 +130,7 @@ $phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
 $phases++;
 
 
-$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "equatorial";
 $phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava Ende";
 $phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
 $phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
@@ -187,7 +187,7 @@ $phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "902");
 $phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = 0;
 $phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
 $phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
-$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 20;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 30;
 $phases++;
 
 
@@ -203,7 +203,7 @@ $uphase[$uphases][PlanetGenerator::COLGEN_TO] = array("0" => "831");
 $uphase[$uphases][PlanetGenerator::COLGEN_ADJACENT] = 0;
 $uphase[$uphases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
 $uphase[$uphases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
-$uphase[$uphases][PlanetGenerator::COLGEN_FRAGMENTATION] = 2;
+$uphase[$uphases][PlanetGenerator::COLGEN_FRAGMENTATION] = 8;
 $uphases++;
 
 
