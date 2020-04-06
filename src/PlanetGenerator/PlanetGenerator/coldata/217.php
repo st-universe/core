0ee4532ec155f511b14d782016b8344548b1553e
Huxinator <nicotintel@gmail.com>
2020-04-06 23:29:17 +0200

    update x gen

--- src/PlanetGenerator/PlanetGenerator/coldata/217.php
+++ src/PlanetGenerator/PlanetGenerator/coldata/217.php
@@ -23,6 +23,151 @@ $magma = rand(5, 9);
 // Surface Phases
 
 
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava rechtsrunter";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "909");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "right";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lavastrom horizontal";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "910");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(909);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "right";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan links";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "906");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(910);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "below";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava Ende";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "922");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(909);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan rechts";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "904");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 2;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "right";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava Ende";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "914");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(904);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan runter";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "919");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 2;
+$phases++;
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "below";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava runter";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "907");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(919);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "right";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava Ende";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "923");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(907);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava Ende";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "912");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 25;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "right";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Lava links runter";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "911");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(912);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+
+$phase[$phases][PlanetGenerator::COLGEN_MODE] = "below";
+$phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Vulkan hoch";
+$phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
+$phase[$phases][PlanetGenerator::COLGEN_FROM] = array("0" => "918");
+$phase[$phases][PlanetGenerator::COLGEN_TO] = array("0" => "920");
+$phase[$phases][PlanetGenerator::COLGEN_ADJACENT] = array(911);
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
+$phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 1;
+$phases++;
+
+/*
+
+
 $phase[$phases][PlanetGenerator::COLGEN_MODE] = "normal";
 $phase[$phases][PlanetGenerator::COLGEN_DESCRIPTION] = "Plattform";
 $phase[$phases][PlanetGenerator::COLGEN_NUM] = 1;
@@ -46,9 +191,10 @@ $phase[$phases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
 $phase[$phases][PlanetGenerator::COLGEN_FRAGMENTATION] = 20;
 $phases++;
 
+*/
 
 // Underground
-
+/*
 $uphase[$uphases][PlanetGenerator::COLGEN_MODE] = "normal";
 $uphase[$uphases][PlanetGenerator::COLGEN_DESCRIPTION] = "Untergrund Magma";
 $uphase[$uphases][PlanetGenerator::COLGEN_NUM] = $magma;
@@ -59,7 +205,7 @@ $uphase[$uphases][PlanetGenerator::COLGEN_NOADJACENT] = 0;
 $uphase[$uphases][PlanetGenerator::COLGEN_NOADJACENTLIMIT] = 0;
 $uphase[$uphases][PlanetGenerator::COLGEN_FRAGMENTATION] = 2;
 $uphases++;
-
+*/
 
 
 return [
