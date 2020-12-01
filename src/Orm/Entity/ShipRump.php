7201fb9ec4d52c0a9780acbd7998e34ca256468d
jrichter <jrichter@intersoft.de>
2020-12-01 19:47:32 +0100

    Beam-Faktor zum Datenmodell hinzugefügt

--- src/Orm/Entity/ShipRump.php
+++ src/Orm/Entity/ShipRump.php
@@ -66,6 +66,9 @@ class ShipRump implements ShipRumpInterface
 
     /** @column(type="smallint") * */
     private $base_torpedo_storage = 0;
+    
+    /** @column(type="smallint") * */
+    private $beam_factor = 0;
 
     /** @column(type="smallint") * */
     private $phaser_volleys = 0;
@@ -303,6 +306,17 @@ class ShipRump implements ShipRumpInterface
         return $this;
     }
 
+    public function getBeamFactor(): int;
+    {
+        return $this->beam_factor;
+    }
+
+    public function setBeamFactor(int $beamFactor): int;
+    {
+        $this->beam_factor = $beamFactor;
+        return $this;
+    }
+    
     public function getPhaserVolleys(): int
     {
         return $this->phaser_volleys;
