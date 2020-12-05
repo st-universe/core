04f08cf1aa0d5d82d2f1e0dd5b553d854a473e5e
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-05 23:35:10 +0100

    Revert "PrettyException for user 126"
    This reverts commit 413d77e14e1e15fa4d4fd9b683dae559b1a3f576.

--- src/Config/ErrorHandler.php
+++ src/Config/ErrorHandler.php
@@ -3,17 +3,15 @@
 declare(strict_types=1);
 
 use Noodlehaus\ConfigInterface;
-use Stu\Module\Control\GameControllerInterface;
 use Whoops\Handler\PlainTextHandler;
 use Whoops\Handler\PrettyPageHandler;
 use Whoops\Run;
 
 $config = $container->get(ConfigInterface::class);
-$gameController = $container->get(GameControllerInterface::class);
 
 $whoops = new Run();
 
-if ($gameController->getUser()->getId() == 126 || $config->get('debug.debug_mode') === true) {
+if ($config->get('debug.debug_mode') === true) {
 //    error_reporting(E_ALL);
     error_reporting(E_ALL & ~E_NOTICE);
 
