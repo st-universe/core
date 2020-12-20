8276c79657a4c0558c19c786fbac4b15a3f4f096
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-20 11:14:11 +0100

    Revert "goods locations"
    This reverts commit c18598499d42013b1714e3a695942a1f53b09434.

--- src/Module/Database/View/ShowGoodsLocations/ShowGoodsLocationsRequestInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Stu\Module\Database\View\ShowGoodsLocations;
-
-interface ShowGoodsLocationsRequestInterface
-{
-    public function getCommodityId(): int;
-}
--- src/Public/static/js/database.js
+++
@@ -1,5 +0,0 @@
-function getCommodityLocations(commodityId) {
-	elt = 'commodityLocations';
-	openPJsWin(elt, 1);
-	ajax_update(elt,'database.php?commodityId='+commodityId+'&SHOW_GOODS_LOCATIONS=1');
-}
