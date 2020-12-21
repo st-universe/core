8cce738cddee9c8761ce1c3452eec1e9ec52757a
jrichter <jrichter@intersoft.de>
2020-12-21 17:55:56 +0100

    infos when systems can't be activated

--- src/Module/Ship/Action/FleetAlertRed/FleetAlertRed.php
+++ src/Module/Ship/Action/FleetAlertRed/FleetAlertRed.php
@@ -54,17 +54,19 @@ final class FleetAlertRed implements ActionControllerInterface
                 $game->addInformation(sprintf(_('%s: Nicht genügend Energie um auf Alarm-Rot zu wechseln'), $ship->getName()));
                 continue;
             }
-
+            
             $alertSystems = [
-                ShipSystemTypeEnum::SYSTEM_SHIELDS,
-                ShipSystemTypeEnum::SYSTEM_PHASER,
-                ShipSystemTypeEnum::SYSTEM_TORPEDO
+                'Schilde' => ShipSystemTypeEnum::SYSTEM_SHIELDS,
+                'Phaser' => ShipSystemTypeEnum::SYSTEM_PHASER,
+                'Torpedowerfer' => ShipSystemTypeEnum::SYSTEM_TORPEDO
             ];
-    
-            foreach ($alertSystems as $systemId) {
+            
+            foreach ($alertSystems as $key => $systemId) {
                 try {
                     $this->shipSystemManager->activate($ship, $systemId);
+                    $game->addInformation(sprintf(_('%s: System %s wurde aktiviert'), $ship->getName(), $key));
                 } catch (ShipSystemException $e) {
+                    $game->addInformation(sprintf(_('%s: System %s konnte nicht aktiviert werden'), $ship->getName(), $key));
                     continue;
                 }
             }
