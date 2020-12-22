3e359f2d1cd9d1207ac59c2d9955785282ad1053
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-22 21:33:26 +0100

    Revert "Revert "rename hasCloak method""
    This reverts commit 9b97da5145a2c59afb1f1f9958628f471190f274.

--- src/Orm/Entity/Ship.php
+++ src/Orm/Entity/Ship.php
@@ -1345,6 +1345,11 @@ class Ship implements ShipInterface
         return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_TORPEDO);
     }
 
+    public function hasCloak(): bool
+    {
+        return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_CLOAK);
+    }
+
     public function hasWarpcore(): bool
     {
         return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_WARPCORE);
