54f44720ddec9679575e8e552f9ff66f6512b5bc
jrichter <jrichter@intersoft.de>
2020-12-23 01:11:22 +0100

    remove code duplication

--- src/Module/Ship/Action/FleetDeactivateCloak/FleetDeactivateCloak.php
+++ src/Module/Ship/Action/FleetDeactivateCloak/FleetDeactivateCloak.php
@@ -5,53 +5,22 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\FleetDeactivateCloak;
 
 use request;
-use Stu\Component\Ship\System\ShipSystemManagerInterface;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
-use Stu\Module\Ship\Lib\ShipLoaderInterface;
-use Stu\Module\Ship\View\ShowShip\ShowShip;
-use Stu\Orm\Repository\ShipRepositoryInterface;
+use Stu\Module\Ship\Action\Deactivate\AbstractSystemDeactivator;
 
-final class FleetDeactivateCloak implements ActionControllerInterface
+final class FleetDeactivateCloak extends AbstractSystemDeactivator implements ActionControllerInterface
 {
     public const ACTION_IDENTIFIER = 'B_FLEET_DEACTIVATE_CLOAK';
-
-    private ShipLoaderInterface $shipLoader;
-
-    private ShipRepositoryInterface $shipRepository;
-
-    private ShipSystemManagerInterface $shipSystemManager;
-
-    public function __construct(
-        ShipLoaderInterface $shipLoader,
-        ShipRepositoryInterface $shipRepository,
-        ShipSystemManagerInterface $shipSystemManager
-    ) {
-        $this->shipLoader = $shipLoader;
-        $this->shipRepository = $shipRepository;
-        $this->shipSystemManager = $shipSystemManager;
-    }
 
     public function handle(GameControllerInterface $game): void
     {
         $game->setView(ShowShip::VIEW_IDENTIFIER);
 
-        $userId = $game->getUser()->getId();
-
-        $ship = $this->shipLoader->getByIdAndUser(
-            request::indInt('id'),
-            $userId
-        );
-
-        foreach ($ship->getFleet()->getShips() as $ship) {
-            $this->shipSystemManager->deactivate($ship, ShipSystemTypeEnum::SYSTEM_CLOAK);
-
-            $this->shipRepository->save($ship);
-        }
-        // @todo red alert
-
-        $game->addInformation(_("Flottenbefehl ausgeführt: Deaktivierung der Tarnung"));
+        $this->deactivateFleet(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_CLOAK, _('Tarnung'), $game);
+        
+        // @TODO red alert
     }
 
     public function performSessionCheck(): bool
