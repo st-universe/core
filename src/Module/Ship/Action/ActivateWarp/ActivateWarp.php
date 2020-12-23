6410d2a573686af895441f216939d836134ee8bf
jrichter <jrichter@intersoft.de>
2020-12-24 00:49:26 +0100

    wrong system type

--- src/Module/Ship/Action/ActivateWarp/ActivateWarp.php
+++ src/Module/Ship/Action/ActivateWarp/ActivateWarp.php
@@ -27,7 +27,7 @@ final class ActivateWarp implements ActionControllerInterface
     {
         $game->setView(ShowShip::VIEW_IDENTIFIER);
 
-        $this->helper->activate(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_CLOAK, _('Warpantrieb'), $game);
+        $this->helper->activate(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_WARPDRIVE, _('Warpantrieb'), $game);
     }
 
     public function performSessionCheck(): bool
