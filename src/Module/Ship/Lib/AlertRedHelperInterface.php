0b1b32896fe9379a4a7b88f8307d5df2444c5780
jrichter <jrichter@intersoft.de>
2020-12-23 14:00:53 +0100

    add & to interface

--- src/Module/Ship/Lib/AlertRedHelperInterface.php
+++ src/Module/Ship/Lib/AlertRedHelperInterface.php
@@ -6,7 +6,7 @@ use Stu\Orm\Entity\ShipInterface;
 
 interface AlertRedHelperInterface
 {
-    public function checkForAlertRedShips(ShipInterface $leadShip, $informations) : array;
+    public function checkForAlertRedShips(ShipInterface $leadShip, &$informations) : array;
 
-    public function performAttackCycle(ShipInterface $alertShip, ShipInterface $leadShip, $informations) : void;
+    public function performAttackCycle(ShipInterface $alertShip, ShipInterface $leadShip, &$informations) : void;
 }
