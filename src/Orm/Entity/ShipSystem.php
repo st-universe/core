eb25a22526ccff5cbf43e534a95be7e4962370d3
jrichter <jrichter@intersoft.de>
2020-12-23 23:35:12 +0100

    add system status styles

--- src/Orm/Entity/ShipSystem.php
+++ src/Orm/Entity/ShipSystem.php
@@ -90,6 +90,29 @@ class ShipSystem implements ShipSystemInterface
         return $this;
     }
 
+    public function getCssClass(): string
+    {
+        if ($this->getStatus() < 1)
+        {
+            return _("sysStatus0");
+        } elseif ($this->getStatus() < 26)
+        {
+            return _("sysStatus1to25");
+        }
+        } elseif ($this->getStatus() < 51)
+        {
+            return _("sysStatus26to50");
+        }
+        } elseif ($this->getStatus() < 76)
+        {
+            return _("sysStatus51to75");
+        }
+        } else
+        {
+            return _("sysStatus76to100");
+        }
+    }
+
     public function getMode(): int
     {
         return $this->mode;
