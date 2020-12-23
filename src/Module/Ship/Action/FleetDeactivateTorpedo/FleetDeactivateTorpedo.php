b2519fa2969ee0146d7cab8f1595cbf3c924e74c
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-23 01:43:03 +0100

    Revert "remove code duplication"
    This reverts commit 54f44720ddec9679575e8e552f9ff66f6512b5bc.

--- src/Module/Ship/Action/FleetDeactivateTorpedo/FleetDeactivateTorpedo.php
+++ src/Module/Ship/Action/FleetDeactivateTorpedo/FleetDeactivateTorpedo.php
@@ -5,20 +5,51 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\FleetDeactivateTorpedo;
 
 use request;
+use Stu\Component\Ship\System\ShipSystemManagerInterface;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
-use Stu\Module\Ship\Action\Deactivate\AbstractSystemDeactivator;
+use Stu\Module\Ship\Lib\ShipLoaderInterface;
+use Stu\Module\Ship\View\ShowShip\ShowShip;
+use Stu\Orm\Repository\ShipRepositoryInterface;
 
-final class FleetDeactivateTorpedo extends AbstractSystemDeactivator implements ActionControllerInterface
+final class FleetDeactivateTorpedo implements ActionControllerInterface
 {
     public const ACTION_IDENTIFIER = 'B_FLEET_DEACTIVATE_TORPEDO';
+
+    private ShipLoaderInterface $shipLoader;
+
+    private ShipRepositoryInterface $shipRepository;
+
+    private ShipSystemManagerInterface $shipSystemManager;
+
+    public function __construct(
+        ShipLoaderInterface $shipLoader,
+        ShipRepositoryInterface $shipRepository,
+        ShipSystemManagerInterface $shipSystemManager
+    ) {
+        $this->shipLoader = $shipLoader;
+        $this->shipRepository = $shipRepository;
+        $this->shipSystemManager = $shipSystemManager;
+    }
 
     public function handle(GameControllerInterface $game): void
     {
         $game->setView(ShowShip::VIEW_IDENTIFIER);
 
-        $this->deactivateFleet(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_TORPEDO, _('Projektilwaffe'), $game);
+        $userId = $game->getUser()->getId();
+
+        $ship = $this->shipLoader->getByIdAndUser(
+            request::indInt('id'),
+            $userId
+        );
+
+        foreach ($ship->getFleet()->getShips() as $ship) {
+            $this->shipSystemManager->deactivate($ship, ShipSystemTypeEnum::SYSTEM_TORPEDO);
+
+            $this->shipRepository->save($ship);
+        }
+        $game->addInformation(_("Flottenbefehl ausgeführt: Deaktivierung der Projektilwaffe"));
     }
 
     public function performSessionCheck(): bool
