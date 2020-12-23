54bdfbf880f1e9b0c2f06066563fd8bb8929e3fe
jrichter <jrichter@intersoft.de>
2020-12-23 02:39:32 +0100

    use helper class

--- src/Module/Ship/Action/FleetDeactivateNbs/FleetDeactivateNbs.php
+++ src/Module/Ship/Action/FleetDeactivateNbs/FleetDeactivateNbs.php
@@ -5,51 +5,29 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\FleetDeactivateNbs;
 
 use request;
-use Stu\Component\Ship\System\ShipSystemManagerInterface;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
-use Stu\Module\Ship\Lib\ShipLoaderInterface;
 use Stu\Module\Ship\View\ShowShip\ShowShip;
-use Stu\Orm\Repository\ShipRepositoryInterface;
+use Stu\Module\Ship\Lib\ActivatorDeactivatorHelperInterface;
 
 final class FleetDeactivateNbs implements ActionControllerInterface
 {
     public const ACTION_IDENTIFIER = 'B_FLEET_DEACTIVATE_NBS';
 
-    private ShipLoaderInterface $shipLoader;
-
-    private ShipRepositoryInterface $shipRepository;
-
-    private ShipSystemManagerInterface $shipSystemManager;
+    private ActivatorDeactivatorHelperInterface $helper;
 
     public function __construct(
-        ShipLoaderInterface $shipLoader,
-        ShipRepositoryInterface $shipRepository,
-        ShipSystemManagerInterface $shipSystemManager
+        ActivatorDeactivatorHelperInterface $helper
     ) {
-        $this->shipLoader = $shipLoader;
-        $this->shipRepository = $shipRepository;
-        $this->shipSystemManager = $shipSystemManager;
+        $this->helper = $helper;
     }
 
     public function handle(GameControllerInterface $game): void
     {
         $game->setView(ShowShip::VIEW_IDENTIFIER);
 
-        $userId = $game->getUser()->getId();
-
-        $ship = $this->shipLoader->getByIdAndUser(
-            request::indInt('id'),
-            $userId
-        );
-
-        foreach ($ship->getFleet()->getShips() as $ship) {
-            $this->shipSystemManager->deactivate($ship, ShipSystemTypeEnum::SYSTEM_NBS);
-
-            $this->shipRepository->save($ship);
-        }
-        $game->addInformation("Flottenbefehl ausgeführt: Deaktivierung der Nahbereichssensoren");
+        $this->helper->deactivateFleet(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_NBS, _('Nahbereichssensoren'), $game);
     }
 
     public function performSessionCheck(): bool
