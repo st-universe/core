7e5be125df75e40194e06027acb468fed5e6ab90
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-23 01:43:03 +0100

    Revert "remove code duplication"
    This reverts commit 54f44720ddec9679575e8e552f9ff66f6512b5bc.

--- src/Module/Ship/Action/DeactivateCloak/DeactivateCloak.php
+++ src/Module/Ship/Action/DeactivateCloak/DeactivateCloak.php
@@ -5,21 +5,52 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\DeactivateCloak;
 
 use request;
+use Stu\Component\Ship\System\ShipSystemManagerInterface;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
-use Stu\Module\Ship\Action\Deactivate\AbstractSystemDeactivator;
+use Stu\Module\Ship\Lib\ShipLoaderInterface;
+use Stu\Module\Ship\View\ShowShip\ShowShip;
+use Stu\Orm\Repository\ShipRepositoryInterface;
 
-final class DeactivateCloak extends AbstractSystemDeactivator implements ActionControllerInterface
+final class DeactivateCloak implements ActionControllerInterface
 {
     public const ACTION_IDENTIFIER = 'B_DEACTIVATE_CLOAK';
+
+    private ShipLoaderInterface $shipLoader;
+
+    private ShipRepositoryInterface $shipRepository;
+
+    private ShipSystemManagerInterface $shipSystemManager;
+
+    public function __construct(
+        ShipLoaderInterface $shipLoader,
+        ShipRepositoryInterface $shipRepository,
+        ShipSystemManagerInterface $shipSystemManager
+    ) {
+        $this->shipLoader = $shipLoader;
+        $this->shipRepository = $shipRepository;
+        $this->shipSystemManager = $shipSystemManager;
+    }
 
     public function handle(GameControllerInterface $game): void
     {
         $game->setView(ShowShip::VIEW_IDENTIFIER);
 
-        // @TODO redalert
-        $this->deactivate(request::indInt('id'), ShipSystemTypeEnum::SYSTEM_CLOAK, _('Tarnung'), $game);
+        $userId = $game->getUser()->getId();
+
+        $ship = $this->shipLoader->getByIdAndUser(
+            request::indInt('id'),
+            $userId
+        );
+
+        $this->shipSystemManager->deactivate($ship, ShipSystemTypeEnum::SYSTEM_CLOAK);
+
+        $this->shipRepository->save($ship);
+
+        // @todo redalert
+
+        $game->addInformation("Tarnung deaktiviert");
     }
 
     public function performSessionCheck(): bool
