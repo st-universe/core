0db6c6dffc2fad02a1c4271f0d43b9a27a80daed
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-24 12:57:38 +0100

    Revert "Tachyon Scanner and Warpdrive fix"
    This reverts commit 75a2ed810c68aafde8f11ec834f89271ec985600.

--- src/Module/ShipModule/ModuleSpecialAbilityEnum.php
+++ src/Module/ShipModule/ModuleSpecialAbilityEnum.php
@@ -8,7 +8,7 @@ final class ModuleSpecialAbilityEnum
 {
     public const MODULE_SPECIAL_CLOAK = 1;
     public const MODULE_SPECIAL_RPG = 2;
-    public const MODULE_SPECIAL_TACHYON_SCANNER = 4;
+    public const MODULE_SPECIAL_TOMATO_PEELER = 42;
 
     public static function getDescription(int $specialId): string {
         switch ($specialId) {
@@ -16,8 +16,8 @@ final class ModuleSpecialAbilityEnum
                 return _('Tarnung');
             case static::MODULE_SPECIAL_RPG:
                 return _('RPG-Schiff');
-            case static::MODULE_SPECIAL_TACHYON_SCANNER:
-                return _('Tachyon-Scanner');
+            case static::MODULE_SPECIAL_TOMATO_PEELER:
+                return _('Tomaten-Schäler');
         }
         return '';
     }
