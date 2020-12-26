f3197aa30ddd05b00bc9b3ba70c8311b3677846b
jrichter <jrichter@intersoft.de>
2020-12-26 21:54:48 +0100

    verhuxt

---
+++ src/Module/Ship/Action/DoTachyonScan/DoTachyonScan.php
@@ -0,0 +1,95 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Ship\Action\DoTachyonScan;
+
+use request;
+use Stu\Module\Control\ActionControllerInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Ship\Lib\ShipLoaderInterface;
+use Stu\Module\Ship\View\ShowShip\ShowShip;
+use Stu\Orm\Repository\MapRepositoryInterface;
+use Stu\Orm\Repository\ShipRepositoryInterface;
+use Stu\Orm\Repository\StarSystemMapRepositoryInterface;
+use Stu\Orm\Repository\TachyonScanRepositoryInterface;
+
+final class DoTachyonScan implements ActionControllerInterface
+{
+    public const ACTION_IDENTIFIER = 'B_TACHYON_SCAN';
+
+    private ShipLoaderInterface $shipLoader;
+
+    private TachyonScanRepositoryInterface $tachyonScanRepository;
+
+    private MapRepositoryInterface $mapRepository;
+
+    private StarSystemMapRepositoryInterface $starSystemMapRepository;
+
+    private ShipRepositoryInterface $shipRepository;
+
+    public function __construct(
+        ShipLoaderInterface $shipLoader,
+        TachyonScanRepositoryInterface $tachyonScanRepository,
+        MapRepositoryInterface $mapRepository,
+        StarSystemMapRepositoryInterface $starSystemMapRepository,
+        ShipRepositoryInterface $shipRepository
+    ) {
+        $this->shipLoader = $shipLoader;
+        $this->tachyonScanRepository = $tachyonScanRepository;
+        $this->mapRepository = $mapRepository;
+        $this->starSystemMapRepository = $starSystemMapRepository;
+        $this->shipRepository = $shipRepository;
+    }
+
+    public function handle(GameControllerInterface $game): void
+    {
+        $game->setView(ShowShip::VIEW_IDENTIFIER);
+
+        $userId = $game->getUser()->getId();
+
+        $ship = $this->shipLoader->getByIdAndUser(
+            request::indInt('id'),
+            $userId
+        );
+
+        // scanner needs to be present
+        if (!$ship->hasTachyonScanner())
+        {
+            $game->addInformation(_('Aktion nicht möglich, kein Tachyon-Scanner installiert!'));
+            return;
+        }
+
+        // scanner needs to be active
+        if (!$ship->getTachyonState())
+        {
+            $game->addInformation(_('Aktion nicht möglich, der Tachyon-Scanner muss aktiviert sein!'));
+            return;
+        }
+
+        $tachyonScan = $this->tachyonScanRepository->prototype();
+        $tachyonScan.setUser($ship->getUser());
+
+        if ($ship->getSystem() === null)
+        {
+            $tachyonScan.setMap($this->mapRepository->getByCoordinates($ship->getPosX(), $ship->getPosY()));
+        }
+        else {
+            $tachyonScan.setStarsystemMap($this->starSystemMapRepository->
+                            getByCoordinates($ship->getSystem()->getId(), $ship->getPosX(), $ship->getPosY()));
+        }
+
+        $tachyonScan.setScanTime(time());
+        $this->tachyonScanRepository->save($tachyonScan);
+
+        $ship->setEps($ship->getEps() - 10);
+        $this->shipRepository->save($ship);
+
+        $game->addInformation("Der umfangreiche Tachyon-Scan wurde durchgeführt");
+    }
+
+    public function performSessionCheck(): bool
+    {
+        return true;
+    }
+}
