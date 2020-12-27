2c34228fa879254001e8eeb096d315f71c673867
jrichter <jrichter@intersoft.de>
2020-12-27 20:15:08 +0100

    fix field name

--- src/Orm/Repository/TachyonScanRepository.php
+++ src/Orm/Repository/TachyonScanRepository.php
@@ -23,10 +23,16 @@ final class TachyonScanRepository extends EntityRepository implements TachyonSca
     {
         $rsm = new ResultSetMapping();
         $rsm->addEntityResult(TachyonScan::class, 't');
-        $rsm->addFieldResult('t', 'scan_id', 'scan_id');
+        $rsm->addFieldResult('t', 'id', 'id');
+        $rsm->addFieldResult('t', 'user_id', 'user_id');
+        $rsm->addFieldResult('t', 'map_id', 'map_id');
+        $rsm->addFieldResult('t', 'starsystem_map_id', 'starsystem_map_id');
+        $rsm->addFieldResult('t', 'scan_time', 'scan_time');
 
         return $this->getEntityManager()->createNativeQuery(
-                'SELECT ts.id as scan_id FROM stu_tachyon_scan ts
+                'SELECT ts.id as id, ts.user_id as user_id, ts.map_id as map_id, ts.starsystem_map_id as starsystem_map_id,
+                        ts.scan_time as scan_time
+                FROM stu_tachyon_scan ts
                 WHERE ts.scan_time > :theTime
                 AND ts.user_id = :userId
                 AND (CASE WHEN :isSystem = true
