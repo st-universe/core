b128cb1e7a1f8b36badac9cc478f51752090f5b1
Huxinator <52821266+Huxinator@users.noreply.github.com>
2020-12-27 12:00:43 +0100

    Revert "ask for active tachyon scans"
    This reverts commit 468d20c20fac0545e07ee633f384b3e67ff18b28.

--- src/Orm/Repository/TachyonScanRepositoryInterface.php
+++ src/Orm/Repository/TachyonScanRepositoryInterface.php
@@ -3,7 +3,6 @@
 namespace Stu\Orm\Repository;
 
 use Doctrine\Persistence\ObjectRepository;
-use Stu\Orm\Entity\ShipInterface;
 use Stu\Orm\Entity\TachyonScanInterface;
 
 /**
@@ -14,6 +13,4 @@ interface TachyonScanRepositoryInterface extends ObjectRepository
     public function prototype(): TachyonScanInterface;
 
     public function save(TachyonScanInterface $obj): void;
-
-    public function findActiveByShipLocationAndOwner(ShipInterface $ship): iterable;
 }
