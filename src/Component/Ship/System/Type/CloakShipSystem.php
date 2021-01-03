98e7146dda7d9db62d6a91d451798d83854d1348
jrichter <jrichter@intersoft.de>
2021-01-03 13:41:26 +0100

    improve cloak conditions

--- src/Component/Ship/System/Type/CloakShipSystem.php
+++ src/Component/Ship/System/Type/CloakShipSystem.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stu\Component\Ship\System\Type;
 
+use Stu\Component\Ship\ShipAlertStateEnum;
 use Stu\Component\Ship\System\ShipSystemModeEnum;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Component\Ship\System\ShipSystemTypeInterface;
@@ -11,6 +12,17 @@ use Stu\Orm\Entity\ShipInterface;
 
 final class CloakShipSystem extends AbstractShipSystemType implements ShipSystemTypeInterface
 {
+    public function checkActivationConditions(ShipInterface $ship, &$reason): bool
+    {
+        if ($ship->getAlertState() == ShipAlertStateEnum::ALERT_RED)
+        {
+            $reason = _('die Alarmstufe Rot ist');
+            return false;
+        }
+
+        return true;
+    }
+
     public function getEnergyUsageForActivation(): int
     {
         return 10;
@@ -21,11 +33,20 @@ final class CloakShipSystem extends AbstractShipSystemType implements ShipSystem
         return 8;
     }
 
+    public function getPriority(): int
+    {
+        return 0;
+    }
+
     public function activate(ShipInterface $ship): void
     {
         $ship->deactivateTraktorBeam();
         $ship->setDockedTo(null);
+
         $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_SHIELDS)->setMode(ShipSystemModeEnum::MODE_OFF);
+        $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_PHASER)->setMode(ShipSystemModeEnum::MODE_OFF);
+        $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_TORPEDO)->setMode(ShipSystemModeEnum::MODE_OFF);
+
         $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_CLOAK)->setMode(ShipSystemModeEnum::MODE_ON);
     }
     
