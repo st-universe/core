a58983b092655935391733601dba6059c40092a6
jrichter <jrichter@intersoft.de>
2021-01-11 17:45:08 +0100

    add enum entries and troop quarters ship system

--- src/Orm/Entity/Ship.php
+++ src/Orm/Entity/Ship.php
@@ -1516,6 +1516,11 @@ class Ship implements ShipInterface
         return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_TACHYON_SCANNER);
     }
 
+    public function hasTroopQuarters(): bool
+    {
+        return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_TROOP_QUARTERS);
+    }
+
     public function hasWarpcore(): bool
     {
         return $this->hasShipSystem(ShipSystemTypeEnum::SYSTEM_WARPCORE);
