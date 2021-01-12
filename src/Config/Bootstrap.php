35ab630a7111560ea84ed43ad5e34b261b5ab9c4
Huxinator <52821266+Huxinator@users.noreply.github.com>
2021-01-12 18:46:03 +0100

    Revert "killing me softly"
    This reverts commit 1babcfa7bbc578ebaa262b77d4eeb6c3c3e1df86.

--- src/Config/Bootstrap.php
+++ src/Config/Bootstrap.php
@@ -6,7 +6,7 @@ namespace Stu\Config;
 
 use Cache\Adapter\PHPArray\ArrayCachePool;
 use Cache\Adapter\Redis\RedisCachePool;
-use Cache\Bridge\Doctrine\DoctrineCacheBridge;;;;;;;;;;;KILLING ME SOFTLY
+use Cache\Bridge\Doctrine\DoctrineCacheBridge;
 use Curl\Curl;
 use DI\ContainerBuilder;
 use Doctrine\ORM\Configuration;
