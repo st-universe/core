fcbc78f160152fcfbf514a7e52c3d2b4f0707b62
jrichter <jrichter@intersoft.de>
2021-01-17 15:23:41 +0100

    clear ship system list

--- src/Module/Colony/Action/DisassembleShip/DisassembleShip.php
+++ src/Module/Colony/Action/DisassembleShip/DisassembleShip.php
@@ -73,6 +73,8 @@ final class DisassembleShip implements ActionControllerInterface
         $ship = $this->shipLoader->getByIdAndUser((int) $ship_id, $userId);
         $this->retrieveSomeIntactModules($ship, $colony, $game);
 
+        $ship->getSystems()->clear();
+
         $this->shipRemover->remove($ship);
 
         $game->addInformationf(_('Das Schiff wurde demontiert'));
