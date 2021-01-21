3cdf239332abd5dc6fa295dee75065a169876eac
jrichter <jrichter@intersoft.de>
2021-01-21 21:19:00 +0100

    get researched terraformings

--- src/Orm/Repository/TerraformingRepository.php
+++ src/Orm/Repository/TerraformingRepository.php
@@ -5,12 +5,28 @@ declare(strict_types=1);
 namespace Stu\Orm\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Stu\Orm\Entity\Researched;
+use Stu\Orm\Entity\Terraforming;
 
 final class TerraformingRepository extends EntityRepository implements TerraformingRepositoryInterface
 {
-    public function getBySourceFieldType(int $sourceFieldTypeId): array {
-        return $this->findBy([
-            'v_feld' => $sourceFieldTypeId
-        ]);
+    public function getBySourceFieldType(int $sourceFieldTypeId, int $userId): iterable
+    {
+        return $this->getEntityManager()->createQuery(
+            sprintf(
+                'SELECT t FROM %s t
+                 WHERE t.v_feld = :sourceFieldTypeId
+                 AND (t.research_id IS NULL
+                        OR EXISTS (SELECT r.id
+                                    FROM %s r
+                                    WHERE t.research_id = r.research_id
+                                    AND t.user_id = :userId))',
+                Terraforming::class,
+                Researched::class
+            )
+        )->setParameters([
+            'userId' => $userId,
+            'sourceFieldTypeId' => $sourceFieldTypeId
+        ])->getResult();
     }
 }
