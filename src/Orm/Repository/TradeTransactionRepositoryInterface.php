32a801905bb01e354d0846b55650a3ccca460f66
jrichter <jrichter@intersoft.de>
2021-01-25 21:20:48 +0100

    new entity trade transaction

---
+++ src/Orm/Repository/TradeTransactionRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Stu\Orm\Repository;
+
+use Doctrine\Persistence\ObjectRepository;
+use Stu\Orm\Entity\TradeTransactionInterface;
+
+/**
+ * @method null|TradeTransactionInterface find(integer $id)
+ */
+interface TradeTransactionRepositoryInterface extends ObjectRepository
+{
+    public function prototype(): TradeTransactionInterface;
+
+    public function save(TradeTransactionInterface $tradeTransaction): void;
+}
