8094b30ea6e0d092d0a554ba59b1b0a7ab6db932
jrichter <jrichter@intersoft.de>
2021-01-29 20:05:06 +0100

    show signature age and nbs check

--- src/Module/Ship/View/ShowSectorScan/ShowSectorScan.php
+++ src/Module/Ship/View/ShowSectorScan/ShowSectorScan.php
@@ -19,8 +19,8 @@ final class ShowSectorScan implements ViewControllerInterface
 
     private FlightSignatureRepositoryInterface $flightSignatureRepository;
 
-    private $otherSigCount = 0;
-    private $otherCloakCount = 0;
+    private $fadedSignaturesUncloaked = [];
+    private $fadedSignaturesCloaked = [];
 
     public function __construct(
         ShipLoaderInterface $shipLoader,
@@ -38,6 +38,11 @@ final class ShowSectorScan implements ViewControllerInterface
             request::indInt('id'),
             $userId
         );
+
+        if (!$ship->getNbs()) {
+            $game->addInformation("Die Nahbereichssensoren sind nicht aktiv");
+            return;
+        }
 
         $game->setPageTitle("Sektor Scan");
         $game->setTemplateFile('html/ajaxwindow.xhtml');
@@ -60,8 +65,8 @@ final class ShowSectorScan implements ViewControllerInterface
         }
 
         $game->setTemplateVar('SIGNATURES', $this->getSignatures($mapField, $ship->getSystem() !== null, $userId));
-        $game->setTemplateVar('OTHER_SIG_COUNT', $this->otherSigCount > 0 ? $this->otherSigCount : null);
-        $game->setTemplateVar('OTHER_CLOAKED_COUNT', $this->otherCloakCount > 0 ? $this->otherCloakCount : null);
+        $game->setTemplateVar('OTHER_SIG_COUNT', empty($this->fadedSignaturesUncloaked) ? null : count($this->fadedSignaturesUncloaked));
+        $game->setTemplateVar('OTHER_CLOAKED_COUNT', empty($this->fadedSignaturesCloaked) ? null : count($this->fadedSignaturesCloaked));
         $game->setTemplateVar('SHIP', $ship);
     }
 
@@ -80,10 +85,10 @@ final class ShowSectorScan implements ViewControllerInterface
                 if ($wrapper->getRump() == null) {
                     if ($sig->isCloaked()) {
                         if ($sig->getTime() > (time() - 43200)) {
-                            $this->otherCloakCount++;
+                            $this->fadedSignaturesCloaked[$id] = $id;
                         }
                     } else {
-                        $this->otherSigCount++;
+                        $this->fadedSignaturesUncloaked[$id] = $id;
                     }
                 } else {
                     $filteredSigs[$id] = $wrapper;
