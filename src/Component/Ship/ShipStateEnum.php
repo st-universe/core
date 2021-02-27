3d2cdfde51c06b5a015b0a7649e8a519ce3f168a
jrichter <jrichter@intersoft.de>
2021-02-27 19:51:29 +0100

    new ship state

--- src/Component/Ship/ShipStateEnum.php
+++ src/Component/Ship/ShipStateEnum.php
@@ -7,6 +7,7 @@ namespace Stu\Component\Ship;
 final class ShipStateEnum
 {
 
-    public const SHIP_STATE_REPAIR = 1;
     public const SHIP_STATE_NONE = 0;
+    public const SHIP_STATE_REPAIR = 1;
+    public const SHIP_STATE_SYSTEM_MAPPING = 2;
 }
