7805e4d55acbc82ec99aac30729cb1fa4eba6243
jrichter <jrichter@intersoft.de>
2021-02-28 21:19:02 +0100

    nbs and lss are required for astro lab

--- src/Component/Ship/System/Type/NearFieldScannerShipSystem.php
+++ src/Component/Ship/System/Type/NearFieldScannerShipSystem.php
@@ -4,20 +4,44 @@ declare(strict_types=1);
 
 namespace Stu\Component\Ship\System\Type;
 
+use Stu\Component\Ship\ShipStateEnum;
 use Stu\Component\Ship\System\ShipSystemModeEnum;
 use Stu\Component\Ship\System\ShipSystemTypeEnum;
 use Stu\Component\Ship\System\ShipSystemTypeInterface;
+use Stu\Module\Ship\Lib\AstroEntryLibInterface;
 use Stu\Orm\Entity\ShipInterface;
 
 final class NearFieldScannerShipSystem extends AbstractShipSystemType implements ShipSystemTypeInterface
 {
+    private AstroEntryLibInterface $astroEntryLib;
+
+    public function __construct(
+        AstroEntryLibInterface $astroEntryLib
+    ) {
+        $this->astroEntryLib = $astroEntryLib;
+    }
+
     public function activate(ShipInterface $ship): void
     {
         $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_NBS)->setMode(ShipSystemModeEnum::MODE_ON);
     }
-    
+
     public function deactivate(ShipInterface $ship): void
     {
         $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_NBS)->setMode(ShipSystemModeEnum::MODE_OFF);
+        $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_ASTRO_LABORATORY)->setMode(ShipSystemModeEnum::MODE_OFF);
+
+        if ($ship->getState() === ShipStateEnum::SHIP_STATE_SYSTEM_MAPPING) {
+            $this->astroEntryLib->cancelAstroFinalizing($ship);
+        }
+    }
+
+    public function handleDestruction(ShipInterface $ship): void
+    {
+        $ship->getShipSystem(ShipSystemTypeEnum::SYSTEM_ASTRO_LABORATORY)->setMode(ShipSystemModeEnum::MODE_OFF);
+
+        if ($ship->getState() === ShipStateEnum::SHIP_STATE_SYSTEM_MAPPING) {
+            $this->astroEntryLib->cancelAstroFinalizing($ship);
+        }
     }
 }
