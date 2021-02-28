2319d256040b62337f671f2adfc5e42d013335b6
Huxinator <52821266+Huxinator@users.noreply.github.com>
2021-02-28 09:39:24 +0100

    Revert "new ship state"
    This reverts commit 347022ade3fed8a8b2387baaf2278229a71436e5.

--- src/Component/Ship/ShipStateEnum.php
+++ src/Component/Ship/ShipStateEnum.php
@@ -7,7 +7,6 @@ namespace Stu\Component\Ship;
 final class ShipStateEnum
 {
 
-    public const SHIP_STATE_NONE = 0;
     public const SHIP_STATE_REPAIR = 1;
-    public const SHIP_STATE_SYSTEM_MAPPING = 2;
+    public const SHIP_STATE_NONE = 0;
 }
