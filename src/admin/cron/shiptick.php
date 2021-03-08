e479a58aef3f55f03918697e995478c454556a0a
jrichter <jrichter@intersoft.de>
2021-03-08 20:36:03 +0100

    divers

--- src/admin/cron/shiptick.php
+++ src/admin/cron/shiptick.php
@@ -1,7 +1,11 @@
 <?php
 
 use Doctrine\ORM\EntityManagerInterface;
+use Noodlehaus\ConfigInterface;
+
+use Stu\Component\Game\GameEnum;
 use Stu\Module\Tick\Ship\ShipTickManagerInterface;
+use Stu\Module\Message\Lib\PrivateMessageSenderInterface;
 
 require_once __DIR__ . '/../../Config/Bootstrap.php';
 
@@ -9,6 +13,24 @@ $db = $container->get(EntityManagerInterface::class);
 
 $db->beginTransaction();
 
-$container->get(ShipTickManagerInterface::class)->work();
+try {
+    $container->get(ShipTickManagerInterface::class)->work();
+} catch (Throwable $t) {
+    $admins = $container->get(ConfigInterface::class)->get('game.admins');
+
+    $privateMessageSender = $container->get(PrivateMessageSenderInterface::class);
+
+    foreach ($admins as $adminId) {
+        $privateMessageSender->send(
+            GameEnum::USER_NOONE,
+            $adminId,
+            _('[b][color=FF2626]Der Schiffstick-CRON ist fehlgeschlagen, bitte manuell den Grund prüfen![/color][/b]') + "\n"
+                + _('Fehlermeldung:') + "\n"
+                + $t->getMessage()
+        );
+    }
+
+    throw $t;
+}
 
 $db->commit();
