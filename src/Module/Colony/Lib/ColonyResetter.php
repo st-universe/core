d6a70bcb90e9d4dcf3d507aca51db48759748d3a
jrichter <jrichter@intersoft.de>
2021-05-14 22:03:14 +0200

    clear blockers and defenders if colony is resettet

--- src/Module/Colony/Lib/ColonyResetter.php
+++ src/Module/Colony/Lib/ColonyResetter.php
@@ -5,11 +5,15 @@ declare(strict_types=1);
 namespace Stu\Module\Colony\Lib;
 
 use Stu\Component\Game\GameEnum;
+use Stu\Module\Message\Lib\PrivateMessageFolderSpecialEnum;
+use Stu\Module\Message\Lib\PrivateMessageSenderInterface;
 use Stu\Orm\Entity\ColonyInterface;
+use Stu\Orm\Entity\FleetInterface;
 use Stu\Orm\Repository\ColonyRepositoryInterface;
 use Stu\Orm\Repository\ColonyShipQueueRepositoryInterface;
 use Stu\Orm\Repository\ColonyStorageRepositoryInterface;
 use Stu\Orm\Repository\ColonyTerraformingRepositoryInterface;
+use Stu\Orm\Repository\FleetRepositoryInterface;
 use Stu\Orm\Repository\PlanetFieldRepositoryInterface;
 use Stu\Orm\Repository\UserRepositoryInterface;
 
@@ -29,6 +33,10 @@ final class ColonyResetter implements ColonyResetterInterface
 
     private PlanetFieldRepositoryInterface $planetFieldRepository;
 
+    private FleetRepositoryInterface $fleetRepository;
+
+    private PrivateMessageSenderInterface $privateMessageSender;
+
     public function __construct(
         ColonyRepositoryInterface $colonyRepository,
         UserRepositoryInterface $userRepository,
@@ -36,7 +44,9 @@ final class ColonyResetter implements ColonyResetterInterface
         ColonyStorageRepositoryInterface $colonyStorageRepository,
         ColonyTerraformingRepositoryInterface $colonyTerraformingRepository,
         ColonyShipQueueRepositoryInterface $colonyShipQueueRepository,
-        PlanetFieldRepositoryInterface $planetFieldRepository
+        PlanetFieldRepositoryInterface $planetFieldRepository,
+        FleetRepositoryInterface $fleetRepository,
+        PrivateMessageSenderInterface $privateMessageSender
     ) {
         $this->colonyRepository = $colonyRepository;
         $this->userRepository = $userRepository;
@@ -45,12 +55,17 @@ final class ColonyResetter implements ColonyResetterInterface
         $this->colonyTerraformingRepository = $colonyTerraformingRepository;
         $this->colonyShipQueueRepository = $colonyShipQueueRepository;
         $this->planetFieldRepository = $planetFieldRepository;
+        $this->fleetRepository = $fleetRepository;
+        $this->privateMessageSender = $privateMessageSender;
     }
 
     public function reset(
         ColonyInterface $colony
     ): void {
         $this->colonyLibFactory->createColonySurface($colony)->updateSurface();
+
+        $this->resetBlockers($colony);
+        $this->resetDefenders($colony);
 
         $colony->setEps(0)
             ->setMaxEps(0)
@@ -73,5 +88,46 @@ final class ColonyResetter implements ColonyResetterInterface
 
         $this->colonyShipQueueRepository->truncateByColony($colony);
         $this->planetFieldRepository->truncateByColony($colony);
+    }
+
+    private function resetBlockers(ColonyInterface $colony): void
+    {
+        foreach ($colony->getBlockers() as $blockerFleet) {
+            $this->sendMessage($colony, $blockerFleet, false);
+            $blockerFleet->setBlockedColony(null);
+            $this->fleetRepository->save($blockerFleet);
+        }
+        $colony->getBlockers()->clear();
+    }
+
+    private function resetDefenders(ColonyInterface $colony): void
+    {
+        foreach ($colony->getDefenders() as $defenderFleet) {
+            $this->sendMessage($colony, $defenderFleet, true);
+            $defenderFleet->setDefendedColony(null);
+            $this->fleetRepository->save($defenderFleet);
+        }
+        $colony->getDefenders()->clear();
+    }
+
+    private function sendMessage(ColonyInterface $colony, FleetInterface $fleet, bool $isDefending): void
+    {
+        $txt = sprintf(
+            'Der Spieler %s hat die Kolonie %s in Sektor %d|%d (%s System) verlassen. Deine Flotte %s hat die %s beendet.',
+            $colony->getUser()->getName(),
+            $colony->getName(),
+            $colony->getSx(),
+            $colony->getSy(),
+            $colony->getSystem()->getName(),
+            $fleet->getName(),
+            $isDefending ? 'Verteidigung' : 'Blockade'
+        );
+
+        $this->privateMessageSender->send(
+            GameEnum::USER_NOONE,
+            (int) $fleet->getUserId(),
+            $txt,
+            PrivateMessageFolderSpecialEnum::PM_SPECIAL_SHIP
+        );
     }
 }
