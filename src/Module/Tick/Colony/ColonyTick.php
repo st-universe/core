553b7fb5fa1d35d3eb3524c221e018e76a49cf2c
Huxinator <nicotintel@gmail.com>
2021-05-29 20:34:54 +0200

    i dont like gagh

--- src/Module/Tick/Colony/ColonyTick.php
+++ src/Module/Tick/Colony/ColonyTick.php
@@ -27,7 +27,7 @@ use Stu\Orm\Repository\ShipRumpUserRepositoryInterface;
 
 final class ColonyTick implements ColonyTickInterface
 {
-    public const PEOPLE_FOOD = 7;
+    public const PEOPLE_FOOD = 0;
 
     private CommodityRepositoryInterface $commodityRepository;
 
