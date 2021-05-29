446b4035610be40c26d715f3b81e240c7035e2d4
jrichter <jrichter@intersoft.de>
2021-05-29 20:16:47 +0200

    add missing comma

--- src/Orm/Entity/FlightSignature.php
+++ src/Orm/Entity/FlightSignature.php
@@ -11,7 +11,7 @@ namespace Stu\Orm\Entity;
  *     indexes={
  *         @Index(name="flight_sig_user_idx", columns={"user_id"}),
  *         @Index(name="flight_sig_map_idx", columns={"map_id"}),
- *         @Index(name="flight_sig_starsystem_map_idx", columns={"starsystem_map_id"})
+ *         @Index(name="flight_sig_starsystem_map_idx", columns={"starsystem_map_id"}),
  *         @Index(name="flight_sig_sensor_result_idx", columns={"from_direction","to_direction","time"})
  *     }
  * )
