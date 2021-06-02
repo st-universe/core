1b69ad211691fedbfc9cf6f30e5d326ea7b64453
jrichter <jrichter@intersoft.de>
2021-06-02 10:56:43 +0200

    more PMs for ship repair and cancellation

--- src/Module/Colony/Action/RepairShip/RepairShip.php
+++ src/Module/Colony/Action/RepairShip/RepairShip.php
@@ -10,6 +10,8 @@ use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Colony\Lib\ColonyLoaderInterface;
 use Stu\Module\Colony\View\ShowShipRepair\ShowShipRepair;
+use Stu\Module\Message\Lib\PrivateMessageFolderSpecialEnum;
+use Stu\Module\Message\Lib\PrivateMessageSenderInterface;
 use Stu\Orm\Entity\ShipInterface;
 use Stu\Orm\Repository\ColonyShipRepairRepositoryInterface;
 use Stu\Orm\Repository\PlanetFieldRepositoryInterface;
@@ -30,18 +32,22 @@ final class RepairShip implements ActionControllerInterface
 
     private ShipRepositoryInterface $shipRepository;
 
+    private PrivateMessageSenderInterface $privateMessageSender;
+
     public function __construct(
         ColonyLoaderInterface $colonyLoader,
         ColonyShipRepairRepositoryInterface $colonyShipRepairRepository,
         ShipRumpBuildingFunctionRepositoryInterface $shipRumpBuildingFunctionRepository,
         PlanetFieldRepositoryInterface $planetFieldRepository,
-        ShipRepositoryInterface $shipRepository
+        ShipRepositoryInterface $shipRepository,
+        PrivateMessageSenderInterface $privateMessageSender
     ) {
         $this->colonyLoader = $colonyLoader;
         $this->colonyShipRepairRepository = $colonyShipRepairRepository;
         $this->shipRumpBuildingFunctionRepository = $shipRumpBuildingFunctionRepository;
         $this->planetFieldRepository = $planetFieldRepository;
         $this->shipRepository = $shipRepository;
+        $this->privateMessageSender = $privateMessageSender;
     }
 
     public function handle(GameControllerInterface $game): void
@@ -57,7 +63,7 @@ final class RepairShip implements ActionControllerInterface
 
         $field = $this->planetFieldRepository->getByColonyAndFieldId(
             $colony->getId(),
-            (int)request::indInt('fid'),
+            (int) request::indInt('fid'),
         );
 
         if ($field === null) {
@@ -71,7 +77,7 @@ final class RepairShip implements ActionControllerInterface
             /**
              * @var ShipInterface $ship
              */
-            foreach ($fleet['ships'] as $ship_id => $ship) {
+            foreach ($fleet['ships'] as $ship) {
                 if (!$ship->canBeRepaired() || $ship->getState() == ShipStateEnum::SHIP_STATE_REPAIR) {
                     continue;
                 }
@@ -119,6 +125,22 @@ final class RepairShip implements ActionControllerInterface
 
         $ticks = $ship->getRepairDuration();
         $game->addInformationf(_('Das Schiff wird repariert. Fertigstellung in %d Runden'), $ticks);
+
+        if ($ship->getUserId() != $userId) {
+            $this->privateMessageSender->send(
+                $userId,
+                $ship->getUserId(),
+                sprintf(
+                    "Die %s wird in Sektor %s bei der Kolonie %s des Spielers %s repariert. Fertigstellung in %d Runden.",
+                    $ship->getName(),
+                    $ship->getSectorString(),
+                    $colony->getName(),
+                    $colony->getUser()->getName(),
+                    $ticks
+                ),
+                PrivateMessageFolderSpecialEnum::PM_SPECIAL_SHIP
+            );
+        }
     }
 
     public function performSessionCheck(): bool
