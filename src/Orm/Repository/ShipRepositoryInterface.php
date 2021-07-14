87662e81d2b15500c1a535be1b4f59641e47f0fc
jrichter <jrichter@intersoft.de>
2021-07-14 21:35:28 +0200

    new method

--- src/Orm/Repository/ShipRepositoryInterface.php
+++ src/Orm/Repository/ShipRepositoryInterface.php
@@ -98,6 +98,8 @@ interface ShipRepositoryInterface extends ObjectRepository
 
     public function getSensorResultInnerSystem(int $systemId, int $sx, int $sy, int $sensorRange, bool $doSubspace, $ignoreId): iterable;
 
+    public function getSensorResultInnerSystemNew(int $systemId, int $sx, int $sy, int $sensorRange, bool $doSubspace, $ignoreId): iterable;
+
     public function getSensorResultOuterSystem(int $cx, int $cy, int $sensorRange, bool $doSubspace, $ignoreId): iterable;
 
     //TODO remove, if not used
