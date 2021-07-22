a627080c382dea08f1ad4c83f20bfc4b7a03a146
jrichter <jrichter@intersoft.de>
2021-07-22 18:01:12 +0200

    remove log output

--- src/Module/Ship/Lib/ShipCreator.php
+++ src/Module/Ship/Lib/ShipCreator.php
@@ -77,11 +77,7 @@ final class ShipCreator implements ShipCreatorInterface
 
     public function createBy(int $userId, int $shipRumpId, int $shipBuildplanId, ?ColonyInterface $colony = null): ShipInterface
     {
-        if ($userId == 126) {
-            $this->loggerUtil->init('stu', LoggerEnum::LEVEL_ERROR);
-        } else {
-            $this->loggerUtil->init();
-        }
+        $this->loggerUtil->init();
 
         $ship = $this->shipRepository->prototype();
         $ship->setUser($this->userRepository->find($userId));
