fbc6c8684335177b6bf53ce622cf8c4f044332fd
jrichter <jrichter@intersoft.de>
2021-07-28 21:09:36 +0200

    build construction

--- src/Component/Ship/ShipRumpEnum.php
+++ src/Component/Ship/ShipRumpEnum.php
@@ -6,7 +6,7 @@ namespace Stu\Component\Ship;
 
 final class ShipRumpEnum
 {
-    //rump categories
+    // rump categories
     public const SHIP_CATEGORY_DEBRISFIELD = 7;
     public const SHIP_CATEGORY_TRUMFIELD = 8;
     public const SHIP_CATEGORY_ESCAPE_PODS = 9;
@@ -14,7 +14,13 @@ final class ShipRumpEnum
     public const SHIP_CATEGORY_CONSTRUCTION = 11;
     public const SHIP_CATEGORY_STATION = 12;
 
-    //rump offsets
+    // rump roles TODO
+    public const SHIP_ROLE_SENSOR = 12;
+    public const SHIP_ROLE_DEPOT_SMALL = 12;
+    public const SHIP_ROLE_DEPOT_LARGE = 12;
+    public const SHIP_ROLE_BASE_LARGE = 12;
+
+    // rump offsets
     public const SHIP_RUMP_BASE_ID_ESCAPE_PODS = 100;
-    public const SHIP_RUMP_BASE_ID_WORKBEE = 160;
+    public const SHIP_RUMP_BASE_ID_CONSTRUCTION = 10000;
 }
