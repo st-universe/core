c8c8d9ed89c1f1dde41e43f8875f7b26d3ff2664
jrichter <jrichter@intersoft.de>
2021-07-29 20:48:26 +0200

    logging

--- src/Module/Station/View/ShowStationCosts/ShowStationCosts.php
+++ src/Module/Station/View/ShowStationCosts/ShowStationCosts.php
@@ -8,6 +8,8 @@ use request;
 
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Orm\Repository\ShipBuildplanRepositoryInterface;
 
 final class ShowStationCosts implements ViewControllerInterface
@@ -16,14 +18,22 @@ final class ShowStationCosts implements ViewControllerInterface
 
     private ShipBuildplanRepositoryInterface $shipBuildplanRepository;
 
+    private LoggerUtilInterface $loggerUtil;
+
     public function __construct(
-        ShipBuildplanRepositoryInterface $shipBuildplanRepository
+        ShipBuildplanRepositoryInterface $shipBuildplanRepository,
+        LoggerUtilInterface $loggerUtil
     ) {
         $this->shipBuildplanRepository = $shipBuildplanRepository;
+        $this->loggerUtil = $loggerUtil;
     }
 
     public function handle(GameControllerInterface $game): void
     {
+        $this->loggerUtil->init('stu', LoggerEnum::LEVEL_ERROR);
+
+        $this->loggerUtil->log('A');
+
         $game->setTemplateVar('ERROR', true);
 
         $game->setPageTitle(_('Baukosten'));
@@ -35,17 +45,27 @@ final class ShowStationCosts implements ViewControllerInterface
         $wantedPlanId = request::getIntFatal('pid');
         $plans = $this->shipBuildplanRepository->getStationBuildplansByUser($userId);
 
+        $this->loggerUtil->log('B');
+
         $plan = null;
         foreach ($plans as $p) {
+
+            $this->loggerUtil->log('C');
             if ($p->getId() === $wantedPlanId) {
+                $this->loggerUtil->log('D');
                 $plan = $p;
                 break;
             }
         }
 
+        $this->loggerUtil->log('E');
+
+
         if ($plan === null) {
             return;
         }
+
+        $this->loggerUtil->log('F');
 
         $game->setTemplateVar('PLAN', $plan);
         $game->setTemplateVar('ERROR', false);
