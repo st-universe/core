52419dc822d4f9b1f296372282f9911c35695f0b
jrichter <jrichter@intersoft.de>
2021-07-31 00:06:29 +0200

    repair ship.php

--- src/Module/Station/services.php
+++ src/Module/Station/services.php
@@ -6,16 +6,13 @@ namespace Stu\Module\Station;
 
 use Stu\Module\Control\GameController;
 use Stu\Module\Ship\View\ShowShip\ShowShip;
-use Stu\Module\Station\Action\BuildStation\BuildStation;
 use Stu\Module\Station\View\Overview\Overview;
 use Stu\Module\Station\View\ShowStationCosts\ShowStationCosts;
 
 use function DI\autowire;
 
 return [
-    'STATION_ACTIONS' => [
-        BuildStation::ACTION_IDENTIFIER => autowire(BuildStation::class),
-    ],
+    'STATION_ACTIONS' => [],
     'STATION_VIEWS' => [
         GameController::DEFAULT_VIEW => autowire(Overview::class),
         Overview::VIEW_IDENTIFIER => autowire(Overview::class),
