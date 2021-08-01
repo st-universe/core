2a51631075e8fb246f4ab7d5bc6f0eb518ecc1ef
jrichter <jrichter@intersoft.de>
2021-08-01 13:37:18 +0200

    bugfix

--- src/Component/Queue/Consumer/DelayedJobConsumer.php
+++ src/Component/Queue/Consumer/DelayedJobConsumer.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\EntityManagerInterface;
 use Stu\Component\Process\BuildingJobFinishProcessInterface;
 use Stu\Component\Process\TerraformingJobFinishProcessInterface;
 use Stu\Component\Queue\Driver\DelayedJobDriverInterface;
-use Stu\Component\Queue\Message\MessageTransformatorInterface;
 use Stu\Component\Queue\Message\TransformableMessageInterface;
 use Stu\Component\Queue\Message\Type\BuildingJobProcessMessageInterface;
 use Stu\Component\Queue\Message\Type\MessageTypeEnum;
@@ -20,8 +19,6 @@ final class DelayedJobConsumer implements DelayedJobConsumerInterface
 {
     private DelayedJobDriverInterface $delayedJobDriver;
 
-    private MessageTransformatorInterface $messageTransformator;
-
     private BuildingJobFinishProcessInterface $buildingJobFinishProcess;
 
     private EntityManagerInterface $entityManager;
@@ -32,24 +29,23 @@ final class DelayedJobConsumer implements DelayedJobConsumerInterface
 
     public function __construct(
         DelayedJobDriverInterface $delayedJobDriver,
-        MessageTransformatorInterface $messageTransformator,
         BuildingJobFinishProcessInterface $buildingJobFinishProcess,
         EntityManagerInterface $entityManager,
         DelayedJobProcessorInterface $delayedJobProcessor,
         TerraformingJobFinishProcessInterface $terraformingJobFinishProcess
     ) {
         $this->delayedJobDriver = $delayedJobDriver;
-        $this->messageTransformator = $messageTransformator;
         $this->buildingJobFinishProcess = $buildingJobFinishProcess;
         $this->entityManager = $entityManager;
         $this->delayedJobProcessor = $delayedJobProcessor;
         $this->terraformingJobFinishProcess = $terraformingJobFinishProcess;
     }
 
-    public function consume(): void {
+    public function consume(): void
+    {
         $this->delayedJobProcessor->registerProcessor(
             MessageTypeEnum::BUILDING_JOB,
-            function(BuildingJobProcessMessageInterface $message): void {
+            function (BuildingJobProcessMessageInterface $message): void {
                 $this->entityManager->beginTransaction();
 
                 $this->buildingJobFinishProcess->work($message);
@@ -59,7 +55,7 @@ final class DelayedJobConsumer implements DelayedJobConsumerInterface
         );
         $this->delayedJobProcessor->registerProcessor(
             MessageTypeEnum::TERRAFORMING_JOB,
-            function(TerraformingJobProcessMessage $message): void {
+            function (TerraformingJobProcessMessage $message): void {
                 $this->entityManager->beginTransaction();
 
                 $this->terraformingJobFinishProcess->work($message);
@@ -71,7 +67,7 @@ final class DelayedJobConsumer implements DelayedJobConsumerInterface
         $this->delayedJobDriver->consume(
             'delayed_processes',
             'delayed_process_consumer_' . getmypid(),
-            function(TransformableMessageInterface $message): void {
+            function (TransformableMessageInterface $message): void {
                 $this->delayedJobProcessor->process($message);
             },
             QueueRouteEnum::DELAYED_JOB
