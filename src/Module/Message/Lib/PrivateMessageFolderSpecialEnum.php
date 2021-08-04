5eca9d4279564d1fa2259d44f6aa344980f5d572
jrichter <jrichter@intersoft.de>
2021-08-04 22:27:14 +0200

    new pm category for stations

--- src/Module/Message/Lib/PrivateMessageFolderSpecialEnum.php
+++ src/Module/Message/Lib/PrivateMessageFolderSpecialEnum.php
@@ -12,10 +12,12 @@ final class PrivateMessageFolderSpecialEnum
     public const PM_SPECIAL_TRADE = 4;
     public const PM_SPECIAL_SYSTEM = 5;
     public const PM_SPECIAL_PMOUT = 6;
+    public const PM_SPECIAL_STATION = 7;
 
     public const DEFAULT_CATEGORIES = [
         self::PM_SPECIAL_MAIN => 'Persönlich',
         self::PM_SPECIAL_SHIP => 'Schiffe',
+        self::PM_SPECIAL_SHIP => 'Stationen',
         self::PM_SPECIAL_COLONY => 'Kolonien',
         self::PM_SPECIAL_TRADE => 'Handel',
         self::PM_SPECIAL_SYSTEM => 'Systemmeldungen',
