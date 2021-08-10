554f85f2dcd69cb035efe8bbac4ae40a2a7d576a
jrichter <jrichter@intersoft.de>
2021-08-10 21:10:40 +0200

    more style

--- src/Module/Starmap/Lib/ExploreableStarMap.php
+++ src/Module/Starmap/Lib/ExploreableStarMap.php
@@ -97,7 +97,7 @@ class ExploreableStarMap implements ExploreableStarMapInterface
             $type = $this->getFieldId();
         }
 
-        $style = "background-image: url('assets/map/" . $type . ".png');";
+        $style = "background-image: url('assets/map/" . $type . ".png'); width: 30px; height: 30px;";
         $style .= $borderStyle;
         return $style;
     }
