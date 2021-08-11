5ded949ad4c4fe80771ef7da6c6dde6d34a1389d
jrichter <jrichter@intersoft.de>
2021-08-11 22:38:42 +0200

    move popup

--- src/Public/static/js/ally.js
+++ src/Public/static/js/ally.js
@@ -35,6 +35,6 @@ function showMemberRumpInfo(obj, userid, rumpid) {
 	closeAjaxWindow();
 
 	var pos = findObject(obj);
-	openWindowPosition('elt', 1, 700, pos[0], pos[1]);
+	openWindowPosition('elt', 1, 700, pos[0] - 400, pos[1]);
 	ajax_update('elt', 'alliance.php?SHOW_MEMBER_RUMP_INFO=1&uid=' + userid + '&rid=' + rumpid);
 }
