be7e1251bea370eda25b18f1c3c36c24d7d08fde
jrichter <jrichter@intersoft.de>
2021-08-16 10:06:21 +0200

    deactivate faulty code

--- src/Module/Maintenance/MapCycle.php
+++ src/Module/Maintenance/MapCycle.php
@@ -36,6 +36,8 @@ final class MapCycle implements MaintenanceHandlerInterface
         $fieldcount = $this->mapRepository->count([]);
         $list = $this->userRepository->getByMappingType(MapEnum::MAPTYPE_INSERT);
         foreach ($list as $user) {
+
+            // if user has mapped everything
             if ($this->userMapRepository->getAmountByUser($user->getId()) >= $fieldcount) {
                 $this->cycle($user);
             }
@@ -50,17 +52,16 @@ final class MapCycle implements MaintenanceHandlerInterface
 
         $userId = $user->getId();
 
-        $connection = $this->entityManager->getConnection();
-
-        $result = $connection->query(
+        /**   $result = $connection->query(
+         $connection = $this->entityManager->getConnection();
             sprintf(
                 'SELECT cx,cy,id FROM stu_map WHERE id NOT IN (SELECT map_id FROM stu_user_map WHERE user_id = %d)',
                 $user->getId()
             )
         );
-
+         */
         $this->userMapRepository->truncateByUser($userId);
-
+        /** 
         while ($data = $connection->fetchAssoc($result)) {
             $connection->query(
                 sprintf(
@@ -72,6 +73,6 @@ final class MapCycle implements MaintenanceHandlerInterface
                 )
             );
         }
-
+         */
     }
 }
