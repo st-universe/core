6e76fd3764d5d6662c22b2507cdde025761fe1db
jrichter <jrichter@intersoft.de>
2021-08-16 19:18:09 +0200

    station ship list and management

---
+++ src/Module/Station/View/ShowStationShiplist/ShowStationShiplist.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Station\View\ShowStationShiplist;
+
+use Stu\Component\Station\StationUtilityInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Control\ViewControllerInterface;
+use Stu\Module\Ship\Lib\ShipLoaderInterface;
+use Stu\Module\Tal\OrbitShipItem;
+use Stu\Module\Tal\OrbitShipItemInterface;
+use Stu\Orm\Entity\ShipInterface;
+
+final class ShowStationShiplist implements ViewControllerInterface
+{
+    public const VIEW_IDENTIFIER = 'SHOW_STATION_SHIPLIST';
+
+    private ShipLoaderInterface $shipLoader;
+
+    private ShowStationShiplistRequestInterface $showStationShiplistRequest;
+
+    private StationUtilityInterface $stationUtility;
+
+    public function __construct(
+        ShipLoaderInterface $shipLoader,
+        ShowStationShiplistRequestInterface $showStationShiplistRequest,
+        StationUtilityInterface $stationUtility
+    ) {
+        $this->shipLoader = $shipLoader;
+        $this->showStationShiplistRequest = $showStationShiplistRequest;
+        $this->stationUtility = $stationUtility;
+    }
+
+    public function handle(GameControllerInterface $game): void
+    {
+        $userId = $game->getUser()->getId();
+
+        $station = $this->shipLoader->getByIdAndUser(
+            $this->showStationShiplistRequest->getStationId(),
+            $userId
+        );
+
+        $shipList = [];
+
+        foreach ($this->stationUtility->getManageableShipList($station) as $entry) {
+            $entry['ships'] = array_map(
+                function (ShipInterface $ship): OrbitShipItemInterface {
+                    return new OrbitShipItem($ship);
+                },
+                $entry['ships']
+            );
+            $shipList[] = $entry;
+        }
+
+        $game->setPageTitle(_('Schiffe im Bereich'));
+        $game->setTemplateFile('html/ajaxwindow.xhtml');
+        $game->setMacro('html/stationmacros.xhtml/shiplist');
+        $game->setTemplateVar('STATION', $station);
+        $game->setTemplateVar('SHIP_LIST', $shipList);
+    }
+}
--- src/Module/Station/services.php
+++ src/Module/Station/services.php
@@ -9,12 +9,20 @@ use Stu\Module\Ship\View\ShowShip\ShowShip;
 use Stu\Module\Station\Action\BuildStation\BuildStation;
 use Stu\Module\Station\View\Overview\Overview;
 use Stu\Module\Station\View\ShowSensorScan\ShowSensorScan;
+use Stu\Module\Station\View\ShowShipManagement\ShowShipManagement;
+use Stu\Module\Station\View\ShowShipManagement\ShowShipManagementRequest;
+use Stu\Module\Station\View\ShowShipManagement\ShowShipManagementRequestInterface;
 use Stu\Module\Station\View\ShowStationCosts\ShowStationCosts;
 use Stu\Module\Station\View\ShowStationInfo\ShowStationInfo;
+use Stu\Module\Station\View\ShowStationShiplist\ShowStationShiplist;
+use Stu\Module\Station\View\ShowStationShiplist\ShowStationShiplistRequest;
+use Stu\Module\Station\View\ShowStationShiplist\ShowStationShiplistRequestInterface;
 
 use function DI\autowire;
 
 return [
+    ShowShipManagementRequestInterface::class => autowire(ShowShipManagementRequest::class),
+    ShowStationShiplistRequestInterface::class => autowire(ShowStationShiplistRequest::class),
     'STATION_ACTIONS' => [
         BuildStation::ACTION_IDENTIFIER => autowire(BuildStation::class)
     ],
@@ -24,6 +32,8 @@ return [
         ShowShip::VIEW_IDENTIFIER => autowire(ShowShip::class),
         ShowStationCosts::VIEW_IDENTIFIER => autowire(ShowStationCosts::class),
         ShowSensorScan::VIEW_IDENTIFIER => autowire(ShowSensorScan::class),
-        ShowStationInfo::VIEW_IDENTIFIER => autowire(ShowStationInfo::class)
+        ShowStationInfo::VIEW_IDENTIFIER => autowire(ShowStationInfo::class),
+        ShowShipManagement::VIEW_IDENTIFIER => autowire(ShowShipManagement::class),
+        ShowStationShiplist::VIEW_IDENTIFIER => autowire(ShowStationShiplist::class)
     ],
 ];
