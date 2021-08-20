8c2b226238f1157bebfe5f81676277764ab3769d
jrichter <jrichter@intersoft.de>
2021-08-20 20:52:03 +0200

    new history category

--- src/Module/History/Lib/EntryCreatorInterface.php
+++ src/Module/History/Lib/EntryCreatorInterface.php
@@ -8,6 +8,11 @@ interface EntryCreatorInterface
 {
     public function addShipEntry(string $text, int $userId = GameEnum::USER_NOONE): void;
 
+    public function addStationEntry(
+        string $text,
+        int $userId = GameEnum::USER_NOONE
+    ): void;
+
     public function addColonyEntry(
         string $text,
         int $userId = GameEnum::USER_NOONE
