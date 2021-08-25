2aea1ff3a69c0a999537e58586f69b4ea1e8e714
jrichter <jrichter@intersoft.de>
2021-08-25 17:51:39 +0200

    show correct station crew

--- src/Module/Station/View/ShowStationInfo/ShowStationInfo.php
+++ src/Module/Station/View/ShowStationInfo/ShowStationInfo.php
@@ -6,29 +6,29 @@ namespace Stu\Module\Station\View\ShowStationInfo;
 
 use request;
 use Stu\Component\Station\StationEnum;
+use Stu\Component\Station\StationUtilityInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 use Stu\Module\Logging\LoggerUtilInterface;
-use Stu\Orm\Repository\ShipRumpRepositoryInterface;
 use Stu\Orm\Repository\ShipRumpUserRepositoryInterface;
 
 final class ShowStationInfo implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_STATION_INFO';
 
-    private ShipRumpUserRepositoryInterface $shipRumpUserRepository;
+    private StationUtilityInterface $stationUtility;
 
-    private ShipRumpRepositoryInterface $shipRumpRepository;
+    private ShipRumpUserRepositoryInterface $shipRumpUserRepository;
 
     private LoggerUtilInterface $loggerUtil;
 
     public function __construct(
+        StationUtilityInterface $stationUtility,
         ShipRumpUserRepositoryInterface $shipRumpUserRepository,
-        ShipRumpRepositoryInterface $shipRumpRepository,
         LoggerUtilInterface $loggerUtil
     ) {
+        $this->stationUtility = $stationUtility;
         $this->shipRumpUserRepository = $shipRumpUserRepository;
-        $this->shipRumpRepository = $shipRumpRepository;
         $this->loggerUtil = $loggerUtil;
     }
 
@@ -43,15 +43,16 @@ final class ShowStationInfo implements ViewControllerInterface
         $game->setMacro('html/stationmacros.xhtml/stationinfo');
 
         $userId = $game->getUser()->getId();
+        $planId = (int)request::getIntFatal('pid');
 
-        $rumpId = (int)request::getIntFatal('rid');
+        $plan = $this->stationUtility->getBuidplanIfResearchedByUser($planId, $userId);
+        $rump = $plan->getRump();
 
-        if (!$this->shipRumpUserRepository->isAvailableForUser($rumpId, $userId)) {
+        if (!$this->shipRumpUserRepository->isAvailableForUser($rump->getId(), $userId)) {
             return;
         }
 
-        $rump = $this->shipRumpRepository->find($rumpId);
-        $game->setTemplateVar('RUMP', $rump);
+        $game->setTemplateVar('PLAN', $plan);
 
         $limit = StationEnum::BUILDABLE_LIMITS_PER_ROLE[$rump->getRoleId()];
         $game->setTemplateVar('LIMIT', $limit === PHP_INT_MAX ? 'unbegrenzt' : $limit);
