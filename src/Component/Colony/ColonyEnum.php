f558c74fa8e11ad2dfbcce11ce890fae1d690d95
jrichter <jrichter@intersoft.de>
2021-08-25 20:46:49 +0200

    waste removal feature

--- src/Component/Colony/ColonyEnum.php
+++ src/Component/Colony/ColonyEnum.php
@@ -24,18 +24,19 @@ final class ColonyEnum
 
     public const BUILDMENU_SCROLLOFFSET = 6;
 
-    public const MENU_BUILDPLANS = 9;
-    public const MENU_BUILDINGS = 5;
-    public const MENU_TORPEDOFAB = 11;
     public const MENU_BUILD = 1;
-    public const MENU_OPTION = 3;
-    public const MENU_ACADEMY = 12;
     public const MENU_INFO = 2;
+    public const MENU_OPTION = 3;
     public const MENU_SOCIAL = 4;
-    public const MENU_SHIPYARD = 8;
-    public const MENU_MODULEFAB = 7;
-    public const MENU_FIGHTER_SHIPYARD = 10;
+    public const MENU_BUILDINGS = 5;
     public const MENU_AIRFIELD = 6;
+    public const MENU_MODULEFAB = 7;
+    public const MENU_SHIPYARD = 8;
+    public const MENU_BUILDPLANS = 9;
+    public const MENU_FIGHTER_SHIPYARD = 10;
+    public const MENU_TORPEDOFAB = 11;
+    public const MENU_ACADEMY = 12;
+    public const MENU_WASTE = 13;
 
     // shield stuff
     public const SHIELDS_PER_EPS = 10;
