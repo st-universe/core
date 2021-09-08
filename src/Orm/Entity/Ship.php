2b3c5274511be1b827f9dc25efd0c1c7282541e3
jrichter <jrichter@intersoft.de>
2021-09-08 11:15:39 +0200

    hull color style

--- src/Orm/Entity/Ship.php
+++ src/Orm/Entity/Ship.php
@@ -1752,4 +1752,35 @@ class Ship implements ShipInterface
             ->setSizeModifier(1.6)
             ->render();
     }
+
+    public function getHullColorStyle(): string
+    {
+        return $this->getColorStyle($this->getHuell(), $this->getMaxHuell());
+    }
+
+    private function getColorStyle(int $actual, int $max): string
+    {
+        // full
+        if ($actual == $max) {
+            return 'color: #dddddd;';
+        }
+
+        // less than 100% - green
+        if ($actual / $max > 0.75) {
+            return 'color: #19c100;';
+        }
+
+        // less than 75% - yellow
+        if ($actual / $max > 0.50) {
+            return 'color: #f4e932;';
+        }
+
+        // less than 50% - orange
+        if ($actual / $max > 0.25) {
+            return 'color: #f48b28;';
+        }
+
+        // less than 25% - red
+        return 'color: #ff3c3c;';
+    }
 }
