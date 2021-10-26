663ce62a51ac02b89e2a8864eb4f4acb985a34f2
Janko <jadari@web.de>
2021-10-26 21:52:47 +0200

    repo method

--- src/Orm/Repository/RepairTaskRepository.php
+++ src/Orm/Repository/RepairTaskRepository.php
@@ -49,4 +49,17 @@ final class RepairTaskRepository extends EntityRepository implements RepairTaskR
         $q->setParameter('shipId', $shipId);
         $q->execute();
     }
+
+    public function getFinishedRepairTasks(): array
+    {
+        return $this->getEntityManager()->createQuery(
+            sprintf(
+                'SELECT rt FROM %s rt
+                WHERE rt.finish_time <= :actualTime',
+                RepairTask::class
+            )
+        )->setParameters([
+            'actualTime' => time()
+        ])->getResult();
+    }
 }
