722707a36faa48c2e5e612f3c73070a044f0e029
Huxinator <nicotintel@gmail.com>
2021-12-01 12:16:13 +0100

    border colors

--- src/Module/Starmap/Lib/ExploreableStarMap.php
+++ src/Module/Starmap/Lib/ExploreableStarMap.php
@@ -91,9 +91,9 @@ class ExploreableStarMap implements ExploreableStarMapInterface
         if ($this->mapBorderType === null) {
             if ($this->influence_id === 53) {
                 $borderStyle = 'border: 1px solid #800080';
-            } if ($this->influence_id === 43) {
+            } elseif ($this->influence_id === 43) {
                 $borderStyle = 'border: 1px solid #E6E6E6';
-            } if ($this->influence_id === 145) {
+            } elseif ($this->influence_id === 145) {
                 $borderStyle = 'border: 1px solid #E6E6E6';
             } else {
                 $borderStyle = '';
