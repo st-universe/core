b5209f2e7ea23bb69e997e27bd263071bb050f60
Janko <jadari@web.de>
2021-12-16 21:35:03 +0100

    read config values

--- src/Module/Index/View/Overview/Overview.php
+++ src/Module/Index/View/Overview/Overview.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stu\Module\Index\View\Overview;
 
+use Noodlehaus\ConfigInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 use Stu\Orm\Repository\NewsRepositoryInterface;
@@ -12,10 +13,14 @@ final class Overview implements ViewControllerInterface
 {
     private NewsRepositoryInterface $newsRepository;
 
+    private ConfigInterface $config;
+
     public function __construct(
-        NewsRepositoryInterface $newsRepository
+        NewsRepositoryInterface $newsRepository,
+        ConfigInterface $config
     ) {
         $this->newsRepository = $newsRepository;
+        $this->config = $config;
     }
 
     public function handle(GameControllerInterface $game): void
@@ -24,5 +29,6 @@ final class Overview implements ViewControllerInterface
         $game->setTemplateFile('html/index.xhtml');
 
         $game->setTemplateVar('SYSTEM_NEWS', $this->newsRepository->getRecent(5));
+        $game->setTemplateVar('WIKI', $this->config->get('wiki.base_url'));
     }
 }
