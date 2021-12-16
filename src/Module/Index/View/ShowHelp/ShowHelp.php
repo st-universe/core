cb0fd2ccf8f6fa36c2b893ac78e0c2170600e0f2
Janko <jadari@web.de>
2021-12-16 21:23:07 +0100

    read config values

--- src/Module/Index/View/ShowHelp/ShowHelp.php
+++ src/Module/Index/View/ShowHelp/ShowHelp.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stu\Module\Index\View\ShowHelp;
 
+use Noodlehaus\ConfigInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 
@@ -11,9 +12,20 @@ final class ShowHelp implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_HELP';
 
+    private ConfigInterface $config;
+
+    public function __construct(ConfigInterface $config)
+    {
+        $this->config = $config;
+    }
+
     public function handle(GameControllerInterface $game): void
     {
         $game->setPageTitle(_('Hilfe - Star Trek Universe'));
         $game->setTemplateFile('html/index_help.xhtml');
+
+        $game->setTemplateVar('BOARD', $this->config->get('board.base_url'));
+        $game->setTemplateVar('WIKI', $this->config->get('wiki.base_url'));
+        $game->setTemplateVar('CHAT', $this->config->get('discord.url'));
     }
 }
