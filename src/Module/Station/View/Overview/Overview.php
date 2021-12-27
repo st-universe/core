1995b6ef29a5dcd49652212f44a632327cb9631f
Janko <jadari@web.de>
2021-12-27 18:38:58 +0100

    check for activated uplink

--- src/Module/Station/View/Overview/Overview.php
+++ src/Module/Station/View/Overview/Overview.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Stu\Module\Station\View\Overview;
 
-use Stu\Component\Game\GameEnum;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 use Stu\Orm\Repository\ShipRepositoryInterface;
