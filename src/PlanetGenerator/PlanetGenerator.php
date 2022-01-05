a3773a4773c4adc00e2db0397abd992ef103e3aa
Janko <jadari@web.de>
2022-01-05 09:58:34 +0100

    log id

--- src/PlanetGenerator/PlanetGenerator.php
+++ src/PlanetGenerator/PlanetGenerator.php
@@ -466,6 +466,10 @@ final class PlanetGenerator implements PlanetGeneratorInterface
             throw new Exception('Error loading planetgenerator description file for id ' . $id);
         }
 
+        if (is_int($requireResult)) {
+            throw new Exception('Error loading planetgenerator description file for id ' . $id);
+        }
+
         return $requireResult;
     }
 
