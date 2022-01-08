1b20f18adfdb233b100a66e6077bfc1afb9985f4
Janko <jadari@web.de>
2022-01-08 22:00:23 +0100

    log

--- src/Component/Player/Deletion/Handler/AllianceDeletionHandler.php
+++ src/Component/Player/Deletion/Handler/AllianceDeletionHandler.php
@@ -6,6 +6,8 @@ namespace Stu\Component\Player\Deletion\Handler;
 
 use Stu\Component\Alliance\AllianceEnum;
 use Stu\Module\Alliance\Lib\AllianceActionManagerInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Orm\Entity\UserInterface;
 use Stu\Orm\Repository\AllianceJobRepositoryInterface;
 
@@ -15,25 +17,35 @@ final class AllianceDeletionHandler implements PlayerDeletionHandlerInteface
 
     private AllianceActionManagerInterface $allianceActionManager;
 
+    private LoggerUtilInterface $loggerUtil;
+
     public function __construct(
         AllianceJobRepositoryInterface $allianceJobRepository,
-        AllianceActionManagerInterface $allianceActionManager
+        AllianceActionManagerInterface $allianceActionManager,
+        LoggerUtilInterface $loggerUtil
     ) {
         $this->allianceJobRepository = $allianceJobRepository;
         $this->allianceActionManager = $allianceActionManager;
+        $this->loggerUtil = $loggerUtil;
     }
 
     public function delete(UserInterface $user): void
     {
+        $this->loggerUtil->log('stu', LoggerEnum::LEVEL_ERROR);
+
         foreach ($this->allianceJobRepository->getByUser($user->getId()) as $job) {
+            $this->loggerUtil->log('A');
             if ($job->getType() === AllianceEnum::ALLIANCE_JOBS_FOUNDER) {
+                $this->loggerUtil->log('B');
                 $alliance = $job->getAlliance();
 
                 $successor = $alliance->getSuccessor();
 
                 if ($successor === null) {
+                    $this->loggerUtil->log('C');
                     $this->allianceActionManager->delete($alliance->getId(), false);
                 } else {
+                    $this->loggerUtil->log('D');
                     $successorUserId = $successor->getUserId();
 
                     $this->allianceActionManager->setJobForUser(
@@ -42,7 +54,6 @@ final class AllianceDeletionHandler implements PlayerDeletionHandlerInteface
                         AllianceEnum::ALLIANCE_JOBS_FOUNDER
                     );
                 }
-
             }
 
             $this->allianceJobRepository->delete($job);
