26ceb1b56ff721ce98e59809a7283cb82438ea40
Janko <jadari@web.de>
2022-01-16 21:00:34 +0100

    fix starting colonies query

--- src/Orm/Repository/ColonyRepository.php
+++ src/Orm/Repository/ColonyRepository.php
@@ -63,7 +63,7 @@ final class ColonyRepository extends EntityRepository implements ColonyRepositor
                  WHERE c.user_id = :userId AND c.colonies_classes_id IN (
                     SELECT pt.id FROM %s pt WHERE pt.allow_start = :allowStart
                 ) AND sm.systems_id IN (
-                    SELECT m.systems_id FROM %s m WHERE m.systems_id > 0 AND m.region_id IN (
+                    SELECT m.systems_id FROM %s m WHERE m.systems_id > 0 AND m.admin_region_id IN (
                         SELECT mrs.region_id from %s mrs WHERE mrs.faction_id = :factionId
                     )
                 )',
