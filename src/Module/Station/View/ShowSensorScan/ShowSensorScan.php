609ab8bf305eba7130658c22d872eed5bc40e194
Janko <jadari@web.de>
2022-01-21 14:21:52 +0100

    log on

--- src/Module/Station/View/ShowSensorScan/ShowSensorScan.php
+++ src/Module/Station/View/ShowSensorScan/ShowSensorScan.php
@@ -64,6 +64,10 @@ final class ShowSensorScan implements ViewControllerInterface
     {
         $userId = $game->getUser()->getId();
 
+        if ($userId === 126) {
+            $this->loggerUtil->init('stu', LoggerEnum::LEVEL_ERROR);
+        }
+
         $ship = $this->shipLoader->getByIdAndUser(
             request::indInt('id'),
             $userId,
