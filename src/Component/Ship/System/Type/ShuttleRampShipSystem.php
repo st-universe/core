852f3cdc3e7493d0c559547865ebd2fa1292c65f
Janko <jadari@web.de>
2022-02-09 21:52:53 +0100

    shuttles get destroyed with shuttle ramp

--- src/Component/Ship/System/Type/ShuttleRampShipSystem.php
+++ src/Component/Ship/System/Type/ShuttleRampShipSystem.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Stu\Component\Ship\System\Type;
 
+use Stu\Component\Ship\Storage\ShipStorageManager;
+use Stu\Component\Ship\Storage\ShipStorageManagerInterface;
 use Stu\Component\Ship\System\ShipSystemModeEnum;
 use Stu\Component\Ship\System\ShipSystemTypeInterface;
 use Stu\Orm\Entity\ShipInterface;
 
 final class ShuttleRampShipSystem extends AbstractShipSystemType implements ShipSystemTypeInterface
 {
+    private ShipStorageManagerInterface $shipStorageManager;
+
+    public function __construct(ShipStorageManagerInterface $shipStorageManager)
+    {
+        $this->shipStorageManager = $shipStorageManager;
+    }
     public function activate(ShipInterface $ship): void
     {
         //nothing to do here
@@ -33,5 +41,15 @@ final class ShuttleRampShipSystem extends AbstractShipSystemType implements Ship
     public function getEnergyConsumption(): int
     {
         return 0;
+    }
+
+    public function handleDestruction(ShipInterface $ship): void
+    {
+        // delete shuttles
+        foreach ($ship->getStorage() as $stor) {
+            if ($stor->getCommodity()->isShuttle()) {
+                $this->shipStorageManager->lowerStorage($ship, $stor->getCommodity(), $stor->getAmount());
+            }
+        }
     }
 }
