079220561372b25a753b620526aa0fc88944d856
Janko <jadari@web.de>
2022-02-21 20:52:50 +0100

    try

--- src/Module/Admin/Action/SendMassMail.php
+++ src/Module/Admin/Action/SendMassMail.php
@@ -56,9 +56,9 @@ final class SendMassMail implements ActionControllerInterface
         }
 
         $count = 0;
-        if ($medium === self::MEDIUM_EMAIL) {
+        if ($medium === static::MEDIUM_EMAIL) {
             $count = $this->sendMassEmails($text, $subject);
-        } else if ($medium === self::MEDIUM_PM) {
+        } else if ($medium === static::MEDIUM_PM) {
             $count = $this->sendMassPm($text, $subject);
         }
 
