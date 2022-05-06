0bf4ca9f608e3ec96b91945f8187128ca0bd89ff
Janko <jadari@web.de>
2022-05-06 19:21:43 +0200

    log on

--- src/Module/Index/Action/Login/Login.php
+++ src/Module/Index/Action/Login/Login.php
@@ -8,6 +8,9 @@ use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Lib\LoginException;
 use Stu\Lib\SessionInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilFactoryInterface;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Module\PlayerSetting\Lib\PlayerEnum;
 use Stu\Orm\Repository\UserRepositoryInterface;
 
@@ -22,14 +25,18 @@ final class Login implements ActionControllerInterface
 
     private UserRepositoryInterface $userRepository;
 
+    private LoggerUtilInterface $loggerUtil;
+
     public function __construct(
         LoginRequestInterface $loginRequest,
         SessionInterface $session,
-        UserRepositoryInterface $userRepository
+        UserRepositoryInterface $userRepository,
+        LoggerUtilFactoryInterface $loggerUtilFactory
     ) {
         $this->loginRequest = $loginRequest;
         $this->session = $session;
         $this->userRepository = $userRepository;
+        $this->loggerUtil = $loggerUtilFactory->getLoggerUtil();
     }
 
     /**
@@ -44,13 +51,26 @@ final class Login implements ActionControllerInterface
 
         $user = $this->session->getUser();
 
+        if ($this->loginRequest->getLoginName() === 'smstest') {
+            $this->loggerUtil->init('login', LoggerEnum::LEVEL_ERROR);
+        }
+
+        if ($user === null) {
+            $this->loggerUtil->log('user is null');
+        } else {
+            $this->loggerUtil->log('user is not null');
+        }
+
         // check for sms verification
         if ($user !== null && $user->getActive() === PlayerEnum::USER_SMS_VERIFICATION) {
+            $this->loggerUtil->log('A');
             $smsCode = $this->loginRequest->getSmsVerificationCode();
             if ($smsCode === null || $smsCode !== $user->getSmsCode()) {
+                $this->loggerUtil->log('B');
                 throw new LoginException(_('SMS-Verifikation Code inkorrekt'));
             }
 
+            $this->loggerUtil->log('C');
             // sms code was correct, activate user
             $user->setActive(PlayerEnum::USER_NEW);
             $this->userRepository->save($user);
