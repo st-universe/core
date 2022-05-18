ae308d1adfdb9498afc5acf5966fa7ebd9e8f6bf
Janko <jadari@web.de>
2022-05-18 12:13:12 +0200

    custom colors

--- src/Module/Admin/View/Map/ShowMapInfluenceAreas.php
+++ src/Module/Admin/View/Map/ShowMapInfluenceAreas.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stu\Module\Admin\View\Map;
 
+use request;
 use Stu\Component\Map\MapEnum;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
@@ -28,6 +29,8 @@ final class ShowMapInfluenceAreas implements ViewControllerInterface
 
     public function handle(GameControllerInterface $game): void
     {
+        $showAllyAreas = request::getInt('showAlly');
+
         $img = imagecreatetruecolor(MapEnum::MAP_MAX_X * 15, MapEnum::MAP_MAX_Y * 15);
 
         // mapfields
@@ -50,8 +53,43 @@ final class ShowMapInfluenceAreas implements ViewControllerInterface
             if ($data->getSystem()) {
                 $col = imagecolorallocate($border, 255, 0, 0);
             } else {
-                $col = imagecolorallocate($border, $rest, $rest, $rest);
+                $customColor = null;
+                $influenceArea = $data->getInfluenceArea();
+                if ($influenceArea !== null) {
+                    $base = $influenceArea->getBase();
+
+                    $ally = $base->getUser()->getAlliance();
+
+                    if ($ally !== null) {
+                        $rgbCode = $ally->getRgbCode();
+                    } else {
+                        $rgbCode = $base->getUser()->getRgbCode();
+                    }
+
+                    if ($rgbCode !== '') {
+                        $red = 100;
+                        $green = 100;
+                        $blue = 100;
+
+                        $ret = [];
+                        if (mb_eregi("[#]?([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})", $rgbCode, $ret)) {
+
+                            $red = hexdec($ret[1]);
+
+                            $green = hexdec($ret[2]);
+
+                            $blue = hexdec($ret[3]);
+                        }
+                        $customColor = $rgbCode;
+                        $col = imagecolorallocate($border, $red, $green, $blue);
+                    }
+                }
+
+                if ($customColor === null) {
+                    $col = imagecolorallocate($border, $rest, $rest, $rest);
+                }
             }
+
             imagefill($border, 0, 0, $col);
             imagecopy($img, $border, $curx, $cury, 0, 0, 15, 15);
             $curx += 15;
