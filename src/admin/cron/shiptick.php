5e43c04dcb5e3aa637078c9132016da5f74f44da
Janko <jadari@web.de>
2022-07-07 11:59:12 +0200

    log faulty tries

--- src/admin/cron/shiptick.php
+++ src/admin/cron/shiptick.php
@@ -2,9 +2,14 @@
 
 use Doctrine\ORM\EntityManagerInterface;
 use Stu\Component\Admin\Notification\FailureEmailSenderInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilFactoryInterface;
 use Stu\Module\Tick\Ship\ShipTickManagerInterface;
 
 require_once __DIR__ . '/../../Config/Bootstrap.php';
+
+$loggerUtil = $container->get(LoggerUtilFactoryInterface::class)->getLoggerUtil();
+$loggerUtil->init("mail", LoggerEnum::LEVEL_ERROR);
 
 $entityManager = $container->get(EntityManagerInterface::class);
 
@@ -17,19 +22,28 @@ while ($remainingtries > 0) {
 
     if ($exception === null) {
         break;
-    } else if ($remainingtries === 0) {
-        $emailSender = $container->get(FailureEmailSenderInterface::class);
-        $emailSender->sendMail(
-            "stu shiptick failure",
-            sprintf(
-                "Current system time: %s\nThe shiptick cron caused an error:\n\n%s\n\n%s",
-                date('Y-m-d H:i:s'),
-                $exception->getMessage(),
-                $exception->getTraceAsString()
-            )
-        );
+    } else {
+        // logging problem
+        $loggerUtil->log(sprintf(
+            "Shiptick caused an exception. Remaing tries: %d",
+            $remainingtries
+        ));
 
-        throw $exception;
+        // sending email if no remaining tries left
+        if ($remainingtries === 0) {
+            $emailSender = $container->get(FailureEmailSenderInterface::class);
+            $emailSender->sendMail(
+                "stu shiptick failure",
+                sprintf(
+                    "Current system time: %s\nThe shiptick cron caused an error:\n\n%s\n\n%s",
+                    date('Y-m-d H:i:s'),
+                    $exception->getMessage(),
+                    $exception->getTraceAsString()
+                )
+            );
+
+            throw $exception;
+        }
     }
 }
 
