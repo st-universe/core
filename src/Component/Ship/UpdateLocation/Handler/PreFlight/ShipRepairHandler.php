a5ac1b34a1fe97cd7739c0eee2c974c54462f0c6
Janko <jadari@web.de>
2022-07-11 15:16:19 +0200

    log on

--- src/Component/Ship/UpdateLocation/Handler/PreFlight/ShipRepairHandler.php
+++ src/Component/Ship/UpdateLocation/Handler/PreFlight/ShipRepairHandler.php
@@ -6,13 +6,27 @@ namespace Stu\Component\Ship\UpdateLocation\Handler\PreFlight;
 
 use Stu\Component\Ship\UpdateLocation\Handler\AbstractUpdateLocationHandler;
 use Stu\Component\Ship\UpdateLocation\Handler\UpdateLocationHandlerInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilFactoryInterface;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Orm\Entity\ShipInterface;
 
 final class ShipRepairHandler extends AbstractUpdateLocationHandler implements UpdateLocationHandlerInterface
 {
+    private LoggerUtilInterface $loggerUtil;
+
+    public function __construct(LoggerUtilFactoryInterface $loggerUtilFactory)
+    {
+        $this->loggerUtil = $loggerUtilFactory->getLoggerUtil();
+    }
+
     public function handle(ShipInterface $ship, ?ShipInterface $tractoringShip): void
     {
+        $this->loggerUtil->init('stu', LoggerEnum::LEVEL_ERROR);
+
+        $this->loggerUtil->log('     handle');
         if ($ship->cancelRepair()) {
+            $this->loggerUtil->log('     canceledRepair');
             $this->addMessageInternal(sprintf(_('Die Reparatur der %s wurde abgebrochen'), $ship->getName()));
         }
     }
