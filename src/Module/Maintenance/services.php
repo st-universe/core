df9cd363bde49a20fc02780958e63d6cf04f772f
Janko <jadari@web.de>
2022-07-17 12:18:10 +0200

    delete users at the end

--- src/Module/Maintenance/services.php
+++ src/Module/Maintenance/services.php
@@ -9,12 +9,12 @@ use function DI\autowire;
 return [
     DatabaseBackup::class => autowire(DatabaseBackup::class),
     MapCycle::class => autowire(MapCycle::class),
-    IdleUserDeletion::class => autowire(IdleUserDeletion::class),
     ExpiredInvitationTokenDeletion::class => autowire(ExpiredInvitationTokenDeletion::class),
     OldTachyonScanDeletion::class => autowire(OldTachyonScanDeletion::class),
     OldTradeOffersDeletion::class => autowire(OldTradeOffersDeletion::class),
     CorruptFleetDeletion::class => autowire(CorruptFleetDeletion::class),
     OldFlightSignatureDeletion::class => autowire(OldFlightSignatureDeletion::class),
     ColonyCorrectorHandler::class => autowire(ColonyCorrectorHandler::class),
-    EmptyPlotDeletion::class => autowire(EmptyPlotDeletion::class)
+    EmptyPlotDeletion::class => autowire(EmptyPlotDeletion::class),
+    IdleUserDeletion::class => autowire(IdleUserDeletion::class)
 ];
