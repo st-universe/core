60ccc4cc3993141ecf0f2348f0ee33efbb5a2da5
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-07-23 18:12:02 +0200

    try

--- src/Lib/VisualNavPanelEntry.php
+++ src/Lib/VisualNavPanelEntry.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 use Stu\Component\Ship\ShipRumpEnum;
 use Stu\Orm\Entity\ShipInterface;
 use Stu\Orm\Entity\StarSystemInterface;
+use Stu\Orm\Entity\MapBorderTypeInterface;
+use Stu\Orm\Entity\MapRegionInterface;
+use Stu\Orm\Entity\StarSystemInterface;
+
 
 class VisualNavPanelEntry
 {
@@ -14,6 +18,12 @@ class VisualNavPanelEntry
     private $isTachyonSystemActive;
 
     private $tachyonFresh;
+	
+    private ?MapBorderTypeInterface $mapBorderType;
+
+	private ?MapRegionInterface $adminRegion;
+	
+	private ?StarSystemInterface $influenceArea;
 
     private $ship;
 
@@ -45,11 +55,20 @@ class VisualNavPanelEntry
     {
         return $this->data['posy'];
     }
-
+	
+    public function getFieldId(): int
+    {
+        return $this->field_id;
+    }
+	
     function getMapfieldType()
     {
-        return $this->data['type'];
-    }
+        $type = $this->getFieldId();
+
+        $style = "background-image: url('assets/map/" . $type . ".png');";
+        $style .= $this->getBorder();
+        return $style;
+	}
 
     function getShipCount()
     {
@@ -137,11 +156,41 @@ class VisualNavPanelEntry
         return false;
     }
 
+    function getBorder(): string
+    {
+        $borderType = $this->mapBorderType;
+        if ($borderType === null) {
+            if ($this->adminRegion === null) {
+                if ($this->influenceArea !== null) {
+                    $influenceArea = $this->influenceArea;
+                    $base = $influenceArea->getBase();
+
+                    if ($base !== null) {
+                        $user = $base->getUser();
+                        $ally = $user->getAlliance();
+
+                        if ($ally !== null && strlen($ally->getRgbCode()) > 0) {
+                            return 'border: 1px solid ' . $ally->getRgbCode();
+                        } else if (strlen($user->getRgbCode()) > 0) {
+                            return 'border: 1px solid ' . $user->getRgbCode();
+                        }
+                    }
+                }
+            }
+        } else {
+            return 'border: 1px solid #' . $this->mapBorderType->getColor();
+        }
+
+        return '';
+    }
+
     //obsolete?
-    function getBorder()
+    /**
+	function getBorder()
     {
         return $this->data['color'];
     }
+	*/
 
     private $cssClass = 'lss';
 
