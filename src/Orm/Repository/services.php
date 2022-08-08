2ad8783ef232bf1f2fec3c72322aa70943786df6
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-08-08 23:44:21 +0200

    Revert "mh"
    This reverts commit 1bd58d6d824361c8a8a59ecdde517edc2a10b874.

--- src/Orm/Repository/services.php
+++ src/Orm/Repository/services.php
@@ -598,7 +598,7 @@ return [
     TradeCreateLicenceRepositoryInterface::class => function (
         ContainerInterface $c
     ): TradeCreateLicenceRepositoryInterface {
-        return $c->get(EntityManagerInterface::class)->getRepository(TradeLicenceCreation::class);
+        return $c->get(EntityManagerInterface::class)->getRepository(TradeCreateLicence::class);
     },
     TradeLicenseRepositoryInterface::class => function (
         ContainerInterface $c
