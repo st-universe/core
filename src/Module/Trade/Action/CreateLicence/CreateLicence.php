a2a07cb575d6c211bea990de0b813c0321fc8a9c
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-08-08 19:40:10 +0200

    lets see

---
+++ src/Module/Trade/Action/CreateLicence/CreateLicence.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Trade\Action\CreateLicence;
+
+use Stu\Exception\AccessViolation;
+use Stu\Module\Commodity\CommodityTypeEnum;
+use Stu\Module\Control\ActionControllerInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Trade\Lib\TradeLibFactoryInterface;
+use Stu\Module\Trade\View\ShowAccounts\ShowAccounts;
+use Stu\Orm\Entity\CommodityInterface;
+use Stu\Orm\Repository\CommodityRepositoryInterface;
+use Stu\Orm\Repository\TradeCreateLicenceRepositoryInterface;
+use Stu\Orm\Repository\TradePostRepositoryInterface;
+
+final class CreateLicence implements ActionControllerInterface
+{
+    public const ACTION_IDENTIFIER = 'B_CREATE_LICENCE';
+
+    private CreateLicenceRequestInterface $createLicenceRequest;
+
+    private CommodityRepositoryInterface $commodityRepository;
+
+    private TradeLibFactoryInterface $tradeLibFactory;
+
+    private TradeCreateLicenceRepositoryInterface $tradeLicenceRepository;
+
+    private TradePostRepositoryInterface $tradePostRepository;
+
+    public function __construct(
+        CreateLicenceRequestInterface $createLicenceRequest,
+        CommodityRepositoryInterface $commodityRepository,
+        TradeLibFactoryInterface $tradeLibFactory,
+        TradeCreateLicenceRepositoryInterface $tradeLicenceRepository,
+        TradePostRepositoryInterface $tradePostRepository
+    ) {
+        $this->createLicenceRequest = $createLicenceRequest;
+        $this->commodityRepository = $commodityRepository;
+        $this->tradeLibFactory = $tradeLibFactory;
+        $this->tradeLicenceRepository = $tradeLicenceRepository;
+        $this->tradePostRepository = $tradePostRepository;
+    }
+
+    public function handle(GameControllerInterface $game): void
+    {
+        $game->setView(ShowAccounts::VIEW_IDENTIFIER);
+
+        $userId = $game->getUser()->getId();
+
+        $trade_post = $this->tradePostRepository->find($this->createLicenceRequest->getTradePostId());
+        if ($trade_post === null) {
+            throw new AccessViolation(sprintf("Tradepost not existent! Fool: %d", $userId));
+        }
+        if ($trade_post->getUserId() !== $userId) {
+            throw new AccessViolation(sprintf("Tradepost belongs to other user! Fool: %d", $userId));
+        }
+
+        $wantedGoodId = $this->createLicenceRequest->getWantedLicenceGoodId();
+        $giveAmount = $this->createLicenceRequest->getWantedLicenceAmount();
+        $licenceDays = $this->createLicenceRequest->getLicenceDays();
+
+
+        if ($giveAmount < 1 || $wantedGoodId < 1 ) {
+            return;
+        }
+
+        $setLicence = $this->tradeLicenceRepository->prototype();
+        $setLicence->setTradePost($trade_post);
+        $setLicence->setDate(time());
+        $setLicence->setGoodsId($this->commodityRepository->find($wantedGoodId));
+        $setLicence->setAmount((int) $giveAmount);
+        $setLicence->setDays($licenceDays);
+
+        $this->tradeLicenceRepository->save($setLicence);
+
+
+        $game->addInformation('Handelslizenz geändert');
+    }
+
+    public function performSessionCheck(): bool
+    {
+        return false;
+    }
+}
