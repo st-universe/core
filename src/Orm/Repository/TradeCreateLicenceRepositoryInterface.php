8926b9695dca7c6d7f446aeb0f855e99a3587374
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-08-08 20:09:36 +0200

    Revert "next try"
    This reverts commit 8cf5e2a401b468bb8645d11e69487057e05577cd.

--- src/Orm/Repository/TradeCreateLicenceRepositoryInterface.php
+++ src/Orm/Repository/TradeCreateLicenceRepositoryInterface.php
@@ -7,15 +7,15 @@ namespace Stu\Orm\Repository;
 use Doctrine\Persistence\ObjectRepository;
 use Stu\Orm\Entity\TradeCreateLicenceInterface;
 
-
+/**
+ * @method null|TradeCreateLicenceInterface find(integer $id)
+ */
 interface TradeCreateLicenceRepositoryInterface extends ObjectRepository
 {
-    /**
-    * @return TradeCreateLicenceInterface[]
-    */
     public function prototype(): TradeCreateLicenceInterface;
 
     public function save(TradeCreateLicenceInterface $post): void;
 
     public function delete(TradeCreateLicenceInterface $post): void;
+
 }
