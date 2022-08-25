5bbc7b46f69e50de5b5ef7e89232975711aed683
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-08-25 22:28:04 +0200

    Revert "fix"
    This reverts commit 41a4e9e8fd62ca5366e6a675c6e9dcecd21aab54.

--- src/Orm/Repository/TradeLicenseRepository.php
+++ src/Orm/Repository/TradeLicenseRepository.php
@@ -59,8 +59,7 @@ final class TradeLicenseRepository extends EntityRepository implements TradeLice
         return $this->getEntityManager()
             ->createQuery(
                 sprintf(
-                    'SELECT tl.posts_id FROM %s tl WHERE tl.user_id = :userId AND tl.expired > :actime ORDER BY tl.posts_id ASC',
-                    TradeLicense::class
+                    'SELECT tl.posts_id FROM stu_trade_licences tl WHERE tl.user_id = :userId AND tl.expired > :actime ORDER BY tl.posts_id ASC',
                 )
             )
             ->setParameters([
