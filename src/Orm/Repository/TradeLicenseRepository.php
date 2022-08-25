6da2fa97b2575ca6b70643b29485dfeab71f1ec9
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-08-25 13:21:17 +0200

    or this fix

--- src/Orm/Repository/TradeLicenseRepository.php
+++ src/Orm/Repository/TradeLicenseRepository.php
@@ -106,7 +106,7 @@ final class TradeLicenseRepository extends EntityRepository implements TradeLice
         return $this->getEntityManager()
             ->createQuery(
                 sprintf(
-                    'SELECT tlc.expired FROM %s tlc WHERE tlc.posts_id = :trade_post ORDER BY tlc.id DESC LIMIT 1',
+                    'SELECT tlc.expired FROM %s tlc WHERE tlc.posts_id = :trade_post ORDER BY tlc.id DESC',
                     TradeLicenceCreation::class
                 )
             )
