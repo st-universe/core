214db90e6cc1843a2887479e4b91cc96f974580f
Janko <jadari@web.de>
2022-08-29 18:58:57 +0200

    clean code

--- src/Orm/Repository/TradeTransactionRepository.php
+++ src/Orm/Repository/TradeTransactionRepository.php
@@ -33,18 +33,21 @@ final class TradeTransactionRepository extends EntityRepository implements Trade
 
     public function getTradePostsTop10(): array
     {
-        $time = time() - 604800;
         $rsm = new ResultSetMapping();
         $rsm->addScalarResult('id', 'id', 'integer');
         $rsm->addScalarResult('name', 'name', 'string');
         $rsm->addScalarResult('transactions', 'transactions', 'integer');
         return $this->getEntityManager()->createNativeQuery(
-            'SELECT tp.id, tp.name, COUNT(tt.tradepost_id) as transactions FROM stu_trade_transaction tt LEFT JOIN
-            stu_trade_posts tp ON tp.id = tt.tradepost_id WHERE tt.date > :sevendays AND tt.tradepost_id > 0 GROUP BY tp.id ORDER BY transactions DESC LIMIT 10',
+            'SELECT tp.id, tp.name, COUNT(tt.tradepost_id) as transactions
+            FROM stu_trade_transaction tt
+            LEFT JOIN stu_trade_posts tp ON tp.id = tt.tradepost_id
+            WHERE tt.date > :sevendays
+                AND tt.tradepost_id > 0
+            GROUP BY tp.id ORDER BY transactions DESC LIMIT 10',
             $rsm
         )
             ->setParameters([
-                'sevendays' => $time
+                'sevendays' => time() - 604800
             ])
             ->getArrayResult();
     }
