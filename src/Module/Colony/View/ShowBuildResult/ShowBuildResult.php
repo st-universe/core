224d53b7a5f5a3c822c07158e1a82b8b7dd03cbe
Janko <jadari@web.de>
2022-09-03 20:55:58 +0200

    code optimization

--- src/Module/Colony/View/ShowBuildResult/ShowBuildResult.php
+++ src/Module/Colony/View/ShowBuildResult/ShowBuildResult.php
@@ -6,35 +6,15 @@ namespace Stu\Module\Colony\View\ShowBuildResult;
 
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
-use Stu\Module\Colony\Lib\ColonyLoaderInterface;
 
 final class ShowBuildResult implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_BUILD_RESULT';
 
-    private ColonyLoaderInterface $colonyLoader;
-
-    private ShowBuildResultRequestInterface $showBuildResultRequest;
-
-    public function __construct(
-        ColonyLoaderInterface $colonyLoader,
-        ShowBuildResultRequestInterface $showBuildResultRequest
-    ) {
-        $this->colonyLoader = $colonyLoader;
-        $this->showBuildResultRequest = $showBuildResultRequest;
-    }
-
     public function handle(GameControllerInterface $game): void
     {
-        $userId = $game->getUser()->getId();
-
-        $colony = $this->colonyLoader->byIdAndUser(
-            $this->showBuildResultRequest->getColonyId(),
-            $userId
-        );
         $game->showMacro('html/sitemacros.xhtml/systeminformation');
 
-        //$game->setTemplateVar('COLONY', $colony);
         $game->addExecuteJS('refreshColony();');
     }
 }
