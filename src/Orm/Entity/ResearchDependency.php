6a622f51f4bba3fe2cbf000c385a43675c23b5d9
Janko <jadari@web.de>
2022-09-06 19:42:40 +0200

    remove column

--- src/Orm/Entity/ResearchDependency.php
+++ src/Orm/Entity/ResearchDependency.php
@@ -26,9 +26,6 @@ class ResearchDependency implements ResearchDependencyInterface
     /** @Column(type="smallint") * */
     private $mode;
 
-    /** @Column(type="boolean") */
-    private $is_award_dependency = false;
-
     /**
      * @ManyToOne(targetEntity="Stu\Orm\Entity\Research")
      * @JoinColumn(name="research_id", referencedColumnName="id")
