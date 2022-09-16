83a24aeabc5f86e11664bf6b23fc49a354b4e1bc
Janko <jadari@web.de>
2022-09-16 21:35:45 +0200

    log

--- src/Component/Player/Deletion/Handler/ShipBuildplanDeletionHandler.php
+++ src/Component/Player/Deletion/Handler/ShipBuildplanDeletionHandler.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Stu\Component\Player\Deletion\Handler;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Stu\Module\Logging\LoggerEnum;
+use Stu\Module\Logging\LoggerUtilFactoryInterface;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Orm\Entity\UserInterface;
 use Stu\Orm\Repository\ShipBuildplanRepositoryInterface;
 
@@ -14,17 +17,27 @@ final class ShipBuildplanDeletionHandler implements PlayerDeletionHandlerInterfa
 
     private EntityManagerInterface $entityManager;
 
+    private LoggerUtilInterface $loggerUtil;
+
     public function __construct(
         ShipBuildplanRepositoryInterface $shipBuildplanRepository,
-        EntityManagerInterface  $entityManager
+        EntityManagerInterface  $entityManager,
+        LoggerUtilFactoryInterface $loggerUtilFactory
     ) {
         $this->shipBuildplanRepository = $shipBuildplanRepository;
         $this->entityManager = $entityManager;
+        $this->loggerUtil = $loggerUtilFactory->getLoggerUtil();
     }
 
     public function delete(UserInterface $user): void
     {
+        $this->loggerUtil->init('delete', LoggerEnum::LEVEL_ERROR);
+
+        $this->loggerUtil->log(sprintf('user has %d buildplans', count($this->shipBuildplanRepository->getByUser($user->getId()))));
+
         $this->shipBuildplanRepository->truncateByUser($user->getId());
         $this->entityManager->flush();
+
+        $this->loggerUtil->log(sprintf('user has %d buildplans', count($this->shipBuildplanRepository->getByUser($user->getId()))));
     }
 }
