93a76a82cfaaa3a1bcc8a2ff186c503c5710ca67
Janko <jadari@web.de>
2022-09-17 20:25:16 +0200

    fix

--- src/Orm/Entity/TradeLicenseInfo.php
+++ src/Orm/Entity/TradeLicenseInfo.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Stu\Orm\Entity;
 
 /**
- * @Entity(repositoryClass="Stu\Orm\Repository\TradeCreateLicenceRepository")
+ * @Entity(repositoryClass="Stu\Orm\Repository\TradeLicenseInfoRepository")
  * @Table(
  *     name="stu_trade_license_info",
  *     indexes={
