b7c393ad93a325803d1f9082b366886efed1a456
Janko <jadari@web.de>
2022-09-20 22:33:46 +0200

    new entity for logging game requests

---
+++ src/Orm/Entity/GameRequest.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Orm\Entity;
+
+/**
+ * @Entity(repositoryClass="Stu\Orm\Repository\GameRequestRepository")
+ * @Table(
+ *     name="stu_game_request"
+ * )
+ **/
+class GameRequest implements GameRequestInterface
+{
+    /** 
+     * @Id
+     * @Column(type="integer")
+     * @GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /** @Column(type="integer") * */
+    private $turn_id;
+
+    /** @Column(type="integer") * */
+    private $time;
+
+    /** @Column(type="string") * */
+    private $module;
+
+    /** @Column(type="string", nullable=true) * */
+    private $action;
+
+    /** @Column(type="integer") * */
+    private $action_ms;
+
+    /** @Column(type="string", nullable=true) * */
+    private $view;
+
+    /** @Column(type="integer") * */
+    private $view_ms;
+
+    /**
+     * @OneToOne(targetEntity="GameTurn")
+     * @JoinColumn(name="turn_id", referencedColumnName="id", onDelete="CASCADE")
+     */
+    private $turn;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getTurn(): GameTurnInterface
+    {
+        return $this->turn;
+    }
+
+    public function setTurn(GameTurnInterface $turn): GameRequestInterface
+    {
+        $this->turn = $turn;
+        return $this;
+    }
+
+    public function setTime(int $time): GameRequestInterface
+    {
+        $this->time = $time;
+        return $this;
+    }
+
+    public function setModule(string $module): GameRequestInterface
+    {
+        $this->module = $module;
+        return $this;
+    }
+
+    public function setAction(?string $action): GameRequestInterface
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    public function setActionMs(int $actionMs): GameRequestInterface
+    {
+        $this->action_ms = $actionMs;
+        return $this;
+    }
+
+    public function setView(?string $view): GameRequestInterface
+    {
+        $this->view = $view;
+        return $this;
+    }
+
+    public function setViewMs(int $viewMs): GameRequestInterface
+    {
+        $this->view_ms = $viewMs;
+        return $this;
+    }
+}
---
+++ src/Orm/Entity/GameRequestInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Stu\Orm\Entity;
+
+interface GameRequestInterface
+{
+    public function getId(): int;
+
+    public function getTurn(): GameTurnInterface;
+
+    public function setTurn(GameTurnInterface $turn): GameRequestInterface;
+
+    public function setTime(int $time): GameRequestInterface;
+
+    public function setModule(string $module): GameRequestInterface;
+
+    public function setAction(?string $action): GameRequestInterface;
+
+    public function setActionMs(int $actionMs): GameRequestInterface;
+
+    public function setView(?string $view): GameRequestInterface;
+
+    public function setViewMs(int $viewMs): GameRequestInterface;
+}
