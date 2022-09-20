0bcf83ed3a2d261288f287293277a671bbf5249f
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-09-20 14:30:11 +0200

    fix

--- src/Module/Database/View/ShowStatistics/ShowStatistics.php
+++ src/Module/Database/View/ShowStatistics/ShowStatistics.php
@@ -98,9 +98,10 @@ final class ShowStatistics implements ViewControllerInterface
         $graph->SetColor('black');
         $graph->SetFrame(false);
 	$graph->SetBox(false);
-	$graph->SetAxisStyle(AXSTYLE_BOXIN)
+	$graph->SetAxisStyle(AXSTYLE_BOXIN);
 	$graph->img->SetAntiAliasing();
         $graph->SetScale('intint', $minY, $maxY, $datax[0], $datax[count($datax) - 1]);
+	$graph->tabtitle->SetFillColor('black');
 
         $graph->xaxis->SetLabelAngle(45);
         $graph->xaxis->SetPos('min');
