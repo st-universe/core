8cf9f0cce5c3b1c404c9f81ce1201cfe5f8fd256
Janko <jadari@web.de>
2022-09-21 21:40:06 +0200

    remove unused import

--- src/Module/Admin/View/Ticks/ShowTicks.php
+++ src/Module/Admin/View/Ticks/ShowTicks.php
@@ -4,20 +4,12 @@ declare(strict_types=1);
 
 namespace Stu\Module\Admin\View\Ticks;
 
-use Stu\Module\Control\EntityManagerLoggingInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 
 final class ShowTicks implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_TICKS';
-
-    private EntityManagerLoggingInterface $entityManagerLogging;
-
-    public function __construct(EntityManagerLoggingInterface $entityManagerLogging)
-    {
-        $this->entityManagerLogging = $entityManagerLogging;
-    }
 
     public function handle(GameControllerInterface $game): void
     {
