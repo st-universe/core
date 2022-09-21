8579538177a19bfea16992c542ac2f3a1dde01ba
Janko <jadari@web.de>
2022-09-21 20:36:44 +0200

    foobar

--- src/Module/Admin/View/Ticks/ShowTicks.php
+++ src/Module/Admin/View/Ticks/ShowTicks.php
@@ -4,12 +4,20 @@ declare(strict_types=1);
 
 namespace Stu\Module\Admin\View\Ticks;
 
+use Stu\Module\Control\FoobarInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 
 final class ShowTicks implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_TICKS';
+
+    private $entityManager;
+
+    public function __construct(FoobarInterface $foobar)
+    {
+        $this->entityManager = $foobar;
+    }
 
     public function handle(GameControllerInterface $game): void
     {
