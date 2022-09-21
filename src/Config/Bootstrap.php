1e6c827bf763b96bee8b5db409f5a3aa9b03c382
Janko <jadari@web.de>
2022-09-21 09:55:49 +0200

    callback

--- src/Config/Bootstrap.php
+++ src/Config/Bootstrap.php
@@ -38,6 +38,39 @@ use function DI\autowire;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
+$entityManagerCallback = function (ContainerInterface $c): EntityManagerInterface {
+    $config = $c->get(ConfigInterface::class);
+    $cacheDriver = new DoctrineCacheBridge($c->get(CacheItemPoolInterface::class));
+
+    $emConfig = new Configuration();
+    $emConfig->setAutoGenerateProxyClasses(0);
+    $emConfig->setMetadataCacheImpl($cacheDriver);
+    $emConfig->setQueryCacheImpl($cacheDriver);
+
+    $driverImpl = $emConfig->newDefaultAnnotationDriver(__DIR__ . '/../Orm/Entity/');
+    $emConfig->setMetadataDriverImpl($driverImpl);
+    $emConfig->setProxyDir(sprintf(
+        '%s/../OrmProxy/',
+        __DIR__
+    ));
+    $emConfig->setProxyNamespace($config->get('db.proxy_namespace'));
+
+    $manager = EntityManager::create(
+        [
+            'driver' => 'pdo_pgsql',
+            'user' => $config->get('db.user'),
+            'password' => $config->get('db.pass'),
+            'dbname' => $config->get('db.database'),
+            'host'  => $config->get('db.host'),
+            'charset' => 'utf8',
+        ],
+        $emConfig
+    );
+
+    $manager->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'integer');
+    return $manager;
+};
+
 $builder = new ContainerBuilder();
 
 $builder->addDefinitions([
@@ -81,38 +114,7 @@ $builder->addDefinitions([
         }
     },
     SessionInterface::class => autowire(Session::class),
-    EntityManagerInterface::class => function (ContainerInterface $c): EntityManagerInterface {
-        $config = $c->get(ConfigInterface::class);
-        $cacheDriver = new DoctrineCacheBridge($c->get(CacheItemPoolInterface::class));
-
-        $emConfig = new Configuration();
-        $emConfig->setAutoGenerateProxyClasses(0);
-        $emConfig->setMetadataCacheImpl($cacheDriver);
-        $emConfig->setQueryCacheImpl($cacheDriver);
-
-        $driverImpl = $emConfig->newDefaultAnnotationDriver(__DIR__ . '/../Orm/Entity/');
-        $emConfig->setMetadataDriverImpl($driverImpl);
-        $emConfig->setProxyDir(sprintf(
-            '%s/../OrmProxy/',
-            __DIR__
-        ));
-        $emConfig->setProxyNamespace($config->get('db.proxy_namespace'));
-
-        $manager = EntityManager::create(
-            [
-                'driver' => 'pdo_pgsql',
-                'user' => $config->get('db.user'),
-                'password' => $config->get('db.pass'),
-                'dbname' => $config->get('db.database'),
-                'host'  => $config->get('db.host'),
-                'charset' => 'utf8',
-            ],
-            $emConfig
-        );
-
-        $manager->getConnection()->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'integer');
-        return $manager;
-    },
+    EntityManagerInterface::class => $entityManagerCallback,
     TalPageInterface::class => autowire(TalPage::class),
     GameControllerInterface::class => autowire(GameController::class),
     Parser::class => function (): Parser {
