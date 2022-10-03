c8e766ca57a66ff0350a5c53a158faeeb217c2fb
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-10-03 12:09:00 +0200

    Update Colony.php

--- src/Orm/Entity/Colony.php
+++ src/Orm/Entity/Colony.php
@@ -373,7 +373,7 @@ class Colony implements ColonyInterface
 
             if ($this->getDayTimePrefix() == 1) {
                 $scaled = ceil((((100 / ($this->getRotationTime() * 0.375 - $this->getRotationTime() * 0.25)) * ($this->getColonyTimeSeconds() - $this->getRotationTime() * 0.25)) / 100) * $width);
-                $this->twilightZone = (int) - ((2 * $width) - $scaled);
+                $this->twilightZone = (int) - (($width) - $scaled);
             }
             if ($this->getDayTimePrefix() == 2) {
                 $this->twilightZone = $width;
