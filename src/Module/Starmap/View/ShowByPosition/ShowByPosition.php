a4629b15396fe009e536e989414df4c94ffb4e54
Janko <jadari@web.de>
2022-10-17 15:31:24 +0200

    code optimization

--- src/Module/Starmap/View/ShowByPosition/ShowByPosition.php
+++ src/Module/Starmap/View/ShowByPosition/ShowByPosition.php
@@ -4,77 +4,40 @@ declare(strict_types=1);
 
 namespace Stu\Module\Starmap\View\ShowByPosition;
 
-use Stu\Component\Map\MapEnum;
+use Stu\Component\Game\ModuleViewEnum;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
-use UserYRow;
+use Stu\Module\Starmap\Lib\MapSectionHelper;
+use Stu\Module\Starmap\View\ShowSection\ShowSectionRequestInterface;
 
 final class ShowByPosition implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_STARMAP_POSITION';
-    private const FIELDS_PER_SECTION = 20;
 
-    private ShowByPositionRequestInterface $showByPositionRequest;
+    private ShowSectionRequestInterface $request;
 
     public function __construct(
-        ShowByPositionRequestInterface $showByPositionRequest
+        ShowSectionRequestInterface $request
     ) {
-        $this->showByPositionRequest = $showByPositionRequest;
+        $this->request = $request;
     }
 
     public function handle(GameControllerInterface $game): void
     {
-        $xCoordinate = $this->showByPositionRequest->getXCoordinate();
-        $yCoordinate = $this->showByPositionRequest->getYCoordinate();
-
-        $maxx = $xCoordinate * self::FIELDS_PER_SECTION;
-        $minx = $maxx - self::FIELDS_PER_SECTION + 1;
-        $maxy = $yCoordinate * self::FIELDS_PER_SECTION;
-        $miny = $maxy - self::FIELDS_PER_SECTION + 1;
-
-        $fields = [];
-        foreach (range($miny, $maxy) as $value) {
-            $fields[] = new UserYRow($game->getUser(), $value, $minx, $maxx);
-        }
+        $xCoordinate = $this->request->getXCoordinate();
+        $yCoordinate = $this->request->getYCoordinate();
+        $sectionId = $this->request->getSectionId();
 
         $game->setMacroInAjaxWindow('html/macros.xhtml/starmap');
 
-        $game->setTemplateVar('HEAD_ROW', range($minx, $maxx));
-        $game->setTemplateVar('MAP_FIELDS', $fields);
-
-        if ($yCoordinate > 1) {
-            $game->setTemplateVar(
-                'NAV_UP',
-                $this->constructPath($xCoordinate, $yCoordinate - 1)
-            );
-        }
-        if ($yCoordinate * static::FIELDS_PER_SECTION < MapEnum::MAP_MAX_Y) {
-            $game->setTemplateVar(
-                'NAV_DOWN',
-                $this->constructPath($xCoordinate, $yCoordinate + 1)
-            );
-        }
-        if ($xCoordinate > 1) {
-            $game->setTemplateVar(
-                'NAV_LEFT',
-                $this->constructPath($xCoordinate - 1, $yCoordinate)
-            );
-        }
-        if ($xCoordinate * static::FIELDS_PER_SECTION < MapEnum::MAP_MAX_X) {
-            $game->setTemplateVar(
-                'NAV_RIGHT',
-                $this->constructPath($xCoordinate + 1, $yCoordinate)
-            );
-        }
-    }
-
-    private function constructPath(int $x, int $y): string
-    {
-        return sprintf(
-            'starmap.php?%s=1&x=%d&y=%d',
-            self::VIEW_IDENTIFIER,
-            $x,
-            $y
+        $helper = new MapSectionHelper();
+        $helper->setTemplateVars(
+            $game,
+            $xCoordinate,
+            $yCoordinate,
+            $sectionId,
+            ModuleViewEnum::MODULE_VIEW_STARMAP,
+            self::VIEW_IDENTIFIER
         );
     }
 }
--- src/Module/Starmap/services.php
+++ src/Module/Starmap/services.php
@@ -7,8 +7,6 @@ namespace Stu\Module\Starmap;
 use Stu\Module\Control\GameController;
 use Stu\Module\Starmap\View\Overview\Overview;
 use Stu\Module\Starmap\View\ShowByPosition\ShowByPosition;
-use Stu\Module\Starmap\View\ShowByPosition\ShowByPositionRequest;
-use Stu\Module\Starmap\View\ShowByPosition\ShowByPositionRequestInterface;
 use Stu\Module\Starmap\View\ShowSection\ShowSection;
 use Stu\Module\Starmap\View\ShowSection\ShowSectionRequest;
 use Stu\Module\Starmap\View\ShowSection\ShowSectionRequestInterface;
@@ -16,9 +14,7 @@ use function DI\autowire;
 
 return [
     ShowSectionRequestInterface::class => autowire(ShowSectionRequest::class),
-    ShowByPositionRequestInterface::class => autowire(ShowByPositionRequest::class),
-    'STARMAP_ACTIONS' => [
-    ],
+    'STARMAP_ACTIONS' => [],
     'STARMAP_VIEWS' => [
         GameController::DEFAULT_VIEW => autowire(Overview::class),
         ShowSection::VIEW_IDENTIFIER => autowire(ShowSection::class),
