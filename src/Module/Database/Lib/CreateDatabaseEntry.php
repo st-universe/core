cead7216b1c6a8bd296a317c171f9ca06a450b84
Janko <jadari@web.de>
2022-10-23 20:01:10 +0200

    avoid double award

--- src/Module/Database/Lib/CreateDatabaseEntry.php
+++ src/Module/Database/Lib/CreateDatabaseEntry.php
@@ -75,6 +75,11 @@ final class CreateDatabaseEntry implements CreateDatabaseEntryInterface
 
         $award = $category->getAward();
 
+        //check if user already has award in case of category updates
+        if ($user->hasAward($award->getId())) {
+            return;
+        }
+
         if ($this->databaseUserRepository->hasUserCompletedCategory($user->getId(), $category->getId())) {
 
             $this->createUserAward->createAwardForUser($user, $award);
