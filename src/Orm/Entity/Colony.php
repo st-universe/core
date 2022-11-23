7b32e1f8ef10f5441de946b5d284b4dfac3efae1
Janko <jadari@web.de>
2022-11-23 11:31:15 +0100

    fix

--- src/Orm/Entity/Colony.php
+++ src/Orm/Entity/Colony.php
@@ -863,7 +863,7 @@ class Colony implements ColonyInterface
 
     public function getLifeStandardPercentage(): int
     {
-        return (int)ceil($this->getProductionRaw()[CommodityTypeEnum::COMMODITY_EFFECT_LIFE_STANDARD]->getProduction() * 100 / $this->getBevFood());
+        return min(100, (int)ceil($this->getProductionRaw()[CommodityTypeEnum::COMMODITY_EFFECT_LIFE_STANDARD]->getProduction() * 100 / $this->getBevFood()));
     }
 
     public function getFreeAssignmentCount(?bool $considerLifeStandard = true): int
