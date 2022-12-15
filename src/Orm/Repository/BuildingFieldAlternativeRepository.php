77edca7733adb320ccbdfbe74c920ff5fc33d419
Huxinator <52821266+Huxinator@users.noreply.github.com>
2022-12-15 15:46:03 +0100

    fix

--- src/Orm/Repository/BuildingFieldAlternativeRepository.php
+++ src/Orm/Repository/BuildingFieldAlternativeRepository.php
@@ -19,10 +19,10 @@ final class BuildingFieldAlternativeRepository extends EntityRepository implemen
         ]);
     }
 
-    /*  public function getByBuildingIdAndResearchedByUser(int $buildingId, int $userId): iterable
+    public function getByBuildingIdAndResearchedByUser(int $buildingId, int $userId): iterable
     {
         return $this->getEntityManager()->createQuery(
-            
+
             sprintf(
                 'SELECT b FROM %s b WHERE b.buildings_id = :buildingId AND (
                     b.research_id is null OR b.research_id IN (
@@ -37,5 +37,5 @@ final class BuildingFieldAlternativeRepository extends EntityRepository implemen
                 'buildingId' => $buildingId
             ])
             ->getResult();
-    } */
+    }
 }
