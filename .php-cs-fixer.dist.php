f18decfcd4a1fac9f6757f5b5064530997460b48
Daniel Jakob <github@usox.org>
2023-01-22 21:10:56 +0100

    Update php-cs-fixer
    also add composer scripts targets and a sane base configuration

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,21 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(['src', 'tests'])
+;
+$config = new PhpCsFixer\Config();
+$config->setRules(
+    [
+        '@PSR12' => true,
+        '@PHP81Migration' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline', 'keep_multiple_spaces_after_comma' => false],
+        'no_unused_imports' => true,
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['const', 'class', 'function'],
+        ],
+    ]
+)->setFinder($finder);
+
+return $config;
