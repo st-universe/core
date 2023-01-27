7f54e17ae7747cf38b339626895c1c863ba8c2c2
Daniel Jakob <github@usox.org>
2023-01-27 21:47:08 +0100

    Extract flightsignature creation logic
    Add a separate components for the creation of flightsignatures. Also
    refactor the code a bit

---
+++ tests/Module/Ship/Lib/Movement/Component/FlightSignatureCreatorTest.php
@@ -0,0 +1,269 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Ship\Lib\Movement\Component;
+
+use Mockery;
+use Mockery\MockInterface;
+use Stu\Component\Ship\ShipEnum;
+use Stu\Orm\Entity\FlightSignatureInterface;
+use Stu\Orm\Entity\MapInterface;
+use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\ShipRumpInterface;
+use Stu\Orm\Entity\StarSystemMapInterface;
+use Stu\Orm\Repository\FlightSignatureRepositoryInterface;
+use Stu\StuTestCase;
+
+class FlightSignatureCreatorTest extends StuTestCase
+{
+    /** @var MockInterface&FlightSignatureRepositoryInterface */
+    private MockInterface $flightSignatureRepository;
+
+    private FlightSignatureCreator $subject;
+
+    protected function setUp(): void
+    {
+        $this->flightSignatureRepository = $this->mock(FlightSignatureRepositoryInterface::class);
+
+        $this->subject = new FlightSignatureCreator(
+            $this->flightSignatureRepository
+        );
+    }
+
+    public function directionDataProvider(): array
+    {
+        return [
+            [ShipEnum::DIRECTION_RIGHT, ShipEnum::DIRECTION_LEFT],
+            [ShipEnum::DIRECTION_LEFT, ShipEnum::DIRECTION_RIGHT],
+            [ShipEnum::DIRECTION_TOP, ShipEnum::DIRECTION_BOTTOM],
+            [ShipEnum::DIRECTION_BOTTOM, ShipEnum::DIRECTION_TOP],
+        ];
+    }
+
+    /**
+     * @dataProvider directionDataProvider
+     */
+    public function testCreateOuterSystemSignaturesCreates(
+        int $fromDirection,
+        int $toDirection
+    ): void {
+        $ship = $this->mock(ShipInterface::class);
+        $currentField = $this->mock(MapInterface::class);
+        $nextField = $this->mock(MapInterface::class);
+        $fromSignature = $this->mock(FlightSignatureInterface::class);
+        $toSignature = $this->mock(FlightSignatureInterface::class);
+        $shipRump = $this->mock(ShipRumpInterface::class);
+
+        $userId = 666;
+        $shipId = 42;
+        $shipName = 'some-name';
+        $cloakState = true;
+
+        $ship->shouldReceive('getUser->getId')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($userId);
+        $ship->shouldReceive('getId')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipId);
+        $ship->shouldReceive('getName')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipName);
+        $ship->shouldReceive('getRump')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipRump);
+        $ship->shouldReceive('getCloakState')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($cloakState);
+
+        $fromSignature->shouldReceive('setMap')
+            ->with($currentField)
+            ->once();
+        $fromSignature->shouldReceive('setUserId')
+            ->with($userId)
+            ->once();
+        $fromSignature->shouldReceive('setShipId')
+            ->with($shipId)
+            ->once();
+        $fromSignature->shouldReceive('setShipName')
+            ->with($shipName)
+            ->once();
+        $fromSignature->shouldReceive('setRump')
+            ->with($shipRump)
+            ->once();
+        $fromSignature->shouldReceive('setIsCloaked')
+            ->with($cloakState)
+            ->once();
+        $fromSignature->shouldReceive('setToDirection')
+            ->with($toDirection)
+            ->once();
+        $fromSignature->shouldReceive('setTime')
+            ->with(Mockery::type('int'))
+            ->once();
+
+        $toSignature->shouldReceive('setMap')
+            ->with($nextField)
+            ->once();
+        $toSignature->shouldReceive('setUserId')
+            ->with($userId)
+            ->once();
+        $toSignature->shouldReceive('setShipId')
+            ->with($shipId)
+            ->once();
+        $toSignature->shouldReceive('setShipName')
+            ->with($shipName)
+            ->once();
+        $toSignature->shouldReceive('setRump')
+            ->with($shipRump)
+            ->once();
+        $toSignature->shouldReceive('setIsCloaked')
+            ->with($cloakState)
+            ->once();
+        $toSignature->shouldReceive('setFromDirection')
+            ->with($fromDirection)
+            ->once();
+        $toSignature->shouldReceive('setTime')
+            ->with(Mockery::type('int'))
+            ->once();
+
+        $this->flightSignatureRepository->shouldReceive('prototype')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($fromSignature);
+        $this->flightSignatureRepository->shouldReceive('prototype')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($toSignature);
+        $this->flightSignatureRepository->shouldReceive('save')
+            ->with($fromSignature)
+            ->once();
+        $this->flightSignatureRepository->shouldReceive('save')
+            ->with($toSignature)
+            ->once();
+
+        $this->subject->createOuterSystemSignatures(
+            $ship,
+            $toDirection,
+            $currentField,
+            $nextField
+        );
+    }
+
+    /**
+     * @dataProvider directionDataProvider
+     */
+    public function testCreateInnerSystemSignaturesCreates(
+        int $fromDirection,
+        int $toDirection
+    ): void {
+        $ship = $this->mock(ShipInterface::class);
+        $currentField = $this->mock(StarSystemMapInterface::class);
+        $nextField = $this->mock(StarSystemMapInterface::class);
+        $fromSignature = $this->mock(FlightSignatureInterface::class);
+        $toSignature = $this->mock(FlightSignatureInterface::class);
+        $shipRump = $this->mock(ShipRumpInterface::class);
+
+        $userId = 666;
+        $shipId = 42;
+        $shipName = 'some-name';
+        $cloakState = true;
+
+        $ship->shouldReceive('getUser->getId')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($userId);
+        $ship->shouldReceive('getId')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipId);
+        $ship->shouldReceive('getName')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipName);
+        $ship->shouldReceive('getRump')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($shipRump);
+        $ship->shouldReceive('getCloakState')
+            ->withNoArgs()
+            ->twice()
+            ->andReturn($cloakState);
+
+        $fromSignature->shouldReceive('setStarsystemMap')
+            ->with($currentField)
+            ->once();
+        $fromSignature->shouldReceive('setUserId')
+            ->with($userId)
+            ->once();
+        $fromSignature->shouldReceive('setShipId')
+            ->with($shipId)
+            ->once();
+        $fromSignature->shouldReceive('setShipName')
+            ->with($shipName)
+            ->once();
+        $fromSignature->shouldReceive('setRump')
+            ->with($shipRump)
+            ->once();
+        $fromSignature->shouldReceive('setIsCloaked')
+            ->with($cloakState)
+            ->once();
+        $fromSignature->shouldReceive('setToDirection')
+            ->with($toDirection)
+            ->once();
+        $fromSignature->shouldReceive('setTime')
+            ->with(Mockery::type('int'))
+            ->once();
+
+        $toSignature->shouldReceive('setStarsystemMap')
+            ->with($nextField)
+            ->once();
+        $toSignature->shouldReceive('setUserId')
+            ->with($userId)
+            ->once();
+        $toSignature->shouldReceive('setShipId')
+            ->with($shipId)
+            ->once();
+        $toSignature->shouldReceive('setShipName')
+            ->with($shipName)
+            ->once();
+        $toSignature->shouldReceive('setRump')
+            ->with($shipRump)
+            ->once();
+        $toSignature->shouldReceive('setIsCloaked')
+            ->with($cloakState)
+            ->once();
+        $toSignature->shouldReceive('setFromDirection')
+            ->with($fromDirection)
+            ->once();
+        $toSignature->shouldReceive('setTime')
+            ->with(Mockery::type('int'))
+            ->once();
+
+        $this->flightSignatureRepository->shouldReceive('prototype')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($fromSignature);
+        $this->flightSignatureRepository->shouldReceive('prototype')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($toSignature);
+        $this->flightSignatureRepository->shouldReceive('save')
+            ->with($fromSignature)
+            ->once();
+        $this->flightSignatureRepository->shouldReceive('save')
+            ->with($toSignature)
+            ->once();
+
+        $this->subject->createInnerSystemSignatures(
+            $ship,
+            $toDirection,
+            $currentField,
+            $nextField
+        );
+    }
+}
