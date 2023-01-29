b2ac5b763477594978637b17d64d553f8f770365
Daniel Jakob <github@usox.org>
2023-01-29 22:11:17 +0100

    Replace deprecated orm cli
    Also add missing entity for stu_planets_commodities

--- cli-config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-require_once __DIR__.'/src/Config/Bootstrap.php';
-
-return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
-    $container->get(\Doctrine\ORM\EntityManagerInterface::class)
-);
