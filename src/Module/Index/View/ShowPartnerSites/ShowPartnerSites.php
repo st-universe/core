8493365683afccc2bb2c2f0e4d74509c6ccd3db4
Daniel Jakob <github@usox.org>
2023-01-29 12:02:27 +0100

    Move partner sites into config
    Instead of using database items for a handful of static items, move 'em
    to the config

--- src/Module/Index/View/ShowPartnerSites/ShowPartnerSites.php
+++ src/Module/Index/View/ShowPartnerSites/ShowPartnerSites.php
@@ -4,27 +4,50 @@ declare(strict_types=1);
 
 namespace Stu\Module\Index\View\ShowPartnerSites;
 
+use Noodlehaus\ConfigInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
-use Stu\Orm\Repository\PartnerSiteRepositoryInterface;
 
+/**
+ * Shows all partner site items from the config
+ */
 final class ShowPartnerSites implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_PARTNER_SITES';
 
-    private PartnerSiteRepositoryInterface $partnerSiteRepository;
+    private ConfigInterface $config;
 
     public function __construct(
-        PartnerSiteRepositoryInterface $partnerSiteRepository
+        ConfigInterface $config
     ) {
-        $this->partnerSiteRepository = $partnerSiteRepository;
+        $this->config = $config;
     }
 
     public function handle(GameControllerInterface $game): void
     {
-        $game->setPageTitle(_('Partnerseiten - Star Trek Universe'));
+        $game->setPageTitle('Partnerseiten - Star Trek Universe');
         $game->setTemplateFile('html/index_partner_sites.xhtml');
 
-        $game->setTemplateVar('PARTNERSITES', $this->partnerSiteRepository->getOrdered());
+        $game->setTemplateVar('PARTNERSITES', $this->getPartnerSites());
+    }
+
+    /**
+     * @return array<array{name: string, description: string, url: string, banner_path: string}>
+     */
+    private function getPartnerSites(): array {
+        $baseBannerPath = $this->config->get('partner_sites.banner_path');
+
+        return array_map(
+            function (array $item) use ($baseBannerPath): array {
+                /** @var array{name: string, description: string, url: string, banner_path: string} $item */
+                $item['banner_path'] = sprintf(
+                    '/%s/%s.png',
+                    $baseBannerPath,
+                    $item['banner_path']
+                );
+                return $item;
+            },
+            $this->config->get('partner_sites.items', [])
+        );
     }
 }
---
+++ tests/Module/Index/View/ShowPartnerSites/ShowPartnerSitesTest.php
@@ -0,0 +1,81 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Index\View\ShowPartnerSites;
+
+use Generator;
+use Mockery;
+use Mockery\MockInterface;
+use Noodlehaus\ConfigInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\StuTestCase;
+
+class ShowPartnerSitesTest extends StuTestCase
+{
+    /** @var MockInterface&ConfigInterface */
+    private MockInterface $config;
+
+    private ShowPartnerSites $subject;
+
+    protected function setUp(): void
+    {
+        $this->config = $this->mock(ConfigInterface::class);
+
+        $this->subject = new ShowPartnerSites(
+            $this->config
+        );
+    }
+
+    public function testHandleShowsSites(): void
+    {
+        $game = $this->mock(GameControllerInterface::class);
+
+        $baseBannerPath = 'some-base-banner-path';
+        $siteName = 'some-site';
+        $siteDescription = 'some-fancy-site';
+        $siteUrl = 'some-url';
+        $siteBannerPath = 'some-banner-path';
+        $computedBannerPath = sprintf(
+            '/%s/%s.png',
+            $baseBannerPath,
+            $siteBannerPath
+        );
+
+        $game->shouldReceive('setPageTitle')
+            ->with('Partnerseiten - Star Trek Universe')
+            ->once();
+        $game->shouldReceive('setTemplateFile')
+            ->with('html/index_partner_sites.xhtml')
+            ->once();
+        $game->shouldReceive('setTemplateVar')
+            ->with(
+                'PARTNERSITES',
+                Mockery::on(fn (array $items): bool =>
+                    $items === [[
+                        'name' => $siteName,
+                        'description' => $siteDescription,
+                        'url' => $siteUrl,
+                        'banner_path' => $computedBannerPath,
+                    ]]
+                )
+            )
+            ->once();
+
+        $this->config->shouldReceive('get')
+            ->with('partner_sites.items', [])
+            ->once()
+            ->andReturn([[
+                'name' => $siteName,
+                'description' => $siteDescription,
+                'url' => $siteUrl,
+                'banner_path' => $siteBannerPath,
+            ]]);
+        $this->config->shouldReceive('get')
+            ->with('partner_sites.banner_path')
+            ->once()
+            ->andReturn($baseBannerPath);
+
+        $this->subject->handle($game);
+    }
+}
