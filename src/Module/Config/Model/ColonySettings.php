eab1a121976e346ac5c0fa0f90d6b6bdb45e20ad
Janko <jadari@web.de>
2023-02-16 22:22:21 +0100

    add unit tests

--- src/Module/Config/Model/ColonySettings.php
+++ src/Module/Config/Model/ColonySettings.php
@@ -6,16 +6,17 @@ use Stu\Module\Config\StuConfigException;
 
 final class ColonySettings extends AbstractSettings implements ColonySettingsInterface
 {
-    private const CONFIG_PATH = 'colony';
+    public const SETTING_TICK_WORKER_DEFAULT = 1;
 
+    private const CONFIG_PATH = 'colony';
     private const SETTING_TICK_WORKER = 'tick_worker';
 
     public function getTickWorker(): int
     {
-        $tickWorker = $this->getIntegerConfigValue(self::SETTING_TICK_WORKER, 1);
+        $tickWorker = $this->getIntegerConfigValue(self::SETTING_TICK_WORKER, self::SETTING_TICK_WORKER_DEFAULT);
 
         if ($tickWorker < 1) {
-            throw new StuConfigException(sprintf('Invalid value for %s.%s, should be greater than 0.', $this->getPath(), self::SETTING_TICK_WORKER));
+            throw new StuConfigException(sprintf('Invalid value for "%s.%s", should be greater than 0.', $this->getPath(), self::SETTING_TICK_WORKER));
         }
 
         return $tickWorker;
