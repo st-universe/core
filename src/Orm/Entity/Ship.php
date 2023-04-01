fe34186fcebdfd55ab3ad155e2d4916c89d8badd
Huxinator <52821266+Huxinator@users.noreply.github.com>
2023-04-01 23:54:50 +0200

    fix

--- src/Orm/Entity/Ship.php
+++ src/Orm/Entity/Ship.php
@@ -337,7 +337,7 @@ class Ship implements ShipInterface
     /**
      * @var ArrayCollection<int, ShipCrewInterface>
      *
-     * @OneToMany(targetEntity="ShipCrew", mappedBy="ship", indexBy="id")
+     * @OneToMany(targetEntity="ShipCrew", mappedBy="ship", indexBy="id", cascade={"remove"})
      * @OrderBy({"id": "ASC"})
      */
     private $crew;
