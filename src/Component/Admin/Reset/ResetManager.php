9ae887772cba13e6769b84be0ebafd9b061c1749
Huxinator <52821266+Huxinator@users.noreply.github.com>
2023-04-03 17:11:37 +0200

    try

--- src/Component/Admin/Reset/ResetManager.php
+++ src/Component/Admin/Reset/ResetManager.php
@@ -149,7 +149,7 @@ final class ResetManager implements ResetManagerInterface
     {
         foreach ($this->rpgPlotRepository->findAll() as $plot) {
             //echo "plot:" . $plot->getId() . ", memberCount:" . $plot->getMemberCount() . "\n";
-            /**
+            /* 
              if ($plot->getMemberCount() > 0) {
                 foreach ($plot->getMembers() as $member) {
                     //echo "\tmember:" . $member->getUser()->getId() . "\n";
