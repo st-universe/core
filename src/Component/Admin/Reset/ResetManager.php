4f8c29f5533c21c0d73a418e725a4011d50b65c3
Huxinator <52821266+Huxinator@users.noreply.github.com>
2023-04-03 18:43:13 +0200

    next

--- src/Component/Admin/Reset/ResetManager.php
+++ src/Component/Admin/Reset/ResetManager.php
@@ -71,11 +71,11 @@ final class ResetManager implements ResetManagerInterface
 
             $this->entityManager->flush();
 
-            $this->resetColonySurfaceMasks();
-            //$this->deleteKnPostings();
-            $this->deleteKnPlotMembers();
+            //$this->resetColonySurfaceMasks();
+            $this->deleteKnPostings();
+            //$this->deleteKnPlotMembers();
             //$this->deleteKnPlots();
-            $this->deleteHistory();
+            //$this->deleteHistory();
             //$this->resetGameTurns();
 
             // clear game data
