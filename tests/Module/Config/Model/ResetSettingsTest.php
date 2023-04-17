45c1afe7b957d5d1c1b61ee5cc6d7fdf668536ce
Janko <jadari@web.de>
2023-04-17 14:33:55 +0200

    add reset settings

---
+++ tests/Module/Config/Model/ResetSettingsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Config\Model;
+
+use Mockery\MockInterface;
+use Noodlehaus\ConfigInterface;
+use PHPUnit\Framework\MockObject\MockObject;
+use Stu\Module\Config\StuConfigException;
+use Stu\StuTestCase;
+
+class ResetSettingsTest extends StuTestCase
+{
+    /** @var MockInterface|ConfigInterface */
+    private ConfigInterface $config;
+
+    /** @var MockObject|ResetSettings */
+    private ResetSettings $subject;
+
+    public function setUp(): void
+    {
+        $this->config = $this->mock(ConfigInterface::class);
+
+        $this->subject = new ResetSettings(null, $this->config);
+    }
+
+    public function testGetDelayInSecondsExpectConfigValueWhenPresent(): void
+    {
+        $this->config->shouldReceive('get')
+            ->with('reset.delay_in_seconds')
+            ->once()
+            ->andReturn(15);
+
+        $result = $this->subject->getDelayInSeconds();
+
+        $this->assertEquals(15, $result);
+    }
+
+    public function testGetDelayInSecondsExpectDefaultWhenNotPresent(): void
+    {
+        $this->config->shouldReceive('get')
+            ->with('reset.delay_in_seconds')
+            ->once()
+            ->andReturn(null);
+
+        $result = $this->subject->getDelayInSeconds();
+
+        $this->assertEquals(5, $result);
+    }
+
+    public function testGetDelayInSecondsExpectErrorWhenNotAnInteger(): void
+    {
+        static::expectExceptionMessage('The value "foo" with path "reset.delay_in_seconds" is no valid integer.');
+        static::expectException(StuConfigException::class);
+
+        $this->config->shouldReceive('get')
+            ->with('reset.delay_in_seconds')
+            ->once()
+            ->andReturn('foo');
+
+        $this->subject->getDelayInSeconds();
+    }
+}
