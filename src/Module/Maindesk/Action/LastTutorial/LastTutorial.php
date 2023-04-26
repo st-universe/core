a6c4943db7f53411fcdbaa064728cf12eb1860ba
Huxinator <nicotintel@gmail.com>
2023-04-26 22:27:54 +0200

    try

--- src/Module/Maindesk/Action/LastTutorial/LastTutorial.php
+++ src/Module/Maindesk/Action/LastTutorial/LastTutorial.php
@@ -33,13 +33,9 @@ final class LastTutorial implements ActionControllerInterface
 
         if ((int) $user->getState() === UserEnum::USER_STATE_TUTORIAL2) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL1);
-        }
-
-        if ((int) $user->getState() === UserEnum::USER_STATE_TUTORIAL3) {
+        } elseif ((int) $user->getState() === UserEnum::USER_STATE_TUTORIAL3) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL2);
-        }
-
-        if ((int) $user->getState() === UserEnum::USER_STATE_TUTORIAL4) {
+        } elseif ((int) $user->getState() === UserEnum::USER_STATE_TUTORIAL4) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL3);
         }
 
