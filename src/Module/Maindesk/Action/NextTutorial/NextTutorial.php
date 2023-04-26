b0745ff3663280acb0d76ffa2edff5ac77503a54
Huxinator <nicotintel@gmail.com>
2023-04-26 22:08:12 +0200

    try

--- src/Module/Maindesk/Action/NextTutorial/NextTutorial.php
+++ src/Module/Maindesk/Action/NextTutorial/NextTutorial.php
@@ -32,23 +32,21 @@ final class NextTutorial implements ActionControllerInterface
 
         if ((int) $user->getState() == UserEnum::USER_STATE_TUTORIAL1) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL2);
-            $this->userRepository->save($user);
         }
 
         if ((int) $user->getState() == UserEnum::USER_STATE_TUTORIAL2) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL3);
-            $this->userRepository->save($user);
         }
 
         if ((int) $user->getState() == UserEnum::USER_STATE_TUTORIAL3) {
             $user->setState(UserEnum::USER_STATE_TUTORIAL4);
-            $this->userRepository->save($user);
         }
 
         if ((int) $user->getState() == UserEnum::USER_STATE_TUTORIAL4) {
             $user->setState(UserEnum::USER_STATE_ACTIVE);
-            $this->userRepository->save($user);
         }
+
+        $this->userRepository->save($user);
     }
 
 
