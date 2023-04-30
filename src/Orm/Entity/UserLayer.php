2d6f36a23f542d2bb64085a79eabfa036fb246de
Huxinator <nicotintel@gmail.com>
2023-04-30 22:48:36 +0200

    fix

--- src/Orm/Entity/UserLayer.php
+++ src/Orm/Entity/UserLayer.php
@@ -43,7 +43,7 @@ class UserLayer implements UserLayerInterface
     /**
      * @var UserInterface
      *
-     * @ManyToOne(targetEntity="User")
+     * @ManyToOne(targetEntity="User", cascade={"persist"})
      * @JoinColumn(name="user_id", referencedColumnName="id", onDelete="CASCADE")
      */
     private $user;
