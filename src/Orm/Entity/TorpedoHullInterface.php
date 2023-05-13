145f3ccaf4a60b582ddcf17f1234e11e3d195b84
Huxinator <52821266+Huxinator@users.noreply.github.com>
2023-05-13 15:36:08 +0200

    fix

--- src/Orm/Entity/TorpedoHullInterface.php
+++ src/Orm/Entity/TorpedoHullInterface.php
@@ -26,7 +26,7 @@ interface TorpedoHullInterface
 
     public function hexToRgb(string $color): array;
 
-    public function calculateGradientRgb(string $rgb1, string $rgb2, float $percent): array;
+    public function calculateGradientRgb(array $rgb1, array $rgb2, float $percent): array;
 
-    public function rgbToHex(string $rgb): string;
+    public function rgbToHex(array $rgb): string;
 }
