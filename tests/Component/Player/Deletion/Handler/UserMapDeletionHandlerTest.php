1646c8ef61b4a2da81463b3603602710417a6e2d
Janko <jadari@web.de>
2023-05-13 21:47:12 +0200

    fix unit test

--- tests/Component/Player/Deletion/Handler/UserMapDeletionHandlerTest.php
+++ tests/Component/Player/Deletion/Handler/UserMapDeletionHandlerTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Stu\Component\Player\Deletion\Handler;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Mockery;
 use Mockery\MockInterface;
 use Stu\Orm\Entity\UserInterface;
+use Stu\Orm\Entity\UserLayerInterface;
+use Stu\Orm\Repository\UserLayerRepositoryInterface;
 use Stu\Orm\Repository\UserMapRepositoryInterface;
 use Stu\StuTestCase;
 
@@ -18,6 +21,11 @@ class UserMapDeletionHandlerTest extends StuTestCase
     private $userMapRepository;
 
     /**
+     * @var null|MockInterface|UserLayerRepositoryInterface
+     */
+    private $userLayerRepository;
+
+    /**
      * @var null|UserMapDeletionHandler
      */
     private $handler;
@@ -25,23 +33,35 @@ class UserMapDeletionHandlerTest extends StuTestCase
     public function setUp(): void
     {
         $this->userMapRepository = $this->mock(UserMapRepositoryInterface::class);
+        $this->userLayerRepository = $this->mock(UserLayerRepositoryInterface::class);
 
         $this->handler = new UserMapDeletionHandler(
-            $this->userMapRepository
+            $this->userMapRepository,
+            $this->userLayerRepository
         );
     }
 
     public function testDeleteDeletesUserMapEntries(): void
     {
         $user = Mockery::mock(UserInterface::class);
+        $userLayer = Mockery::mock(UserLayerInterface::class);
+        $userLayers = new ArrayCollection([$userLayer]);
 
         $user->shouldReceive('getId')
             ->withNoArgs()
             ->once()
             ->andReturn(42);
+        $user->shouldReceive('getUserLayers')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($userLayers);
 
         $this->userMapRepository->shouldReceive('truncateByUser')
             ->with(42)
+            ->once();
+
+        $this->userLayerRepository->shouldReceive('delete')
+            ->with($userLayer)
             ->once();
 
         $this->handler->delete($user);
