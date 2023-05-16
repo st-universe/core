d4c2730e31cd2e8128818b9c43b162899aa662da
Huxinator <nicotintel@gmail.com>
2023-05-16 10:56:28 +0200

    fix

--- src/Orm/Repository/WeaponShieldRepository.php
+++ src/Orm/Repository/WeaponShieldRepository.php
@@ -45,13 +45,19 @@ final class WeaponShieldRepository extends EntityRepository implements WeaponShi
 
     public function getFactionByModule($moduleid): array
     {
+        $results = [];
+
         for ($index = 1; $index <= 5; $index++) {
-            return $this->findBy(
-                [
-                    'faction_id' => $index,
-                    'module_id' => $moduleid
-                ]
-            );
+            $result = $this->findBy([
+                'faction_id' => $index,
+                'module_id' => $moduleid
+            ]);
+
+            if ($result) {
+                $results[] = $result;
+            }
         }
+
+        return $results;
     }
 }
