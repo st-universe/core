8b02f0915ac54d7ed7405153d3797b3a6a3373c8
Huxinator <nicotintel@gmail.com>
2023-05-20 13:13:46 +0200

    try

--- src/Public/static/js/ship.js
+++ src/Public/static/js/ship.js
@@ -355,11 +355,11 @@ function maximizeCommodityAmounts() {
 }
 function adjustCellHeight(image) {
 	var cell = image.parentNode.parentNode;
-	var height = image.offsetHeight;
-	cell.style.height = height + 10 + 'px';
+	var height = image.offsetHeight + 10;
+	cell.style.height = height + 'px';
 }
 function adjustCellWidth(image) {
 	var cell = image.parentNode.parentNode;
-	var width = image.offsetWidth;
-	cell.style.width = width + 10 + 'px';
+	var width = image.offsetWidth + 10;
+	cell.style.width = width + 'px';
 }
