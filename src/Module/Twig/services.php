8702f733bbfc21a62e3d8c073a1f13c3f17db3f6
Janko <jadari@web.de>
2023-05-25 09:33:05 +0200

    use GAME as global template variable

--- src/Module/Twig/services.php
+++ src/Module/Twig/services.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Stu\Module\Twig;
 
 use Psr\Container\ContainerInterface;
-use RuntimeException;
 use Stu\Module\Config\StuConfigInterface;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
