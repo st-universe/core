6c9ad14ae7ae238303342c8636a658c6f4bc88e0
Janko <jadari@web.de>
2023-07-14 23:13:39 +0200

    enter waypoint module

---
+++ src/Module/Ship/Lib/Movement/Route/EnterWaypoint.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Ship\Lib\Movement\Route;
+
+use Stu\Lib\InformationWrapper;
+use Stu\Module\Control\StuTime;
+use Stu\Module\Ship\Lib\Movement\Component\FlightSignatureCreatorInterface;
+use Stu\Orm\Entity\MapInterface;
+use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\StarSystemMapInterface;
+use Stu\Orm\Entity\WormholeEntryInterface;
+use Stu\Orm\Repository\WormholeEntryRepositoryInterface;
+
+final class EnterWaypoint implements EnterWaypointInterface
+{
+    private FlightSignatureCreatorInterface $flightSignatureCreator;
+
+    private UpdateFlightDirectionInterface $updateFlightDirection;
+
+    private WormholeEntryRepositoryInterface $wormholeEntryRepository;
+
+    private CheckAstronomicalWaypointsInterface $checkAstronomicalWaypoints;
+
+    private StuTime $stuTime;
+
+    public function __construct(
+        FlightSignatureCreatorInterface $flightSignatureCreator,
+        UpdateFlightDirectionInterface $updateFlightDirection,
+        WormholeEntryRepositoryInterface $wormholeEntryRepository,
+        CheckAstronomicalWaypointsInterface $checkAstronomicalWaypoints,
+        StuTime $stuTime
+    ) {
+        $this->flightSignatureCreator = $flightSignatureCreator;
+        $this->updateFlightDirection = $updateFlightDirection;
+        $this->wormholeEntryRepository = $wormholeEntryRepository;
+        $this->checkAstronomicalWaypoints = $checkAstronomicalWaypoints;
+        $this->stuTime = $stuTime;
+    }
+
+    public function enterNextWaypoint(
+        ShipInterface $ship,
+        bool $isTraversing,
+        MapInterface|StarSystemMapInterface $waypoint,
+        ?WormholeEntryInterface $wormholeEntry,
+        InformationWrapper $informations
+    ): void {
+        $oldWaypoint = $ship->getCurrentMapField();
+
+        $ship->updateLocation(
+            $waypoint instanceof MapInterface ? $waypoint : null,
+            $waypoint instanceof StarSystemMapInterface ? $waypoint : null
+        );
+
+        if ($this->isWormhole($waypoint)) {
+            $ship->setCx(0);
+            $ship->setCy(0);
+        }
+
+        if ($wormholeEntry !== null) {
+            $wormholeEntry->setLastUsed($this->stuTime->time());
+            $this->wormholeEntryRepository->save($wormholeEntry);
+        }
+
+        if ($isTraversing) {
+            $flightDirection = $this->updateFlightDirection->update($oldWaypoint, $waypoint, $ship);
+
+            //create flight signatures
+            $this->flightSignatureCreator->createSignatures(
+                $ship,
+                $flightDirection,
+                $oldWaypoint,
+                $waypoint,
+            );
+        }
+
+        //check astro stuff for tractor
+        if ($waypoint instanceof StarSystemMapInterface) {
+            $this->checkAstronomicalWaypoints->checkWaypoint($ship, $waypoint, $informations);
+        }
+    }
+
+    private function isWormhole(MapInterface|StarSystemMapInterface $waypoint): bool
+    {
+        if ($waypoint instanceof MapInterface) {
+            return false;
+        }
+
+        return $waypoint->getSystem()->isWormhole();
+    }
+}
---
+++ src/Module/Ship/Lib/Movement/Route/EnterWaypointInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Ship\Lib\Movement\Route;
+
+use Stu\Lib\InformationWrapper;
+use Stu\Orm\Entity\MapInterface;
+use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\StarSystemMapInterface;
+use Stu\Orm\Entity\WormholeEntryInterface;
+
+interface EnterWaypointInterface
+{
+    public function enterNextWaypoint(
+        ShipInterface $ship,
+        bool $isTraversing,
+        MapInterface|StarSystemMapInterface $waypoint,
+        ?WormholeEntryInterface $wormholeEntry,
+        InformationWrapper $informations
+    ): void;
+}
