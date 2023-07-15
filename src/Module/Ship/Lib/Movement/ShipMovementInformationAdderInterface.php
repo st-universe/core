400e4cfd769964a12e52206a8c43426d73b3a021
Janko <jadari@web.de>
2023-07-15 20:39:16 +0200

    movement information adder module

---
+++ src/Module/Ship/Lib/Movement/ShipMovementInformationAdderInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Stu\Module\Ship\Lib\Movement;
+
+use Stu\Lib\InformationWrapper;
+use Stu\Orm\Entity\ShipInterface;
+
+interface ShipMovementInformationAdderInterface
+{
+    public function reachedDestination(
+        ShipInterface $ship,
+        bool $isFleetMode,
+        int $routeMode,
+        InformationWrapper $informations
+    ): void;
+
+    public function pulledTractoredShip(
+        string $tractoredShipName,
+        int $routeMode,
+        InformationWrapper $informations
+    ): void;
+
+    public function notEnoughEnergyforTractoring(
+        ShipInterface $ship,
+        int $routeMode,
+        InformationWrapper $informations
+    ): void;
+}
