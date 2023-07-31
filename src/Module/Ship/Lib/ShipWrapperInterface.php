f5ebbd9a35e9684cf33853b1119913d12aef1878
Huxinator <nicotintel@gmail.com>
2023-07-31 18:50:40 +0200

    layout

--- src/Module/Ship/Lib/ShipWrapperInterface.php
+++ src/Module/Ship/Lib/ShipWrapperInterface.php
@@ -30,6 +30,8 @@ interface ShipWrapperInterface
 
     public function getEffectiveEpsProduction(): int;
 
+    public function getEffectiveWarpDriveProduction(): int;
+
     public function getWarpcoreUsage(): int;
 
     public function setAlertState(int $alertState): ?string;
