c98373f780b5c9868fe8646464c886320a9ca717
Janko <jadari@web.de>
2023-08-31 20:18:52 +0200

    standard normal distribution as stuRandom option

--- src/Module/Control/StuRandom.php
+++ src/Module/Control/StuRandom.php
@@ -7,13 +7,33 @@ namespace Stu\Module\Control;
  */
 class StuRandom
 {
-    public function rand(int $min, int $max): int
+    public function rand(int $min, int $max, bool $useStandardNormalDistribution = false): int
     {
+        if ($useStandardNormalDistribution) {
+            return $this->generateRandomValueStandardNormalDistribution($min, $max);
+        }
+
         return random_int($min, $max);
     }
 
     public function array_rand(array $array): string|int
     {
         return array_rand($array);
+    }
+
+    private function generateRandomValueStandardNormalDistribution(int $min, int $max): int
+    {
+        $mean = (int)(($min + $max) / 2); // MW
+        $stdDeviation = (int) ($mean / 2.5); // FWHM
+
+        do {
+            $value = random_int($min, $max);
+            $probability = exp(-0.5 * (($value - $mean) / $stdDeviation) ** 2); // normal distribution
+            $randomProbability = random_int(0, mt_getrandmax()) / mt_getrandmax();
+
+            if ($randomProbability <= $probability) {
+                return $value;
+            }
+        } while (true);
     }
 }
