4e95a4e4dc0782276cb68fe01ff8b0404d7e3894
Janko <jadari@web.de>
2023-09-04 13:38:42 +0200

    prefetch random system names

--- src/Component/StarSystem/GenerateEmptySystems.php
+++ src/Component/StarSystem/GenerateEmptySystems.php
@@ -8,22 +8,29 @@ use RuntimeException;
 use Stu\Component\StarSystem\StarSystemCreationInterface;
 use Stu\Orm\Repository\LayerRepositoryInterface;
 use Stu\Orm\Repository\MapRepositoryInterface;
+use Stu\Orm\Repository\StarSystemRepositoryInterface;
 
 final class GenerateEmptySystems implements GenerateEmptySystemsInterface
 {
+    public const BATCH_AMOUNT = 10;
+
     private LayerRepositoryInterface $layerRepository;
 
     private MapRepositoryInterface $mapRepository;
+
+    private StarSystemRepositoryInterface $starSystemRepository;
 
     private StarSystemCreationInterface $starSystemCreation;
 
     public function __construct(
         LayerRepositoryInterface $layerRepository,
         MapRepositoryInterface $mapRepository,
+        StarSystemRepositoryInterface $starSystemRepository,
         StarSystemCreationInterface $starSystemCreation
     ) {
         $this->layerRepository = $layerRepository;
         $this->mapRepository = $mapRepository;
+        $this->starSystemRepository = $starSystemRepository;
         $this->starSystemCreation = $starSystemCreation;
     }
 
@@ -38,13 +45,15 @@ final class GenerateEmptySystems implements GenerateEmptySystemsInterface
 
         $count = 0;
 
-        foreach ($mapArray as $map) {
-            $this->starSystemCreation->recreateStarSystem($map);
-            $count++;
+        $randomNames = $this->starSystemRepository->getRandomFreeSystemNames(self::BATCH_AMOUNT);
 
-            if ($count === 10) {
+        foreach ($mapArray as $map) {
+            if ($count === self::BATCH_AMOUNT) {
                 break;
             }
+
+            $this->starSystemCreation->recreateStarSystem($map,  $randomNames[$count]->getName());
+            $count++;
         }
 
         return $count;
