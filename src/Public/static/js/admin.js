62183f7a800e9ec8d4b901d7224158bd5093cc25
Janko <jadari@web.de>
2023-09-05 10:24:33 +0200

    system editor navigation via keys

--- src/Public/static/js/admin.js
+++ src/Public/static/js/admin.js
@@ -3,3 +3,20 @@ function showUserLock(userid) {
 	openPJsWin('elt', 1);
 	ajax_update('elt', '/admin/?SHOW_USER_LOCK=1&uid=' + userid);
 }
+
+function registerSystemEditorNavKey() {
+	document.addEventListener("keydown", (event) => {
+		if (event.key === "ArrowLeft") {
+			document.location.href = '?SHOW_SYSTEM=1&sysid=' + previousId;
+		}
+		if (event.key === "ArrowRight") {
+			document.location.href = '?SHOW_SYSTEM=1&sysid=' + nextId;
+		}
+		if (event.key === "ArrowUp") {
+			document.location.href = '?REGENERATE_SYSTEM=1&sysid=' + currentId;
+		}
+		if (event.key === "ArrowDown") {
+			document.location.href = '?SHOW_SYSTEM=1&sysid=' + currentId;
+		}
+	});
+}
