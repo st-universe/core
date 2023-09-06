22eafc2bb0869b8a6e6af0124caed1afe0509c77
Janko <jadari@web.de>
2023-09-06 22:28:41 +0200

    delete idle users at beginning and flush

--- src/Module/Maintenance/services.php
+++ src/Module/Maintenance/services.php
@@ -8,20 +8,21 @@ use function DI\autowire;
 use function DI\get;
 
 return [
-    DatabaseBackup::class => autowire(DatabaseBackup::class),
-    MapCycle::class => autowire(MapCycle::class),
-    OldTachyonScanDeletion::class => autowire(OldTachyonScanDeletion::class),
-    OldTradeOffersDeletion::class => autowire(OldTradeOffersDeletion::class),
-    CorruptFleetDeletion::class => autowire(CorruptFleetDeletion::class),
-    OldFlightSignatureDeletion::class => autowire(OldFlightSignatureDeletion::class),
     ColonyCorrectorHandler::class => autowire(ColonyCorrectorHandler::class),
+    CorruptFleetDeletion::class => autowire(CorruptFleetDeletion::class),
+    DatabaseBackup::class => autowire(DatabaseBackup::class),
     EmptyPlotDeletion::class => autowire(EmptyPlotDeletion::class),
-    IdleUserDeletion::class => autowire(IdleUserDeletion::class),
-    OldTradeLicenseDeletion::class => autowire(OldTradeLicenseDeletion::class),
-    TopFlightsReward::class => autowire(TopFlightsReward::class),
     EndLotteryPeriod::class => autowire(EndLotteryPeriod::class),
+    IdleUserDeletion::class => autowire(IdleUserDeletion::class),
+    MapCycle::class => autowire(MapCycle::class),
+    OldFlightSignatureDeletion::class => autowire(OldFlightSignatureDeletion::class),
+    OldTachyonScanDeletion::class => autowire(OldTachyonScanDeletion::class),
+    OldTradeLicenseDeletion::class => autowire(OldTradeLicenseDeletion::class),
+    OldTradeOffersDeletion::class => autowire(OldTradeOffersDeletion::class),
+    TopFlightsReward::class => autowire(TopFlightsReward::class),
     'maintenance_handler' => [
         get(DatabaseBackup::class),
+        get(IdleUserDeletion::class),
         get(MapCycle::class),
         get(OldTachyonScanDeletion::class),
         get(OldTradeOffersDeletion::class),
@@ -29,7 +30,6 @@ return [
         get(OldFlightSignatureDeletion::class),
         get(ColonyCorrectorHandler::class),
         get(EmptyPlotDeletion::class),
-        get(IdleUserDeletion::class),
         get(OldTradeLicenseDeletion::class),
         get(TopFlightsReward::class),
         get(EndLotteryPeriod::class)
