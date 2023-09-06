5fc34a48480cae23ae83bb3fc094a34a230a83d6
Janko <jadari@web.de>
2023-09-06 22:28:41 +0200

    delete idle users at beginning and flush

--- src/Module/Maintenance/IdleUserDeletion.php
+++ src/Module/Maintenance/IdleUserDeletion.php
@@ -2,20 +2,26 @@
 
 namespace Stu\Module\Maintenance;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Stu\Component\Player\Deletion\PlayerDeletionInterface;
 
 final class IdleUserDeletion implements MaintenanceHandlerInterface
 {
     private PlayerDeletionInterface $playerDeletion;
 
+    private EntityManagerInterface $entityManager;
+
     public function __construct(
-        PlayerDeletionInterface $playerDeletion
+        PlayerDeletionInterface $playerDeletion,
+        EntityManagerInterface $entityManager
     ) {
         $this->playerDeletion = $playerDeletion;
+        $this->entityManager = $entityManager;
     }
 
     public function handle(): void
     {
         $this->playerDeletion->handleDeleteable();
+        $this->entityManager->flush();
     }
 }
