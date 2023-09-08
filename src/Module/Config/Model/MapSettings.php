829fc245f0bbe84c7fe57112e0fca99a6d0a72b3
Janko <jadari@web.de>
2023-09-08 09:39:17 +0200

    map encryption key setting

---
+++ src/Module/Config/Model/MapSettings.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Stu\Module\Config\Model;
+
+final class MapSettings extends AbstractSettings implements MapSettingsInterface
+{
+    private const CONFIG_PATH = 'map';
+    private const SETTING_ENCRYPTION_KEY = 'encryptionKey';
+
+    public function getEncryptionKey(): ?string
+    {
+        if ($this->exists(self::SETTING_ENCRYPTION_KEY)) {
+            $key = $this->getStringConfigValue(self::SETTING_ENCRYPTION_KEY);
+
+            return $key;
+        }
+
+        return null;
+    }
+
+    public function getConfigPath(): string
+    {
+        return self::CONFIG_PATH;
+    }
+}
