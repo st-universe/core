b757a4a1b6184ee3fb52e76d3b443b754e19f76f
Janko <jadari@web.de>
2023-09-16 10:52:06 +0200

    negative prestige for selfdestruction

--- src/Module/Ship/Action/SelfDestruct/SelfDestruct.php
+++ src/Module/Ship/Action/SelfDestruct/SelfDestruct.php
@@ -8,13 +8,16 @@ use request;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\History\Lib\EntryCreatorInterface;
+use Stu\Module\Message\Lib\PrivateMessageSenderInterface;
 use Stu\Module\PlayerSetting\Lib\UserEnum;
+use Stu\Module\Prestige\Lib\CreatePrestigeLogInterface;
 use Stu\Module\Ship\Lib\Battle\AlertRedHelperInterface;
 use Stu\Module\Ship\Lib\ShipLoaderInterface;
 use Stu\Module\Ship\Lib\ShipRemoverInterface;
 use Stu\Module\Ship\Lib\ShipRumpSpecialAbilityEnum;
 use Stu\Module\Ship\View\Overview\Overview;
 use Stu\Module\Ship\View\ShowShip\ShowShip;
+use Stu\Orm\Entity\UserInterface;
 use Stu\Orm\Repository\ShipRepositoryInterface;
 
 final class SelfDestruct implements ActionControllerInterface
@@ -31,18 +34,26 @@ final class SelfDestruct implements ActionControllerInterface
 
     private AlertRedHelperInterface $alertRedHelper;
 
+    private CreatePrestigeLogInterface $createPrestigeLog;
+
+    private PrivateMessageSenderInterface $privateMessageSender;
+
     public function __construct(
         ShipLoaderInterface $shipLoader,
         EntryCreatorInterface $entryCreator,
         ShipRemoverInterface $shipRemover,
         ShipRepositoryInterface $shipRepository,
-        AlertRedHelperInterface $alertRedHelper
+        AlertRedHelperInterface $alertRedHelper,
+        CreatePrestigeLogInterface $createPrestigeLog,
+        PrivateMessageSenderInterface $privateMessageSender
     ) {
         $this->shipLoader = $shipLoader;
         $this->entryCreator = $entryCreator;
         $this->shipRemover = $shipRemover;
         $this->shipRepository = $shipRepository;
         $this->alertRedHelper = $alertRedHelper;
+        $this->createPrestigeLog = $createPrestigeLog;
+        $this->privateMessageSender = $privateMessageSender;
     }
 
     public function handle(GameControllerInterface $game): void
@@ -99,6 +110,9 @@ final class SelfDestruct implements ActionControllerInterface
             );
         }
 
+        $prestigeAmount = $ship->getRump()->getPrestige();
+        $rumpName = $ship->getRump()->getName();
+
         $destroyMsg = $this->shipRemover->destroy($wrapper);
         if ($destroyMsg !== null) {
             $game->addInformation($destroyMsg);
@@ -112,6 +126,31 @@ final class SelfDestruct implements ActionControllerInterface
         if ($user->getState() == UserEnum::USER_STATE_COLONIZATION_SHIP && $this->shipRepository->getAmountByUserAndSpecialAbility($userId, ShipRumpSpecialAbilityEnum::COLONIZE) === 1) {
             $user->setState(UserEnum::USER_STATE_UNCOLONIZED);
         }
+
+        $this->createNegativePrestigeLog($prestigeAmount, $rumpName, $user);
+    }
+
+    private function createNegativePrestigeLog(int $prestigeAmount, string $rumpName, UserInterface $user): void
+    {
+        $amount = -(int)abs($prestigeAmount);
+
+        $description = sprintf(
+            '[b][color=red]%d[/color][/b] Prestige erhalten für die Selbstzerstörung von: %s',
+            $amount,
+            $rumpName
+        );
+
+        $this->createPrestigeLog->createLog($amount, $description, $user, time());
+        $this->sendSystemMessage($description, $user);
+    }
+
+    private function sendSystemMessage(string $description, UserInterface $user): void
+    {
+        $this->privateMessageSender->send(
+            UserEnum::USER_NOONE,
+            $user->getId(),
+            $description
+        );
     }
 
     public function performSessionCheck(): bool
