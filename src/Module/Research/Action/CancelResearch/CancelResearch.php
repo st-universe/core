6cae6ee3c33863f0a562ad24722e801009c126a5
Janko <jadari@web.de>
2023-11-11 11:42:33 +0100

    cancel right research

--- src/Module/Research/Action/CancelResearch/CancelResearch.php
+++ src/Module/Research/Action/CancelResearch/CancelResearch.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Stu\Module\Research\Action\CancelResearch;
 
+use request;
 use Stu\Module\Control\AuthenticatedActionController;
 use Stu\Module\Control\GameController;
 use Stu\Module\Control\GameControllerInterface;
@@ -26,12 +27,15 @@ final class CancelResearch extends AuthenticatedActionController
 
     public function handle(GameControllerInterface $game): void
     {
+        $id = request::getIntFatal('id');
+
         $currentResearch = $this->researchedRepository->getCurrentResearch($game->getUser());
 
-        if (!empty($currentResearch)) {
-            $this->researchedRepository->delete(current($currentResearch));
-
-            $game->addInformation('Die laufende Forschung wurde abgebrochen');
+        foreach ($currentResearch as $researched) {
+            if ($researched->getId() === $id) {
+                $this->researchedRepository->delete($researched);
+                $game->addInformation('Die laufende Forschung wurde abgebrochen');
+            }
         }
         $game->setView(GameController::DEFAULT_VIEW);
     }
