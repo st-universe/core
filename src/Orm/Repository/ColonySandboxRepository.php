5707b1d143c082b2c3920ea6f21a6117dfb633ab
Janko <jadari@web.de>
2023-11-20 11:58:28 +0100

    extend colony sandbox

--- src/Orm/Repository/ColonySandboxRepository.php
+++ src/Orm/Repository/ColonySandboxRepository.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Stu\Orm\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Stu\Orm\Entity\Colony;
+use Stu\Orm\Entity\ColonyInterface;
 use Stu\Orm\Entity\ColonySandbox;
 use Stu\Orm\Entity\ColonySandboxInterface;
+use Stu\Orm\Entity\UserInterface;
 
 /**
  * @extends EntityRepository<ColonySandbox>
@@ -30,5 +33,36 @@ final class ColonySandboxRepository extends EntityRepository implements ColonySa
         $em = $this->getEntityManager();
 
         $em->remove($colonySandbox);
+    }
+
+    public function getByUser(UserInterface $user): array
+    {
+        return $this->getEntityManager()
+            ->createQuery(sprintf(
+                'SELECT cs
+                FROM %s cs
+                JOIN %s c
+                WITH cs.colony = c
+                WHERE c.user = :user',
+                ColonySandbox::class,
+                Colony::class
+            ))
+            ->setParameter('user', $user)
+            ->getResult();
+    }
+
+    public function truncateByColony(ColonyInterface $colony): void
+    {
+        $this->getEntityManager()
+            ->createQuery(
+                sprintf(
+                    'DELETE FROM %s cs WHERE cs.colony = :colony',
+                    ColonySandbox::class
+                )
+            )
+            ->setParameters([
+                'colony' => $colony
+            ])
+            ->execute();
     }
 }
