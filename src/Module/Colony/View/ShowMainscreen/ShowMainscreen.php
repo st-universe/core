1869b92349c95bfb108796aca5f87cf2ff2a54ed
Janko <jadari@web.de>
2023-11-21 14:22:37 +0100

    add colony mainscreen view controller

---
+++ src/Module/Colony/View/ShowMainscreen/ShowMainscreen.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Colony\View\ShowMainscreen;
+
+use Stu\Component\Colony\ColonyMenuEnum;
+use Stu\Lib\Colony\PlanetFieldHostProviderInterface;
+use Stu\Module\Colony\Lib\Gui\ColonyGuiHelperInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Control\ViewControllerInterface;
+
+final class ShowMainscreen implements ViewControllerInterface
+{
+    public const VIEW_IDENTIFIER = 'SHOW_MAINSCREEN';
+
+    private PlanetFieldHostProviderInterface $planetFieldHostProvider;
+
+    private ColonyGuiHelperInterface $colonyGuiHelper;
+
+    public function __construct(
+        PlanetFieldHostProviderInterface $planetFieldHostProvider,
+        ColonyGuiHelperInterface $colonyGuiHelper
+    ) {
+        $this->planetFieldHostProvider = $planetFieldHostProvider;
+        $this->colonyGuiHelper = $colonyGuiHelper;
+    }
+
+    public function handle(GameControllerInterface $game): void
+    {
+        $host = $this->planetFieldHostProvider->loadHostViaRequestParameters($game->getUser());
+
+        $this->colonyGuiHelper->registerComponents(ColonyMenuEnum::MENU_MAINSCREEN, $host, $game);
+        $game->showMacro(ColonyMenuEnum::MENU_MAINSCREEN->getTemplate());
+    }
+}
