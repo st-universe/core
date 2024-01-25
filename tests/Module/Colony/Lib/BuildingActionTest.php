6e813ac783756f8458cf237d90565d8f3bc59459
Janko <jadari@web.de>
2024-01-25 10:16:13 +0100

    fix

---
+++ tests/Module/Colony/Lib/BuildingActionTest.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Colony\Lib;
+
+use Mockery;
+use Mockery\MockInterface;
+use Stu\Component\Building\BuildingManagerInterface;
+use Stu\Component\Colony\Storage\ColonyStorageManagerInterface;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Orm\Entity\BuildingInterface;
+use Stu\Orm\Entity\ColonySandboxInterface;
+use Stu\Orm\Entity\PlanetFieldInterface;
+use Stu\Orm\Repository\ColonyRepositoryInterface;
+use Stu\StuTestCase;
+
+class BuildingActionTest extends StuTestCase
+{
+    /**
+     * @var MockInterface|ColonyRepositoryInterface
+     */
+    private $colonyStorageManager;
+
+    /**
+     * @var MockInterface|BuildingManagerInterface
+     */
+    private $buildingManager;
+
+    /**
+     * @var MockInterface|PlanetFieldInterface
+     */
+    private $field;
+
+    private BuildingActionInterface $subject;
+
+
+    public function setUp(): void
+    {
+        $this->colonyStorageManager = Mockery::mock(ColonyStorageManagerInterface::class);
+        $this->buildingManager = Mockery::mock(BuildingManagerInterface::class);
+
+        $this->field = $this->mock(PlanetFieldInterface::class);
+
+        $this->subject = new BuildingAction(
+            $this->colonyStorageManager,
+            $this->buildingManager
+        );
+    }
+
+    public function testRemoveExpectRemovalOfPreviousBuilding(): void
+    {
+        $game = $this->mock(GameControllerInterface::class);
+        $building = $this->mock(BuildingInterface::class);
+
+        $this->field->shouldReceive('hasBuilding')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(true);
+        $this->field->shouldReceive('getBuilding')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($building);
+        $this->field->shouldReceive('getFieldId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(42);
+        $this->field->shouldReceive('getHost')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($this->mock(ColonySandboxInterface::class));
+
+        $building->shouldReceive('isRemovable')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(true);
+        $building->shouldReceive('getName')
+            ->withNoArgs()
+            ->once()
+            ->andReturn('BUILDING');
+
+        $this->buildingManager->shouldReceive('remove')
+            ->with($this->field, false)
+            ->once();
+
+        $game->shouldReceive('addInformationf')
+            ->with(
+                '%s auf Feld %d wurde demontiert',
+                'BUILDING',
+                42
+            )
+            ->once();
+
+        $this->subject->remove($this->field, $game);
+    }
+}
