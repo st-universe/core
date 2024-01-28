cd73636d0f0db4acaacc924e5519ea14c9ff9e07
Janko <jadari@web.de>
2024-01-28 21:43:46 +0100

    add pirate actions

---
+++ src/Module/Tick/Ship/ManagerComponent/PirateActions.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Tick\Ship\ManagerComponent;
+
+
+final class PirateActions implements ManagerComponentInterface
+{
+    public function __construct()
+    {
+    }
+
+    public function work(): void
+    {
+        // create new pirates (max 5 fleets)
+        // process pirate fleets
+        // -following actions: fly, rub colony, attack ships, hide
+
+    }
+}
