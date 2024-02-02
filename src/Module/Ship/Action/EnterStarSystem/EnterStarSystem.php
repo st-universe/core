c57eeb624da21e0e77cd5c461f23abcc757db838
Janko <jadari@web.de>
2024-02-02 19:09:50 +0100

    random system entry component

--- src/Module/Ship/Action/EnterStarSystem/EnterStarSystem.php
+++ src/Module/Ship/Action/EnterStarSystem/EnterStarSystem.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\EnterStarSystem;
 
 use RuntimeException;
-use Stu\Component\Ship\ShipEnum;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Ship\Action\MoveShip\AbstractDirectedMovement;
 use Stu\Module\Ship\Lib\Movement\Route\FlightRouteInterface;
 use Stu\Module\Ship\Lib\ShipWrapperInterface;
-use Stu\Orm\Entity\ShipInterface;
-use Stu\Orm\Entity\StarSystemInterface;
 
 final class EnterStarSystem extends AbstractDirectedMovement
 {
@@ -34,49 +31,8 @@ final class EnterStarSystem extends AbstractDirectedMovement
             throw new RuntimeException('should not happen');
         }
 
-        [$posx, $posy] = $this->getDestinationCoordinates($ship, $system);
+        $destination = $this->randomSystemEntry->getRandomEntryPoint($ship, $system);
 
-        // the destination starsystem map field
-        $starsystemMap = $this->starSystemMapRepository->getByCoordinates($system->getId(), $posx, $posy);
-
-        if ($starsystemMap === null) {
-            throw new RuntimeException('starsystem map is missing');
-        }
-
-        return $this->flightRouteFactory->getRouteForMapDestination($starsystemMap);
-    }
-
-    /**
-     * @return array{0: int,1: int}
-     */
-    private function getDestinationCoordinates(ShipInterface $ship, StarSystemInterface $system): array
-    {
-        $flightDirection = $ship->getFlightDirection();
-        if ($flightDirection === 0) {
-            $flightDirection = random_int(1, 4);
-        }
-
-        switch ($flightDirection) {
-            case ShipEnum::DIRECTION_BOTTOM:
-                $posx = random_int(1, $system->getMaxX());
-                $posy = 1;
-                break;
-            case ShipEnum::DIRECTION_TOP:
-                $posx = random_int(1, $system->getMaxX());
-                $posy = $system->getMaxY();
-                break;
-            case ShipEnum::DIRECTION_RIGHT:
-                $posx = 1;
-                $posy = random_int(1, $system->getMaxY());
-                break;
-            case ShipEnum::DIRECTION_LEFT:
-                $posx = $system->getMaxX();
-                $posy = random_int(1, $system->getMaxY());
-                break;
-            default:
-                throw new RuntimeException('unsupported flight direction');
-        }
-
-        return [$posx, $posy];
+        return $this->flightRouteFactory->getRouteForMapDestination($destination);
     }
 }
