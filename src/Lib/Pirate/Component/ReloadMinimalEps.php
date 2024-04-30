de639945ea420c714d67fc089b3ce02f3c0508fe
Janko <jadari@web.de>
2024-04-30 11:50:33 +0200

    pirates also reload warpdrive

--- src/Lib/Pirate/Component/ReloadMinimalEps.php
+++ src/Lib/Pirate/Component/ReloadMinimalEps.php
@@ -3,20 +3,39 @@
 namespace Stu\Lib\Pirate\Component;
 
 use Stu\Module\Ship\Lib\FleetWrapperInterface;
+use Stu\Module\Ship\Lib\ShipWrapperInterface;
 
 class ReloadMinimalEps implements ReloadMinimalEpsInterface
 {
     public function reload(FleetWrapperInterface $fleetWrapper, int $minimalPercentage = 20): void
     {
         foreach ($fleetWrapper->getShipWrappers() as $wrapper) {
-            $epsSystem = $wrapper->getEpsSystemData();
+            $this->reloadEps($wrapper, $minimalPercentage);
+            $this->reloadWarpdrive($wrapper, $minimalPercentage);
+        }
+    }
 
-            if (
-                $epsSystem !== null
-                && $epsSystem->getEpsPercentage() < $minimalPercentage
-            ) {
-                $epsSystem->setEps((int)($epsSystem->getMaxEps() * $minimalPercentage / 100))->update();
-            }
+    private function reloadEps(ShipWrapperInterface $wrapper, int $minimalPercentage): void
+    {
+        $epsSystem = $wrapper->getEpsSystemData();
+
+        if (
+            $epsSystem !== null
+            && $epsSystem->getEpsPercentage() < $minimalPercentage
+        ) {
+            $epsSystem->setEps((int)($epsSystem->getMaxEps() * $minimalPercentage / 100))->update();
+        }
+    }
+
+    private function reloadWarpdrive(ShipWrapperInterface $wrapper, int $minimalPercentage): void
+    {
+        $warpdrive = $wrapper->getWarpDriveSystemData();
+
+        if (
+            $warpdrive !== null
+            && $warpdrive->getWarpdrivePercentage() < $minimalPercentage
+        ) {
+            $warpdrive->setWarpDrive((int)($warpdrive->getMaxWarpDrive() * $minimalPercentage / 100))->update();
         }
     }
 }
