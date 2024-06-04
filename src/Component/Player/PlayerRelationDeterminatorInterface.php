9956c3d283778b7a360c8df8aa5e1f323f8fdc1e
Janko <jadari@web.de>
2024-06-04 11:48:58 +0200

    enemy relation determination

--- src/Component/Player/PlayerRelationDeterminatorInterface.php
+++ src/Component/Player/PlayerRelationDeterminatorInterface.php
@@ -12,4 +12,9 @@ interface PlayerRelationDeterminatorInterface
      * Checks if $otherUser is a friend of $user
      */
     public function isFriend(UserInterface $user, UserInterface $otherUser): bool;
+
+    /**
+     * Checks if $otherUser is an enemy of $user
+     */
+    public function isEnemy(UserInterface $user, UserInterface $otherUser): bool;
 }
--- tests/Component/Player/PlayerRelationDeterminatorTest.php
+++ tests/Component/Player/PlayerRelationDeterminatorTest.php
@@ -213,4 +213,170 @@ class PlayerRelationDeterminatorTest extends StuTestCase
             $this->subject->isFriend($this->user, $this->opponent)
         );
     }
+
+    public function testIsEnemyReturnsFalseIfAlliancesMatch(): void
+    {
+        $alliance = $this->mock(AllianceInterface::class);
+
+        $this->user->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($alliance);
+
+        $this->opponent->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($alliance);
+
+        static::assertFalse(
+            $this->subject->isEnemy($this->user, $this->opponent)
+        );
+    }
+
+    public function testIsEnemyReturnsTrueIfAlliancesHaveWarRelation(): void
+    {
+        $allianceUser = $this->mock(AllianceInterface::class);
+        $allianceOpponent = $this->mock(AllianceInterface::class);
+
+        $allianceUserId = 666;
+        $allianceOpponentId = 42;
+
+        $this->user->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceUser);
+
+        $this->opponent->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceOpponent);
+
+        $allianceUser->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceUserId);
+
+        $allianceOpponent->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceOpponentId);
+
+        $this->allianceRelationRepository->shouldReceive('getActiveByTypeAndAlliancePair')
+            ->with(
+                [
+                    AllianceEnum::ALLIANCE_RELATION_WAR
+                ],
+                $allianceOpponentId,
+                $allianceUserId
+            )
+            ->once()
+            ->andReturn($this->mock(AllianceRelationInterface::class));
+
+        static::assertTrue(
+            $this->subject->isEnemy($this->user, $this->opponent)
+        );
+    }
+
+    public function testIsEnemyReturnsFalseIfAlliancesHaveNoEnemyRelationAndUserHasNoContact(): void
+    {
+        $allianceUser = $this->mock(AllianceInterface::class);
+        $allianceOpponent = $this->mock(AllianceInterface::class);
+
+        $allianceUserId = 666;
+        $allianceOpponentId = 42;
+        $userId = 33;
+        $opponentId = 21;
+
+        $this->user->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceUser);
+
+        $this->opponent->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceOpponent);
+
+        $allianceUser->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceUserId);
+
+        $allianceOpponent->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($allianceOpponentId);
+
+        $this->allianceRelationRepository->shouldReceive('getActiveByTypeAndAlliancePair')
+            ->with(
+                [
+                    AllianceEnum::ALLIANCE_RELATION_WAR
+                ],
+                $allianceOpponentId,
+                $allianceUserId
+            )
+            ->once()
+            ->andReturnNull();
+
+        $this->user->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($userId);
+
+        $this->opponent->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($opponentId);
+
+        $this->contactRepository->shouldReceive('getByUserAndOpponent')
+            ->with($userId, $opponentId)
+            ->once()
+            ->andReturnNull();
+
+        static::assertFalse(
+            $this->subject->isEnemy($this->user, $this->opponent)
+        );
+    }
+
+    public function testIsEnemyReturnsTrueIfContactIsEnemy(): void
+    {
+        $userId = 33;
+        $opponentId = 21;
+
+        $contact = $this->mock(ContactInterface::class);
+
+        $this->user->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturnNull();
+
+        $this->opponent->shouldReceive('getAlliance')
+            ->withNoArgs()
+            ->once()
+            ->andReturnNull();
+
+        $this->user->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($userId);
+
+        $this->opponent->shouldReceive('getId')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($opponentId);
+
+        $this->contactRepository->shouldReceive('getByUserAndOpponent')
+            ->with($userId, $opponentId)
+            ->once()
+            ->andReturn($contact);
+
+        $contact->shouldReceive('isEnemy')
+            ->withNoArgs()
+            ->once()
+            ->andReturnTrue();
+
+        static::assertTrue(
+            $this->subject->isEnemy($this->user, $this->opponent)
+        );
+    }
 }
