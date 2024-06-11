962d6274d7879a2a4b3feeb99d037e4471eb4284
Huxinator <nicotintel@gmail.com>
2024-06-11 12:01:09 +0200

    module factory layout

--- src/Module/Colony/View/ShowModuleFab/ShowModuleFab.php
+++ src/Module/Colony/View/ShowModuleFab/ShowModuleFab.php
@@ -6,39 +6,44 @@ namespace Stu\Module\Colony\View\ShowModuleFab;
 
 use request;
 use Stu\Component\Colony\ColonyMenuEnum;
+use Stu\Component\Ship\ShipModuleTypeEnum;
 use Stu\Module\Colony\Lib\ColonyLoaderInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 use Stu\Orm\Repository\BuildingFunctionRepositoryInterface;
 use Stu\Orm\Repository\ModuleBuildingFunctionRepositoryInterface;
 use Stu\Orm\Repository\ModuleQueueRepositoryInterface;
+use Stu\Orm\Repository\ShipRumpRepositoryInterface;
+use Stu\Orm\Repository\ShipRumpModuleLevelRepositoryInterface;
 
 final class ShowModuleFab implements ViewControllerInterface
 {
     public const VIEW_IDENTIFIER = 'SHOW_MODULEFAB';
 
     private ColonyLoaderInterface $colonyLoader;
-
     private ShowModuleFabRequestInterface $showModuleFabRequest;
-
     private ModuleBuildingFunctionRepositoryInterface $moduleBuildingFunctionRepository;
-
     private BuildingFunctionRepositoryInterface $buildingFunctionRepository;
-
     private ModuleQueueRepositoryInterface $moduleQueueRepository;
+    private ShipRumpRepositoryInterface $shipRumpRepository;
+    private ShipRumpModuleLevelRepositoryInterface $shipRumpModuleLevelRepository;
 
     public function __construct(
         ColonyLoaderInterface $colonyLoader,
         ShowModuleFabRequestInterface $showModuleFabRequest,
         ModuleBuildingFunctionRepositoryInterface $moduleBuildingFunctionRepository,
         BuildingFunctionRepositoryInterface $buildingFunctionRepository,
-        ModuleQueueRepositoryInterface $moduleQueueRepository
+        ModuleQueueRepositoryInterface $moduleQueueRepository,
+        ShipRumpRepositoryInterface $shipRumpRepository,
+        ShipRumpModuleLevelRepositoryInterface $shipRumpModuleLevelRepository
     ) {
         $this->colonyLoader = $colonyLoader;
         $this->showModuleFabRequest = $showModuleFabRequest;
         $this->moduleBuildingFunctionRepository = $moduleBuildingFunctionRepository;
         $this->buildingFunctionRepository = $buildingFunctionRepository;
         $this->moduleQueueRepository = $moduleQueueRepository;
+        $this->shipRumpRepository = $shipRumpRepository;
+        $this->shipRumpModuleLevelRepository = $shipRumpModuleLevelRepository;
     }
 
     public function handle(GameControllerInterface $game): void
@@ -57,13 +62,67 @@ final class ShowModuleFab implements ViewControllerInterface
             $userId
         );
 
-        $list = [];
+        $sortedModules = [];
         foreach ($modules as $module) {
-            $list[] = new ModuleFabricationListItemTal(
+            $moduleType = $module->getModule()->getType()->value;
+            $moduleLevel = $module->getModule()->getLevel();
+            if (!isset($sortedModules[$moduleType])) {
+                $sortedModules[$moduleType] = [];
+            }
+            if (!isset($sortedModules[$moduleType][$moduleLevel])) {
+                $sortedModules[$moduleType][$moduleLevel] = [];
+            }
+            $sortedModules[$moduleType][$moduleLevel][] = new ModuleFabricationListItemTal(
                 $this->moduleQueueRepository,
                 $module->getModule(),
                 $colony
             );
+        }
+
+        $shipRumps = $this->shipRumpRepository->getBuildableByUser($userId);
+
+        $moduleTypes = [];
+        foreach (ShipModuleTypeEnum::cases() as $moduleType) {
+            $moduleTypes[$moduleType->value] = [
+                'name' => $moduleType->getDescription(),
+                'image' => "/assets/buttons/modul_screen_{$moduleType->value}.png"
+            ];
+        }
+        $rumpModules = [];
+        $rumpModules[0] = $sortedModules;
+        foreach ($shipRumps as $rump) {
+            $rumpId = $rump->getId();
+            $rumpModules[$rumpId] = [];
+
+            foreach ($sortedModules as $type => $levels) {
+                $mod_level = $this->shipRumpModuleLevelRepository->getByShipRump($rumpId);
+
+                if ($type === ShipModuleTypeEnum::SPECIAL->value) {
+                    foreach ($levels as $level => $modules) {
+                        if (!isset($rumpModules[$rumpId][$type])) {
+                            $rumpModules[$rumpId][$type] = [];
+                        }
+                        $rumpModules[$rumpId][$type][$level] = $modules;
+                    }
+                } else {
+                    $min_level_method = 'getModuleLevel' . $type . 'Min';
+                    $max_level_method = 'getModuleLevel' . $type . 'Max';
+
+                    if ($mod_level !== null && method_exists($mod_level, $min_level_method) && method_exists($mod_level, $max_level_method)) {
+                        $min_level = $mod_level->$min_level_method();
+                        $max_level = $mod_level->$max_level_method();
+
+                        foreach ($levels as $level => $modules) {
+                            if ($level >= $min_level && $level <= $max_level) {
+                                if (!isset($rumpModules[$rumpId][$type])) {
+                                    $rumpModules[$rumpId][$type] = [];
+                                }
+                                $rumpModules[$rumpId][$type][$level] = $modules;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         $game->showMacro(ColonyMenuEnum::MENU_MODULEFAB->getTemplate());
@@ -71,6 +130,9 @@ final class ShowModuleFab implements ViewControllerInterface
 
         $game->setTemplateVar('HOST', $colony);
         $game->setTemplateVar('FUNC', $func);
-        $game->setTemplateVar('MODULE_LIST', $list);
+        $game->setTemplateVar('SORTED_MODULES', $sortedModules);
+        $game->setTemplateVar('SHIP_RUMPS', $shipRumps);
+        $game->setTemplateVar('MODULE_TYPES', $moduleTypes);
+        $game->setTemplateVar('RUMP_MODULES', $rumpModules);
     }
 }
