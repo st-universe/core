f5a035a43b963067500296a011b8cef745eb5af6
Janko <jadari@web.de>
2024-07-16 17:52:38 +0200

    fix ticket-0391

--- src/Component/Refactor/RefactorRunner.php
+++ src/Component/Refactor/RefactorRunner.php
@@ -4,43 +4,111 @@ declare(strict_types=1);
 
 namespace Stu\Component\Refactor;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Stu\Lib\ModuleRumpWrapper\ModuleRumpWrapperProjectileWeapon;
-use Stu\Module\Ship\Lib\ShipWrapperFactoryInterface;
-use Stu\Orm\Repository\ShipRepositoryInterface;
+use Stu\Orm\Entity\AstronomicalEntryInterface;
+use Stu\Orm\Entity\LocationInterface;
+use Stu\Orm\Entity\MapInterface;
+use Stu\Orm\Entity\MapRegionInterface;
+use Stu\Orm\Entity\StarSystemInterface;
+use Stu\Orm\Entity\StarSystemMapInterface;
+use Stu\Orm\Repository\AstroEntryRepositoryInterface;
+use Stu\Orm\Repository\LocationRepositoryInterface;
+use Stu\Orm\Repository\MapRepositoryInterface;
+use Stu\Orm\Repository\StarSystemMapRepositoryInterface;
 
 final class RefactorRunner
 {
     public function __construct(
-        private ShipRepositoryInterface $shipRepository,
-        private ShipWrapperFactoryInterface $shipWrapperFactory
+        private AstroEntryRepositoryInterface $astroEntryRepository,
+        private LocationRepositoryInterface $locationRepository,
+        private MapRepositoryInterface $mapRepository,
+        private StarSystemMapRepositoryInterface $starSystemMapRepository,
     ) {
     }
 
     public function refactor(): void
     {
-        foreach ($this->shipRepository->findAll() as $ship) {
+        foreach ($this->astroEntryRepository->findAll() as $astroEntry) {
 
-            $wrapper = $this->shipWrapperFactory
-                ->wrapShip($ship);
-
-            $launcher = $wrapper
-                ->getProjectileLauncherSystemData();
-
-            if ($launcher === null) {
+            if ($astroEntry->getFieldIds() === '') {
                 continue;
             }
 
-            $buildplan = $ship->getBuildplan();
-            if ($buildplan === null) {
+            $fieldIds = new ArrayCollection(unserialize($astroEntry->getFieldIds()));
+            if ($fieldIds->isEmpty()) {
                 continue;
             }
 
-            $unit = new ModuleRumpWrapperProjectileWeapon(
-                $ship->getRump(),
-                $buildplan
-            );
+            $locations = $fieldIds->map(fn (int $id) => $this->locationRepository->find($id));
 
-            $unit->apply($wrapper);
+            $lost = false;
+            $isMap = false;
+            $isSystemMap = false;
+
+            /** @var LocationInterface|null $location */
+            foreach ($locations as $location) {
+                if ($location === null) {
+                    $lost = true;
+                    break;
+                }
+
+                if (
+                    $location instanceof MapInterface
+                    && $location->getMapRegion() !== $astroEntry->getRegion()
+                ) {
+                    $lost = true;
+                    break;
+                }
+
+                if (
+                    $location instanceof StarSystemMapInterface
+                    && $location->getSystem() !== $astroEntry->getSystem()
+                ) {
+                    $lost = true;
+                    break;
+                }
+
+                $isMap = $isMap || $location->isMap();
+                $isSystemMap = $isSystemMap || !$location->isMap();
+            }
+
+            if ($lost || ($isMap && $isSystemMap)) {
+                $this->obtainMeasurementFields(
+                    $astroEntry->getSystem(),
+                    $astroEntry->getRegion(),
+                    $astroEntry
+                );
+            }
         }
+    }
+
+    private function obtainMeasurementFields(
+        ?StarSystemInterface $system,
+        ?MapRegionInterface $mapRegion,
+        AstronomicalEntryInterface $entry
+    ): void {
+        if ($system !== null) {
+            $this->obtainMeasurementFieldsForSystem($system, $entry);
+        }
+        if ($mapRegion !== null) {
+            $this->obtainMeasurementFieldsForRegion($mapRegion, $entry);
+        }
+
+        $this->astroEntryRepository->save($entry);
+    }
+
+    private function obtainMeasurementFieldsForSystem(StarSystemInterface $system, AstronomicalEntryInterface $entry): void
+    {
+        $idArray = $this->starSystemMapRepository->getRandomSystemMapIdsForAstroMeasurement($system->getId());
+
+        $entry->setFieldIds(serialize($idArray));
+    }
+
+    private function obtainMeasurementFieldsForRegion(MapRegionInterface $mapRegion, AstronomicalEntryInterface $entry): void
+    {
+        $mapIds = $this->mapRepository->getRandomMapIdsForAstroMeasurement($mapRegion->getId(), 25);
+
+        $entry->setFieldIds(serialize($mapIds));
     }
 }
