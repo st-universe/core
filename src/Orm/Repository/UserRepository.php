8371f494b07807258090ba625e79afd15ffe7d9f
Huxinator <nicotintel@gmail.com>
2024-08-04 10:38:22 +0200

    enable flush ohn save

--- src/Orm/Repository/UserRepository.php
+++ src/Orm/Repository/UserRepository.php
@@ -34,7 +34,7 @@ final class UserRepository extends EntityRepository implements UserRepositoryInt
         $em = $this->getEntityManager();
 
         $em->persist($post);
-        //$em->flush();
+        $em->flush();
     }
 
     #[Override]
