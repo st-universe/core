bf1b03aead971ceed27a9eb14997b21fbf59bf47
Huxinator <nicotintel@gmail.com>
2024-08-05 17:48:24 +0200

    add location mining entity

--- src/Orm/Entity/LocationInterface.php
+++ src/Orm/Entity/LocationInterface.php
@@ -58,4 +58,9 @@ interface LocationInterface
     public function isOverWormhole(): bool;
 
     public function getSectorString(): string;
+
+    /**
+     * @return Collection<int, LocationMiningInterface>
+     */
+    public function getLocationMinings(): Collection;
 }
