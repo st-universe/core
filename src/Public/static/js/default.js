65a48caad6f027ff915ef5907c46cec957c71fad
Janko <jadari@web.de>
2024-08-18 15:49:33 +0200

    asynchronous loading of javascript files

--- src/Public/static/js/default.js
+++ src/Public/static/js/default.js
@@ -725,10 +725,46 @@ function maximizeCommodityAmounts() {
         });
 }
 
-
 document.addEventListener("DOMContentLoaded", function () {
         var previewElement = document.getElementById("preview");
         if (previewElement) {
                 initTranslations();
         }
 });
+
+/** ASYNCHRONOUS LOADING OF JAVASCRIPT FILES*/
+const loadScript = (FILE_URL, async = true, type = "text/javascript") => {
+        return new Promise((resolve, reject) => {
+                try {
+                        const scriptEle = document.createElement("script");
+                        scriptEle.type = type;
+                        scriptEle.async = async;
+                        scriptEle.src = FILE_URL;
+
+                        scriptEle.addEventListener("load", (ev) => {
+                                resolve({ status: true });
+                        });
+
+                        scriptEle.addEventListener("error", (ev) => {
+                                reject({
+                                        status: false,
+                                        message: `Failed to load the script ${FILE_URL}`
+                                });
+                        });
+
+                        document.body.appendChild(scriptEle);
+                } catch (error) {
+                        reject(error);
+                }
+        });
+};
+function appendJsAsync(path) {
+
+        loadScript(path)
+                .then(data => {
+                        console.log(`Script '${path}' loaded successfully`, data);
+                })
+                .catch(err => {
+                        console.error(err);
+                });
+}
