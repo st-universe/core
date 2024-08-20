b54ab397e19a47454c7b47c515c570d87a251942
Janko <jadari@web.de>
2024-08-20 12:40:46 +0200

    detect on location

--- src/Module/Ship/Lib/Battle/AlertDetection/AlertedShipsDetection.php
+++ src/Module/Ship/Lib/Battle/AlertDetection/AlertedShipsDetection.php
@@ -6,27 +6,30 @@ use Doctrine\Common\Collections\Collection;
 use Override;
 use Stu\Module\Ship\Lib\ShipWrapperFactoryInterface;
 use Stu\Module\Ship\Lib\ShipWrapperInterface;
+use Stu\Orm\Entity\LocationInterface;
 use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\UserInterface;
 
 class AlertedShipsDetection implements AlertedShipsDetectionInterface
 {
     public function __construct(
         private ShipWrapperFactoryInterface $shipWrapperFactory
-    ) {
-    }
+    ) {}
 
     #[Override]
-    public function getAlertedShipsOnLocation(ShipInterface $incomingShip): Collection
-    {
-        return $incomingShip->getCurrentMapField()->getShips()
+    public function getAlertedShipsOnLocation(
+        LocationInterface $location,
+        UserInterface $user
+    ): Collection {
+        return $location->getShips()
             ->filter(
-                fn (ShipInterface $ship): bool => !$ship->getUser()->isVacationRequestOldEnough()
-                    && $ship->getUser() !== $incomingShip->getUser()
+                fn(ShipInterface $ship): bool => !$ship->getUser()->isVacationRequestOldEnough()
+                    && $ship->getUser() !== $user
                     && ($ship->isFleetLeader() || $ship->getFleet() === null)
                     && !$ship->isAlertGreen()
                     && !$ship->isWarped()
                     && !$ship->getCloakState()
             )
-            ->map(fn (ShipInterface $ship): ShipWrapperInterface => $this->shipWrapperFactory->wrapShip($ship));
+            ->map(fn(ShipInterface $ship): ShipWrapperInterface => $this->shipWrapperFactory->wrapShip($ship));
     }
 }
