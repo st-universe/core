e4c3d6cef9dd1f30365e8387fdfc6ca41910ed71
Janko <jadari@web.de>
2024-08-20 14:13:35 +0200

    introduce prestige calculation

--- src/Module/Prestige/services.php
+++ src/Module/Prestige/services.php
@@ -6,10 +6,13 @@ namespace Stu\Module\Prestige;
 
 use Stu\Module\Prestige\Lib\CreatePrestigeLog;
 use Stu\Module\Prestige\Lib\CreatePrestigeLogInterface;
+use Stu\Module\Prestige\Lib\PrestigeCalculation;
+use Stu\Module\Prestige\Lib\PrestigeCalculationInterface;
 
 use function DI\autowire;
 
 return [
+    PrestigeCalculationInterface::class => autowire(PrestigeCalculation::class),
     CreatePrestigeLogInterface::class => autowire(CreatePrestigeLog::class),
     'PRESTIGE_ACTIONS' => [],
     'PRESTIGE_VIEWS' => []
