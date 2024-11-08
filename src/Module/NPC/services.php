a6e05287d63c1aa8d07bf427b33843df4bc89a8d
Huxinator <nicotintel@gmail.com>
2024-11-08 17:07:15 +0100

    npc ship and buildplancreator

--- src/Module/NPC/services.php
+++ src/Module/NPC/services.php
@@ -9,14 +9,24 @@ use Stu\Module\NPC\Action\CommodityCheat;
 use Stu\Module\NPC\View\NPCLog\NPCLog;
 use Stu\Module\NPC\View\Overview\Overview;
 use Stu\Module\NPC\View\ShowTools\ShowTools;
+use Stu\Module\NPC\Action\CreateBuildplan;
+use Stu\Module\NPC\View\ShowBuildplanCreator\ShowBuildplanCreator;
+use Stu\Module\NPC\Action\CreateShip;
+use Stu\Module\NPC\View\ShowShipCreator\ShowShipCreator;
 
 use function DI\autowire;
 
 return [
-    'NPC_ACTIONS' => [CommodityCheat::ACTION_IDENTIFIER => autowire(CommodityCheat::class)],
+    'NPC_ACTIONS' => [
+        CommodityCheat::ACTION_IDENTIFIER => autowire(CommodityCheat::class),
+        CreateBuildplan::ACTION_IDENTIFIER => autowire(CreateBuildplan::class),
+        CreateShip::ACTION_IDENTIFIER => autowire(CreateShip::class)
+    ],
     'NPC_VIEWS' => [
         GameController::DEFAULT_VIEW => autowire(Overview::class),
         NPCLog::VIEW_IDENTIFIER => autowire(NPCLog::class),
-        ShowTools::VIEW_IDENTIFIER => autowire(ShowTools::class)
+        ShowTools::VIEW_IDENTIFIER => autowire(ShowTools::class),
+        ShowBuildplanCreator::VIEW_IDENTIFIER => autowire(ShowBuildplanCreator::class),
+        ShowShipCreator::VIEW_IDENTIFIER => autowire(ShowShipCreator::class)
     ]
 ];
