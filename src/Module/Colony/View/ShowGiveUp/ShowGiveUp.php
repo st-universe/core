526270b9f42d29af7c1af0af80642778155f19ec
Janko <jadari@web.de>
2024-11-18 10:20:29 +0100

    stan

--- src/Module/Colony/View/ShowGiveUp/ShowGiveUp.php
+++ src/Module/Colony/View/ShowGiveUp/ShowGiveUp.php
@@ -6,22 +6,24 @@ namespace Stu\Module\Colony\View\ShowGiveUp;
 
 use Override;
 use request;
+use Stu\Module\Colony\Lib\ColonyLoaderInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
-use Stu\Orm\Repository\ColonyRepositoryInterface;
 
 final class ShowGiveUp implements ViewControllerInterface
 {
     public const string VIEW_IDENTIFIER = 'SHOW_GIVEUP_AJAX';
 
-    public function __construct(private ColonyRepositoryInterface $colonyRepository)
-    {
-    }
+    public function __construct(private ColonyLoaderInterface $colonyLoader) {}
 
     #[Override]
     public function handle(GameControllerInterface $game): void
     {
-        $colony = $this->colonyRepository->find(request::indInt('id'));
+        $colony = $this->colonyLoader->loadWithOwnerValidation(
+            request::indInt('id'),
+            $game->getUser()->getId(),
+            false
+        );
 
         $code = substr(md5($colony->getName()), 0, 6);
 
