01a9313b765fcc07f41ab591ac74bd42c6996511
Janko <jadari@web.de>
2024-11-19 11:59:01 +0100

    introduce StuContainer

---
+++ src/Config/StuContainer.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Config;
+
+use DI\Container;
+use DI\Definition\ArrayDefinition;
+use DI\Definition\AutowireDefinition;
+use DI\Definition\Definition;
+use DI\Definition\Exception\InvalidDefinition;
+use DI\Definition\Source\MutableDefinitionSource;
+use DI\Proxy\ProxyFactory;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Psr\Container\ContainerInterface;
+
+class StuContainer extends Container
+{
+
+    private MutableDefinitionSource $definitionSource;
+
+    /** @var array<string, Definition> */
+    private ?array $definitions = null;
+
+    /** @var Collection<string, Collection<int|string, mixed>> */
+    private Collection $services;
+
+    /**
+     * @param ContainerInterface $wrapperContainer If the container is wrapped by another container.
+     */
+    public function __construct(
+        MutableDefinitionSource $definitions,
+        ?ProxyFactory $proxyFactory = null,
+        ?ContainerInterface $wrapperContainer = null
+    ) {
+        parent::__construct($definitions, $proxyFactory, $wrapperContainer);
+
+        $this->definitionSource = $definitions;
+        $this->services = new ArrayCollection();
+    }
+
+    /**
+     * @template T
+     * @param class-string<T> $interfaceName
+     * 
+     * @return Collection<int|string, T>
+     */
+    public function getAllImplementations(string $interfaceName): Collection
+    {
+        $services = $this->services->get($interfaceName);
+        if ($services === null) {
+            $services = $this->getServices($interfaceName);
+        }
+
+        return $services;
+    }
+
+    /**
+     * @template T
+     * @param class-string<T> $interfaceName
+     * 
+     * @return Collection<int, T>
+     */
+    private function getServices(string $interfaceName): Collection
+    {
+        $services = new ArrayCollection();
+
+        $definitions = $this->getDefinitions();
+
+        foreach ($definitions as $definitionKey => $definition) {
+
+            if ($definition instanceof ArrayDefinition) {
+
+                foreach ($definition->getValues() as $arrayKey => $value) {
+                    if ($value instanceof AutowireDefinition) {
+                        $this->addDefinition($value->getClassName(), $arrayKey, $services, $interfaceName);
+                    }
+                }
+            }
+        }
+
+        $this->services->set($interfaceName, $services);
+
+        return $services;
+    }
+
+    /** @return array<string, Definition> */
+    private function getDefinitions(): array
+    {
+        if ($this->definitions === null) {
+            $this->definitions = $this->definitionSource->getDefinitions();
+        }
+
+        return $this->definitions;
+    }
+
+    /**
+     * @template T
+     * @param class-string<T> $interfaceName
+     * @param Collection<int|string, T> $services
+     */
+    private function addDefinition(
+        string $className,
+        int|string $key,
+        Collection $services,
+        string $interfaceName
+    ): void {
+        $classImplements = class_implements($className);
+        if (!$classImplements) {
+            return;
+        }
+
+        if (in_array($interfaceName, $classImplements)) {
+            try {
+                $services->set($key, $this->get($className));
+            } catch (InvalidDefinition $e) {
+                return; //TODO find out, why
+            }
+        }
+    }
+}
