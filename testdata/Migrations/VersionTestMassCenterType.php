0b67af8bba9e08b6239381f537f91e1d31510a3f
Huxinator <nicotintel@gmail.com>
2024-11-22 09:42:03 +0100

    add test data

---
+++ testdata/Migrations/VersionTestMassCenterType.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Testdata;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class VersionTestMassCenterType extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Adds default stu_mass_center_type.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('INSERT INTO stu_layer (id, first_field_type_id, description, size)
+                VALUES (1060, 106001, \'Roter Zwerg\', 4);
+        ');
+    }
+}
