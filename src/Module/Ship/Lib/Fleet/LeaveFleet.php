3ae7e9267e6f4665a9943f600a3fcf95afe6f657
Janko <jadari@web.de>
2024-11-26 16:00:16 +0100

    fix fleet leader change on player deletion

--- src/Module/Ship/Lib/Fleet/LeaveFleet.php
+++ src/Module/Ship/Lib/Fleet/LeaveFleet.php
@@ -5,13 +5,21 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Lib\Fleet;
 
 use Override;
+use Stu\Module\Logging\LoggerUtilFactoryInterface;
+use Stu\Module\Logging\LoggerUtilInterface;
 use Stu\Orm\Entity\ShipInterface;
 use Stu\Orm\Repository\ShipRepositoryInterface;
 
 final class LeaveFleet implements LeaveFleetInterface
 {
-    public function __construct(private ShipRepositoryInterface $shipRepository, private ChangeFleetLeaderInterface $changeFleetLeader)
-    {
+    private LoggerUtilInterface $logger;
+
+    public function __construct(
+        private ShipRepositoryInterface $shipRepository,
+        private ChangeFleetLeaderInterface $changeFleetLeader,
+        LoggerUtilFactoryInterface $loggerUtilFactory
+    ) {
+        $this->logger = $loggerUtilFactory->getLoggerUtil();
     }
 
     #[Override]
@@ -23,7 +31,10 @@ final class LeaveFleet implements LeaveFleetInterface
             return false;
         }
 
+        $this->logger->logf('shipId %d leaving fleetId %d', $ship->getId(), $fleet->getId());
+
         if ($ship->isFleetLeader()) {
+            $this->logger->logf('now changing fleet leader');
             $this->changeFleetLeader->change($ship);
         } else {
             $fleet->getShips()->removeElement($ship);
