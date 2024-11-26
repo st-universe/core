2adcedb135c5c60f50b60781a472313843963be6
Janko <jadari@web.de>
2024-11-26 11:35:21 +0100

    fix ticket-0503

--- tests/unit/Component/Player/Deletion/Handler/ShipBuildplanDeletionHandlerTest.php
+++ tests/unit/Component/Player/Deletion/Handler/ShipBuildplanDeletionHandlerTest.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace Stu\Component\Player\Deletion\Handler;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Mockery\MockInterface;
 use Override;
+use Stu\Module\PlayerSetting\Lib\UserEnum;
 use Stu\Orm\Entity\ShipBuildplanInterface;
+use Stu\Orm\Entity\ShipInterface;
 use Stu\Orm\Entity\UserInterface;
 use Stu\Orm\Repository\ShipBuildplanRepositoryInterface;
+use Stu\Orm\Repository\UserRepositoryInterface;
 use Stu\StuTestCase;
 
 class ShipBuildplanDeletionHandlerTest extends StuTestCase
 {
     /** @var MockInterface&ShipBuildplanRepositoryInterface */
-    private MockInterface $shipBuildplanRepository;
+    private $shipBuildplanRepository;
+    /** @var MockInterface&UserRepositoryInterface */
+    private $userRepository;
 
     private ShipBuildplanDeletionHandler $subject;
 
@@ -22,16 +28,23 @@ class ShipBuildplanDeletionHandlerTest extends StuTestCase
     protected function setUp(): void
     {
         $this->shipBuildplanRepository = $this->mock(ShipBuildplanRepositoryInterface::class);
+        $this->userRepository = $this->mock(UserRepositoryInterface::class);
 
         $this->subject = new ShipBuildplanDeletionHandler(
-            $this->shipBuildplanRepository
+            $this->shipBuildplanRepository,
+            $this->userRepository
         );
     }
 
     public function testDeleteDeletesBuildplans(): void
     {
         $user = $this->mock(UserInterface::class);
-        $shipBuildplan = $this->mock(ShipBuildplanInterface::class);
+        $foreignBuildplansUser = $this->mock(UserInterface::class);
+        $shipBuildplanWithoutShips = $this->mock(ShipBuildplanInterface::class);
+        $shipBuildplanWithOwnShip = $this->mock(ShipBuildplanInterface::class);
+        $shipBuildplanWithForeignShip = $this->mock(ShipBuildplanInterface::class);
+        $ownShip = $this->mock(ShipInterface::class);
+        $foreignShip = $this->mock(ShipInterface::class);
 
         $userId = 666;
 
@@ -40,13 +53,59 @@ class ShipBuildplanDeletionHandlerTest extends StuTestCase
             ->once()
             ->andReturn($userId);
 
+        $shipBuildplanWithoutShips->shouldReceive('getShiplist')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(new ArrayCollection());
+
+        $shipBuildplanWithOwnShip->shouldReceive('getShiplist')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(new ArrayCollection([$ownShip]));
+        $shipBuildplanWithOwnShip->shouldReceive('getUser')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($user);
+
+        $shipBuildplanWithForeignShip->shouldReceive('getShiplist')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(new ArrayCollection([$foreignShip]));
+        $shipBuildplanWithForeignShip->shouldReceive('getUser')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($user);
+        $shipBuildplanWithForeignShip->shouldReceive('setUser')
+            ->with($foreignBuildplansUser)
+            ->once();
+
+        $ownShip->shouldReceive('getUser')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($user);
+        $foreignShip->shouldReceive('getUser')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($this->mock(UserInterface::class));
+
         $this->shipBuildplanRepository->shouldReceive('getByUser')
             ->with($userId)
             ->once()
-            ->andReturn([$shipBuildplan]);
+            ->andReturn([$shipBuildplanWithoutShips, $shipBuildplanWithOwnShip, $shipBuildplanWithForeignShip]);
         $this->shipBuildplanRepository->shouldReceive('delete')
-            ->with($shipBuildplan)
+            ->with($shipBuildplanWithoutShips)
             ->once();
+        $this->shipBuildplanRepository->shouldReceive('delete')
+            ->with($shipBuildplanWithOwnShip)
+            ->once();
+        $this->shipBuildplanRepository->shouldReceive('save')
+            ->with($shipBuildplanWithForeignShip)
+            ->once();
+
+        $this->userRepository->shouldReceive('find')
+            ->with(UserEnum::USER_FOREIGN_BUILDPLANS)
+            ->once()
+            ->andReturn($foreignBuildplansUser);
 
         $this->subject->delete($user);
     }
