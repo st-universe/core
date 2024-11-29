98db4341b22f391868358aae1224cb25a1124507
Janko <jadari@web.de>
2024-11-29 14:24:19 +0100

    convert to actionToInnerContent with component update

--- src/Module/Research/Action/StartResearch/StartResearch.php
+++ src/Module/Research/Action/StartResearch/StartResearch.php
@@ -8,6 +8,8 @@ use Override;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameController;
 use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Game\Lib\Component\ComponentEnum;
+use Stu\Module\Game\Lib\Component\ComponentLoaderInterface;
 use Stu\Module\Research\TechlistRetrieverInterface;
 use Stu\Orm\Repository\ResearchedRepositoryInterface;
 
@@ -15,9 +17,12 @@ final class StartResearch implements ActionControllerInterface
 {
     public const string ACTION_IDENTIFIER = 'B_DO_RESEARCH';
 
-    public function __construct(private ResearchedRepositoryInterface $researchedRepository, private TechlistRetrieverInterface $techlistRetriever, private StartResearchRequestInterface $startResearchRequest)
-    {
-    }
+    public function __construct(
+        private ResearchedRepositoryInterface $researchedRepository,
+        private TechlistRetrieverInterface $techlistRetriever,
+        private StartResearchRequestInterface $startResearchRequest,
+        private ComponentLoaderInterface $componentLoader
+    ) {}
 
     #[Override]
     public function handle(GameControllerInterface $game): void
@@ -50,6 +55,8 @@ final class StartResearch implements ActionControllerInterface
         }
 
         $game->setView(GameController::DEFAULT_VIEW);
+
+        $this->componentLoader->addComponentUpdate(ComponentEnum::RESEARCH_NAVLET);
     }
 
     #[Override]
