af56d5ae67e4e0439e59b38cce62c1d83ab3fb64
Huxinator <nicotintel@gmail.com>
2024-12-03 20:19:24 +0100

    restrict wormhole

--- src/Module/Ship/Action/LeaveWormhole/LeaveWormhole.php
+++ src/Module/Ship/Action/LeaveWormhole/LeaveWormhole.php
@@ -15,12 +15,15 @@ final class LeaveWormhole extends AbstractDirectedMovement
 {
     public const string ACTION_IDENTIFIER = 'B_LEAVE_WORMHOLE';
 
+    private const MODE_ALLOWED = 1;
+    private const MODE_DENIED = 2;
+
     #[Override]
     protected function isSanityCheckFaultyConcrete(ShipWrapperInterface $wrapper, GameControllerInterface $game): bool
     {
         $ship = $wrapper->get();
-
         $starsystemMap = $ship->getStarsystemMap();
+
         if ($starsystemMap === null) {
             return true;
         }
@@ -30,15 +33,40 @@ final class LeaveWormhole extends AbstractDirectedMovement
         }
 
         $wormholeEntry = $starsystemMap->getRandomWormholeEntry();
-        return $wormholeEntry === null;
+        if ($wormholeEntry === null) {
+            return true;
+        }
+
+        $restrictions = $wormholeEntry->getRestrictions();
+
+        $hasAllowedEntry = false;
+        foreach ($restrictions as $restriction) {
+            if ($restriction->getMode() === self::MODE_ALLOWED) {
+                $hasAllowedEntry = true;
+                if ($restriction->getUser() === $ship->getUser()) {
+                    return false;
+                }
+            }
+            if ($restriction->getUser() === $ship->getUser() && $restriction->getMode() === self::MODE_DENIED) {
+                $game->addInformation(_("Du hast keine Berechtigung um das Wurmloch zu verlassen"));
+                return true;
+            }
+        }
+
+        if ($hasAllowedEntry) {
+            $game->addInformation(_("Du hast keine Berechtigung um das Wurmloch zu verlassen"));
+            return true;
+        }
+
+        return false;
     }
 
     #[Override]
     protected function getFlightRoute(ShipWrapperInterface $wrapper): FlightRouteInterface
     {
         $ship = $wrapper->get();
-
         $starsystemMap = $ship->getStarsystemMap();
+
         if ($starsystemMap === null) {
             throw new RuntimeException('should not happen');
         }
