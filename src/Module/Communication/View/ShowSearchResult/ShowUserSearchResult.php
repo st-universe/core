9b311091418827270a4b6b21a54d8836dc0229ef
Janko <jadari@web.de>
2024-12-19 12:56:57 +0100

    fix ticket-0556

--- src/Module/Communication/View/ShowSearchResult/ShowUserSearchResult.php
+++ src/Module/Communication/View/ShowSearchResult/ShowUserSearchResult.php
@@ -7,6 +7,7 @@ namespace Stu\Module\Communication\View\ShowSearchResult;
 use Override;
 use Stu\Component\Communication\Kn\KnFactoryInterface;
 use Stu\Component\Communication\Kn\KnItemInterface;
+use Stu\Component\Game\GameEnum;
 use Stu\Component\Game\ModuleViewEnum;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
@@ -17,9 +18,7 @@ final class ShowUserSearchResult implements ViewControllerInterface
 {
     public const string VIEW_IDENTIFIER = 'SHOW_USER_SEARCH';
 
-    public function __construct(private ShowSearchResultRequestInterface $showSearchResultRequest, private KnPostRepositoryInterface $knPostRepository, private KnFactoryInterface $knFactory)
-    {
-    }
+    public function __construct(private ShowSearchResultRequestInterface $showSearchResultRequest, private KnPostRepositoryInterface $knPostRepository, private KnFactoryInterface $knFactory) {}
 
     #[Override]
     public function handle(GameControllerInterface $game): void
@@ -43,12 +42,14 @@ final class ShowUserSearchResult implements ViewControllerInterface
         $game->setTemplateVar(
             'KN_POSTINGS',
             array_map(
-                fn (KnPostInterface $knPost): KnItemInterface => $this->knFactory->createKnItem(
+                fn(KnPostInterface $knPost): KnItemInterface => $this->knFactory->createKnItem(
                     $knPost,
                     $user
                 ),
                 $posts
             )
         );
+
+        $game->addExecuteJS("initTranslations();", GameEnum::JS_EXECUTION_AFTER_RENDER);
     }
 }
