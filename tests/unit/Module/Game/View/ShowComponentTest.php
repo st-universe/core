fe0392cb01b33a2151eb47bf9032fd1386489e95
Janko <jadari@web.de>
2024-12-20 15:27:55 +0100

    fallback for component updates

---
+++ tests/unit/Module/Game/View/ShowComponentTest.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Module\Game\View\ShowComponent;
+
+use Mockery\MockInterface;
+use Override;
+use PHPUnit\Framework\Attributes\DataProvider;
+use request;
+use Stu\Lib\Colony\PlanetFieldHostProviderInterface;
+use Stu\Lib\Colony\PlanetFieldHostTypeEnum;
+use Stu\Lib\Component\ComponentRegistrationInterface;
+use Stu\Module\Colony\Component\ColonyComponentEnum;
+use Stu\Module\Control\GameControllerInterface;
+use Stu\Module\Control\ViewControllerInterface;
+use Stu\Module\Game\Component\GameComponentEnum;
+use Stu\Orm\Entity\ColonyInterface;
+use Stu\Orm\Entity\UserInterface;
+use Stu\StuTestCase;
+
+class ShowComponentTest extends StuTestCase
+{
+    /** @var MockInterface&ComponentRegistrationInterface  */
+    private $componentRegistration;
+    /** @var MockInterface&PlanetFieldHostProviderInterface  */
+    private $planetFieldHostProvider;
+
+    /** @var MockInterface&GameControllerInterface  */
+    private $game;
+
+    private ViewControllerInterface $subject;
+
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->componentRegistration = $this->mock(ComponentRegistrationInterface::class);
+        $this->planetFieldHostProvider = $this->mock(PlanetFieldHostProviderInterface::class);
+
+        $this->game = $this->mock(GameControllerInterface::class);
+
+        $this->subject = new ShowComponent(
+            $this->componentRegistration,
+            $this->planetFieldHostProvider
+        );
+    }
+
+    public static function getOutdatedProvider(): array
+    {
+        return [
+            ['pm'],
+            ['servertime'],
+            ['foo'],
+            ['BAR'],
+            ['HUX_PM_NAVLET'],
+            ['GAME_UNKNOWN']
+        ];
+    }
+
+    #[DataProvider('getOutdatedProvider')]
+    public function testHandleExpectOutdatedWhenComponentUnknown(string $component): void
+    {
+        request::setMockVars(['component' => $component]);
+
+        $this->componentRegistration->shouldReceive('registerComponent')
+            ->with(GameComponentEnum::OUTDATED, null)
+            ->once();
+
+        $this->game->shouldReceive('showMacro')
+            ->with(GameComponentEnum::OUTDATED->getTemplate())
+            ->once();
+
+        $this->subject->handle($this->game);
+    }
+
+    public function testHandleExpectCorrectComponentWithoutEntity(): void
+    {
+        request::setMockVars(['component' => 'GAME_NAGUS_POPUP']);
+
+        $this->componentRegistration->shouldReceive('registerComponent')
+            ->with(GameComponentEnum::NAGUS, null)
+            ->once();
+
+        $this->game->shouldReceive('showMacro')
+            ->with(GameComponentEnum::NAGUS->getTemplate())
+            ->once();
+
+        $this->subject->handle($this->game);
+    }
+
+    public function testHandleExpectCorrectComponentWithEntity(): void
+    {
+        $user = $this->mock(UserInterface::class);
+        $entity = $this->mock(ColonyInterface::class);
+
+        request::setMockVars([
+            'component' => 'COLONY_SHIELDING',
+            'id' => 42
+        ]);
+
+        $this->planetFieldHostProvider->shouldReceive('loadHostViaRequestParameters')
+            ->with($user, false)
+            ->once()
+            ->andReturn($entity);
+
+        $this->componentRegistration->shouldReceive('registerComponent')
+            ->with(ColonyComponentEnum::SHIELDING, $entity)
+            ->once();
+
+        $this->game->shouldReceive('showMacro')
+            ->with(ColonyComponentEnum::SHIELDING->getTemplate())
+            ->once();
+        $this->game->shouldReceive('getUser')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($user);
+
+        $this->subject->handle($this->game);
+    }
+}
