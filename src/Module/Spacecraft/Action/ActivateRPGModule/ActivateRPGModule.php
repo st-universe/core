937f2085060c977b8768afa804dccbdad305c206
Janko <jadari@web.de>
2024-12-23 10:46:22 +0100

    fix ticket-0572

--- src/Module/Spacecraft/Action/ActivateRPGModule/ActivateRPGModule.php
+++ src/Module/Spacecraft/Action/ActivateRPGModule/ActivateRPGModule.php
@@ -10,19 +10,13 @@ use Stu\Component\Spacecraft\System\SpacecraftSystemTypeEnum;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Spacecraft\Lib\ActivatorDeactivatorHelperInterface;
-use Stu\Module\Spacecraft\Lib\SpacecraftLoaderInterface;
-use Stu\Module\Spacecraft\Lib\SpacecraftWrapperInterface;
 use Stu\Module\Spacecraft\View\ShowSpacecraft\ShowSpacecraft;
-use Stu\Orm\Repository\SpacecraftRepositoryInterface;
 
 final class ActivateRPGModule implements ActionControllerInterface
 {
     public const string ACTION_IDENTIFIER = 'B_ACTIVATE_RPG_MODULE';
 
-    /** @param SpacecraftLoaderInterface<SpacecraftWrapperInterface> $spacecraftLoader */
     public function __construct(
-        private SpacecraftLoaderInterface $spacecraftLoader,
-        private SpacecraftRepositoryInterface $spacecraftRepository,
         private ActivatorDeactivatorHelperInterface $helper
     ) {}
 
@@ -31,18 +25,7 @@ final class ActivateRPGModule implements ActionControllerInterface
     {
         $game->setView(ShowSpacecraft::VIEW_IDENTIFIER);
 
-        $userId = $game->getUser()->getId();
-
         $this->helper->activate(request::indInt('id'), SpacecraftSystemTypeEnum::RPG_MODULE, $game, true);
-
-        $ship = $this->spacecraftLoader->getByIdAndUser(
-            request::indInt('id'),
-            $userId
-        );
-
-        $this->spacecraftRepository->save($ship);
-
-        $game->addInformation("Das RPG Modul wurde aktiviert");
     }
 
     #[Override]
