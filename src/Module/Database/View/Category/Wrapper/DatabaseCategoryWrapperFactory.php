ac26e2021a3ce2ea5dd0d4c1efe55a12313dc784
Janko <jadari@web.de>
2024-12-23 10:52:08 +0100

    fix

--- src/Module/Database/View/Category/Wrapper/DatabaseCategoryWrapperFactory.php
+++ src/Module/Database/View/Category/Wrapper/DatabaseCategoryWrapperFactory.php
@@ -10,14 +10,17 @@ use Stu\Orm\Entity\DatabaseEntryInterface;
 use Stu\Orm\Entity\UserInterface;
 use Stu\Orm\Repository\ColonyClassRepositoryInterface;
 use Stu\Orm\Repository\DatabaseUserRepositoryInterface;
-use Stu\Orm\Repository\ShipRepositoryInterface;
 use Stu\Orm\Repository\StarSystemRepositoryInterface;
+use Stu\Orm\Repository\StationRepositoryInterface;
 
 final class DatabaseCategoryWrapperFactory implements DatabaseCategoryWrapperFactoryInterface
 {
-    public function __construct(private DatabaseUserRepositoryInterface $databaseUserRepository, private StarSystemRepositoryInterface $starSystemRepository, private ColonyClassRepositoryInterface $colonyClassRepositoryInterface, private ShipRepositoryInterface $shipRepository)
-    {
-    }
+    public function __construct(
+        private DatabaseUserRepositoryInterface $databaseUserRepository,
+        private StarSystemRepositoryInterface $starSystemRepository,
+        private ColonyClassRepositoryInterface $colonyClassRepositoryInterface,
+        private StationRepositoryInterface $stationRepository
+    ) {}
 
     #[Override]
     public function createDatabaseCategoryWrapper(
@@ -40,7 +43,7 @@ final class DatabaseCategoryWrapperFactory implements DatabaseCategoryWrapperFac
             $this->databaseUserRepository,
             $databaseEntry,
             $this->starSystemRepository,
-            $this->shipRepository,
+            $this->stationRepository,
             $this->colonyClassRepositoryInterface,
             $user
         );
