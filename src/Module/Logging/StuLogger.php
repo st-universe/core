d042b5c206a3653fdc34934ad6b59b700514bd75
Janko <jadari@web.de>
2025-01-03 14:31:13 +0100

    add static logger

---
+++ src/Module/Logging/StuLogger.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Stu\Module\Logging;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Stu\Config\Init;
+use Stu\Module\Config\StuConfigInterface;
+
+class StuLogger
+{
+    private static ?Logger $logger = null;
+
+    public static function log(string $message): void
+    {
+        $method = LoggerEnum::LEVEL_METHODS[LoggerEnum::LEVEL_INFO];
+        self::getLogger()->$method($message);
+    }
+
+    /** @param string|int|float $args */
+    public static function logf(string $information, ...$args): void
+    {
+        self::log(vsprintf(
+            $information,
+            $args
+        ));
+    }
+
+    private static function getLogger(): Logger
+    {
+        if (self::$logger === null) {
+            self::$logger = new Logger('stu');
+            self::$logger->pushHandler(
+                new StreamHandler(
+                    Init::getContainer()
+                        ->get(StuConfigInterface::class)
+                        ->getDebugSettings()
+                        ->getLogfilePath()
+                ),
+            );
+        }
+
+        return self::$logger;
+    }
+}
