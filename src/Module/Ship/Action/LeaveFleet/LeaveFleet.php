1825ecad81b2e8239a7922e4d56a483aeaa1f5ec
g5bot <jadari@web.de>
2025-03-31 10:39:07 +0200

    fix ticket-0714

--- src/Module/Ship/Action/LeaveFleet/LeaveFleet.php
+++ src/Module/Ship/Action/LeaveFleet/LeaveFleet.php
@@ -5,22 +5,41 @@ declare(strict_types=1);
 namespace Stu\Module\Ship\Action\LeaveFleet;
 
 use Override;
+use Stu\Exception\EntityLockedException;
 use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Ship\Lib\ShipLoaderInterface;
 use Stu\Module\Spacecraft\View\ShowInformation\ShowInformation;
+use Stu\Orm\Entity\ShipInterface;
 
 final class LeaveFleet implements ActionControllerInterface
 {
     public const string ACTION_IDENTIFIER = 'B_LEAVE_FLEET';
 
-    public function __construct(private LeaveFleetRequestInterface $leaveFleetRequest, private ShipLoaderInterface $shipLoader) {}
+    public function __construct(
+        private LeaveFleetRequestInterface $leaveFleetRequest,
+        private ShipLoaderInterface $shipLoader
+    ) {}
 
     #[Override]
     public function handle(GameControllerInterface $game): void
     {
-        $ship = $this->shipLoader->getByIdAndUser($this->leaveFleetRequest->getShipId(), $game->getUser()->getId());
+        $game->setView(ShowInformation::VIEW_IDENTIFIER);
 
+        try {
+            $ship = $this->shipLoader->getByIdAndUser(
+                $this->leaveFleetRequest->getShipId(),
+                $game->getUser()->getId()
+            );
+
+            $this->entferneSchiffAusFlotte($ship, $game);
+        } catch (EntityLockedException $e) {
+            $game->addInformation($e->getMessage());
+        }
+    }
+
+    private function entferneSchiffAusFlotte(ShipInterface $ship, GameControllerInterface $game): void
+    {
         $fleet = $ship->getFleet();
         if ($fleet === null) {
             return;
@@ -29,7 +48,6 @@ final class LeaveFleet implements ActionControllerInterface
             return;
         }
 
-        $game->setView(ShowInformation::VIEW_IDENTIFIER);
         $game->addExecuteJS(sprintf('refreshShiplistFleet(%d);', $ship->getFleetId()));
         $game->addExecuteJS('refreshShiplistSingles();');
 
