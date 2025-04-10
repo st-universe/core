6e0cf6cdac511cae63ea01e7724446845f9a3c31
Thommy <57680906+PazBorma@users.noreply.github.com>
2025-04-10 09:33:03 +0200

    Move colony correction to cli (#2061)
    * move colony correction to cli

---
+++ tests/unit/Component/Cli/ColonyCorrectionCommandTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Component\Cli;
+
+use Ahc\Cli\Application;
+use Mockery\MockInterface;
+use Override;
+use Stu\CliInteractorHelper;
+use Stu\Component\Cli\ColonyCorrectionCommand;
+use Stu\Module\Colony\Lib\ColonyCorrector;
+use Stu\StuTestCase;
+
+class ColonyCorrectionCommandTest extends StuTestCase
+{
+    /** @var MockInterface&ColonyCorrector */
+    private MockInterface $colonyCorrector;
+
+    private ColonyCorrectionCommand $subject;
+
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->colonyCorrector = $this->mock(ColonyCorrector::class);
+
+        $this->subject = new ColonyCorrectionCommand($this->colonyCorrector);
+    }
+
+    public function testExecuteExecutes(): void
+    {
+        $app = $this->mock(Application::class);
+        $interactor = $this->mock(CliInteractorHelper::class);
+
+        $this->subject->bind($app);
+
+        $app->shouldReceive('io')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($interactor);
+
+        $interactor->shouldReceive('ok')
+            ->with(
+                'Korrektur der Kolonien wurde durchgeführt.',
+                true
+            )
+            ->once();
+
+        $this->colonyCorrector->shouldReceive('correct')
+            ->withNoArgs()
+            ->once();
+
+        $this->subject->execute();
+    }
+}
