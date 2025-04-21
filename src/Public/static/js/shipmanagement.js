a31310bdb724be5f42f9b7465eeea25f5323ba4b
Nico Tintel <52821266+Huxinator@users.noreply.github.com>
2025-04-21 15:36:16 +0200

    fix ticket 607

--- src/Public/static/js/shipmanagement.js
+++ src/Public/static/js/shipmanagement.js
@@ -55,7 +55,7 @@ function openShuttleManagement(obj, ship, entity) {
 	closeAjaxWindow();
 
 	var pos = findObject(obj);
-	openWindowPosition('elt', 1, 200, pos[0] - 200, pos[1]);
+	openWindowPosition('elt', 1, 200, pos[0] - 100, pos[1]);
 	ajax_update('elt', `?SHOW_SHUTTLE_MANAGEMENT=1&id=${entity}&shuttletarget=${ship}`);
 }
 
