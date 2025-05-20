7a7b6c4795b537be520f90d88c952c5e7d1064fb
g5bot <jadari@web.de>
2025-05-20 13:42:12 +0200

    end takeover on deleted target user

--- src/Orm/Repository/ShipTakeoverRepository.php
+++ src/Orm/Repository/ShipTakeoverRepository.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\EntityRepository;
 use Override;
 use Stu\Orm\Entity\ShipTakeover;
 use Stu\Orm\Entity\ShipTakeoverInterface;
+use Stu\Orm\Entity\Spacecraft;
+use Stu\Orm\Entity\UserInterface;
 
 /**
  * @extends EntityRepository<ShipTakeover>
@@ -34,5 +36,22 @@ final class ShipTakeoverRepository extends EntityRepository implements ShipTakeo
         $em = $this->getEntityManager();
 
         $em->remove($shipTakeover);
+    }
+
+    #[Override]
+    public function getByTargetOwner(UserInterface $user): array
+    {
+        return $this->getEntityManager()->createQuery(
+            sprintf(
+                'SELECT sto FROM %s sto
+                JOIN %s sp
+                WITH sto.target = sp
+                WHERE sp.user = :user',
+                ShipTakeover::class,
+                Spacecraft::class
+            )
+        )
+            ->setParameter('user', $user)
+            ->getResult();
     }
 }
