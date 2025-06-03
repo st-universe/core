6fa4a9a998c8b5a86f03616ba35d36c6a72c77b9
Huxinator <nicotintel@gmail.com>
2025-06-03 11:02:44 +0200

    location in npc history creator

--- src/Module/NPC/Action/CreateHistoryEntry.php
+++ src/Module/NPC/Action/CreateHistoryEntry.php
@@ -11,13 +11,14 @@ use Stu\Module\Control\ActionControllerInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\NPC\View\ShowTools\ShowTools;
 use Stu\Orm\Repository\NPCLogRepositoryInterface;
+use Stu\Orm\Repository\LocationRepositoryInterface;
 use Stu\Module\History\Lib\EntryCreatorInterface;
 
 final class CreateHistoryEntry implements ActionControllerInterface
 {
     public const string ACTION_IDENTIFIER = 'B_CREATE_HISTORY_ENTRY';
 
-    public function __construct(private NPCLogRepositoryInterface $npcLogRepository, private EntryCreatorInterface $entryCreator) {}
+    public function __construct(private NPCLogRepositoryInterface $npcLogRepository, private EntryCreatorInterface $entryCreator, private LocationRepositoryInterface $locationRepository) {}
 
     #[Override]
     public function handle(GameControllerInterface $game): void
@@ -33,6 +34,14 @@ final class CreateHistoryEntry implements ActionControllerInterface
 
         $reason = request::postString('reason');
         $historyText = request::postString('historytext');
+        $x = request::postIntFatal('x');
+        $y = request::postIntFatal('y');
+        $layer = request::postIntFatal('layer');
+
+        if (($x !== 0 && $y === 0) || ($x === 0 && $y !== 0)) {
+            $game->addInformation("X und Y Koordinaten müssen beide gesetzt oder beide leer sein");
+            return;
+        }
 
         $historyTypeId = request::postIntFatal('historytype');
         $historyType = null;
@@ -59,7 +68,13 @@ final class CreateHistoryEntry implements ActionControllerInterface
             return;
         }
 
-        $this->entryCreator->createEntry($historyType, $historyText, $user->getId(), $user->getId());
+        if ($x || $y) {
+            $location = $this->locationRepository->getByCoordinates($x, $y, $layer);
+        } else {
+            $location = null;
+        }
+
+        $this->entryCreator->createEntry($historyType, $historyText, $user->getId(), $user->getId(), $location);
 
         if ($reason !== '') {
 
