cfddf8b4c38cc8d957bebb916c5c9586014aebbb
g5bot <jadari@web.de>
2025-06-25 10:28:03 +0200

    readability

--- src/Lib/Transfer/Wrapper/SpacecraftStorageCommodityLogic.php
+++ src/Lib/Transfer/Wrapper/SpacecraftStorageCommodityLogic.php
@@ -4,13 +4,18 @@ declare(strict_types=1);
 
 namespace Stu\Lib\Transfer\Wrapper;
 
+use Doctrine\Common\Collections\Collection;
 use request;
+use Stu\Component\Spacecraft\System\Data\EpsSystemData;
 use Stu\Lib\Information\InformationInterface;
 use Stu\Lib\Pirate\PirateReactionInterface;
 use Stu\Lib\Pirate\PirateReactionTriggerEnum;
 use Stu\Lib\Transfer\CommodityTransferInterface;
+use Stu\Lib\Transfer\EntityWithStorageInterface;
 use Stu\Module\Spacecraft\Lib\SpacecraftWrapperInterface;
 use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\SpacecraftInterface;
+use Stu\Orm\Entity\StorageInterface;
 
 class SpacecraftStorageCommodityLogic
 {
@@ -68,46 +73,27 @@ class SpacecraftStorageCommodityLogic
         InformationInterface $information
     ): bool {
 
-        $ship = $wrapper->get();
+        $spacecraft = $wrapper->get();
         $epsSystem = $wrapper->getEpsSystemData();
 
-        //sanity checks
-        $isDockTransfer = $this->commodityTransfer->isDockTransfer($ship, $target->get());
-        if (!$isDockTransfer && ($epsSystem === null || $epsSystem->getEps() === 0)) {
-            $information->addInformation("Keine Energie vorhanden");
-            return false;
-        }
-        if ($ship->isCloaked()) {
-            $information->addInformation("Die Tarnung ist aktiviert");
-            return false;
-        }
-        if ($ship->isWarped()) {
-            $information->addInformation("Schiff befindet sich im Warp");
-            return false;
-        }
+        $transferTarget = $isUnload ? $target->get() : $spacecraft;
+        $storage = $isUnload ? $spacecraft->getBeamableStorage() : $target->get()->getBeamableStorage();
 
-        $transferTarget = $isUnload ? $target->get() : $ship;
-        if ($transferTarget->getMaxStorage() <= $transferTarget->getStorageSum()) {
-            $information->addInformationf('%s: Der Lagerraum ist voll', $isUnload ? $target->getName() : $ship->getName());
+        if ($this->isSanityFaulty($spacecraft, $transferTarget, $storage, $epsSystem, $information)) {
             return false;
         }
 
         $commodities = request::postArray('commodities');
         $gcount = request::postArray('count');
 
-        $storage = $isUnload ? $ship->getBeamableStorage() : $target->get()->getBeamableStorage();
-
-        if ($storage->isEmpty()) {
-            $information->addInformation("Keine Waren zum Beamen vorhanden");
-            return false;
-        }
         if (count($commodities) == 0 || count($gcount) == 0) {
             $information->addInformation("Es wurden keine Waren zum Beamen ausgewählt");
             return false;
         }
+
         $information->addInformationf(
             'Die %s hat folgende Waren %s %s %s transferiert',
-            $ship->getName(),
+            $spacecraft->getName(),
             $isUnload ? 'zur' : 'von der',
             $target->get()->getTransferEntityType()->getName(),
             $target->getName()
@@ -125,7 +111,7 @@ class SpacecraftStorageCommodityLogic
                 $commodityId,
                 $gcount[$key],
                 $wrapper,
-                $isUnload ? $ship : $target->get(),
+                $isUnload ? $spacecraft : $target->get(),
                 $transferTarget,
                 $information
             )) {
@@ -134,5 +120,42 @@ class SpacecraftStorageCommodityLogic
         }
 
         return $hasTransfered;
+    }
+
+    /** @param Collection<int, StorageInterface> $storage */
+    private function isSanityFaulty(
+        SpacecraftInterface $spacecraft,
+        EntityWithStorageInterface $transferTarget,
+        Collection $storage,
+        ?EpsSystemData $epsSystem,
+        InformationInterface $information
+    ): bool {
+
+        $isDockTransfer = $this->commodityTransfer->isDockTransfer($spacecraft, $transferTarget);
+
+        if (!$isDockTransfer && ($epsSystem === null || $epsSystem->getEps() === 0)) {
+            $information->addInformation("Keine Energie vorhanden");
+            return true;
+        }
+        if ($spacecraft->isCloaked()) {
+            $information->addInformation("Die Tarnung ist aktiviert");
+            return true;
+        }
+        if ($spacecraft->isWarped()) {
+            $information->addInformation("Schiff befindet sich im Warp");
+            return true;
+        }
+
+        if ($transferTarget->getMaxStorage() <= $transferTarget->getStorageSum()) {
+            $information->addInformationf('%s: Der Lagerraum ist voll', $transferTarget->getName());
+            return true;
+        }
+
+        if ($storage->isEmpty()) {
+            $information->addInformation("Keine Waren zum Beamen vorhanden");
+            return true;
+        }
+
+        return false;
     }
 }
