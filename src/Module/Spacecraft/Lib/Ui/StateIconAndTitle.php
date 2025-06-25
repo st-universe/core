d7b2462d695c0a5864cbd3ffcc0b00597453a8eb
g5bot <jadari@web.de>
2025-06-25 13:45:48 +0200

    code optimization

--- src/Module/Spacecraft/Lib/Ui/StateIconAndTitle.php
+++ src/Module/Spacecraft/Lib/Ui/StateIconAndTitle.php
@@ -12,6 +12,7 @@ use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Ship\Lib\ShipWrapperInterface;
 use Stu\Module\Spacecraft\Lib\SpacecraftWrapperInterface;
 use Stu\Orm\Entity\ShipInterface;
+use Stu\Orm\Entity\SpacecraftInterface;
 
 class StateIconAndTitle
 {
@@ -28,79 +29,103 @@ class StateIconAndTitle
         $spacecraft = $wrapper->get();
         $state = $spacecraft->getState();
 
-        if ($state === SpacecraftStateEnum::RETROFIT) {
-            return ['buttons/konstr1', 'Schiff wird umgerüstet'];
-        }
+        return match ($state) {
+            SpacecraftStateEnum::RETROFIT => ['buttons/konstr1', 'Schiff wird umgerüstet'],
+            SpacecraftStateEnum::REPAIR_ACTIVE => $this->getForActiveRepair($spacecraft),
+            SpacecraftStateEnum::REPAIR_PASSIVE => $this->getForActivePassive($wrapper),
+            SpacecraftStateEnum::ASTRO_FINALIZING => $this->getForAstroFinalizing($wrapper),
+            SpacecraftStateEnum::ACTIVE_TAKEOVER => $this->getForActiveTakeover($wrapper),
+            SpacecraftStateEnum::GATHER_RESOURCES => $this->getForGatherResources($spacecraft),
+            default => $this->getForPassiveTakeover($wrapper)
+        };
+    }
 
-        if ($state === SpacecraftStateEnum::REPAIR_ACTIVE) {
-            $isStation = $spacecraft->isStation();
-            return ['buttons/rep2', sprintf(
-                '%s repariert %s',
-                $isStation ? 'Stationscrew' : 'Schiffscrew',
-                $isStation ? 'die Station' : 'das Schiff',
-            )];
-        }
+    /** @return array<string> */
+    private function getForActiveRepair(SpacecraftInterface $spacecraft): array
+    {
+        $isStation = $spacecraft->isStation();
+        return ['buttons/rep2', sprintf(
+            '%s repariert %s',
+            $isStation ? 'Stationscrew' : 'Schiffscrew',
+            $isStation ? 'die Station' : 'das Schiff',
+        )];
+    }
 
-        if ($state === SpacecraftStateEnum::REPAIR_PASSIVE) {
-            $isStation = $spacecraft->isStation();
-            $repairDuration = $wrapper->getRepairDuration();
-            return ['buttons/rep2', sprintf('%s wird repariert (noch %d Runden)', $isStation ? 'Station' : 'Schiff', $repairDuration)];
-        }
+    /** @return array<string> */
+    private function getForActivePassive(SpacecraftWrapperInterface $wrapper): array
+    {
+        return ['buttons/rep2', sprintf(
+            '%s wird repariert (noch %d Runden)',
+            $wrapper->get()->isStation() ? 'Station' : 'Schiff',
+            $wrapper->getRepairDuration()
+        )];
+    }
 
+    /** @return array<string>|null */
+    private function getForAstroFinalizing(SpacecraftWrapperInterface $wrapper): ?array
+    {
         $astroLab = $wrapper instanceof ShipWrapperInterface ? $wrapper->getAstroLaboratorySystemData() : null;
-        if (
-            $state === SpacecraftStateEnum::ASTRO_FINALIZING
-            && $astroLab !== null
-        ) {
-            return ['buttons/map1', sprintf(
-                'Schiff kartographiert (noch %d Runden)',
-                $astroLab->getAstroStartTurn() + AstronomicalMappingEnum::TURNS_TO_FINISH - $this->game->getCurrentRound()->getTurn()
-            )];
+        if ($astroLab === null) {
+            return null;
         }
 
-        $takeover = $spacecraft->getTakeoverActive();
-        if (
-            $state === SpacecraftStateEnum::ACTIVE_TAKEOVER
-            && $takeover !== null
-        ) {
-            $targetNamePlainText = $this->bbCodeParser->parse($takeover->getTargetSpacecraft()->getName())->getAsText();
-            return ['buttons/take2', sprintf(
-                'Schiff übernimmt die "%s" (noch %d Runden)',
-                $targetNamePlainText,
-                $wrapper->getTakeoverTicksLeft($takeover)
-            )];
+        return ['buttons/map1', sprintf(
+            'Schiff kartographiert (noch %d Runden)',
+            $astroLab->getAstroStartTurn() + AstronomicalMappingEnum::TURNS_TO_FINISH - $this->game->getCurrentRound()->getTurn()
+        )];
+    }
+
+    /** @return array<string>|null */
+    private function getForActiveTakeover(SpacecraftWrapperInterface $wrapper): ?array
+    {
+        $takeover = $wrapper->get()->getTakeoverActive();
+        if ($takeover === null) {
+            return null;
         }
 
-        $takeover = $spacecraft->getTakeoverPassive();
-        if ($takeover !== null) {
-            $sourceUserNamePlainText = $this->bbCodeParser->parse($takeover->getSourceSpacecraft()->getUser()->getName())->getAsText();
-            return ['buttons/untake2', sprintf(
-                'Schiff wird von Spieler "%s" übernommen (noch %d Runden)',
-                $sourceUserNamePlainText,
-                $wrapper->getTakeoverTicksLeft($takeover)
-            )];
+        return ['buttons/take2', sprintf(
+            'Schiff übernimmt die "%s" (noch %d Runden)',
+            $this->bbCodeParser->parse($takeover->getTargetSpacecraft()->getName())->getAsText(),
+            $wrapper->getTakeoverTicksLeft($takeover)
+        )];
+    }
+
+    /** @return array<string>|null */
+    private function getForGatherResources(SpacecraftInterface $spacecraft): ?array
+    {
+        if (!$spacecraft instanceof ShipInterface) {
+            return null;
         }
 
-        if (
-            $state === SpacecraftStateEnum::GATHER_RESOURCES
-            && $spacecraft instanceof ShipInterface
-        ) {
-            $miningqueue = $spacecraft->getMiningQueue();
-            $module = $spacecraft->getSpacecraftSystem(SpacecraftSystemTypeEnum::BUSSARD_COLLECTOR)->getModule();
-
-            if ($miningqueue !== null) {
-                $locationmining = $miningqueue->getLocationMining();
-                if ($module !== null) {
-                    $gathercount = $module->getFactionId() == null ? 100 : 200;
-                    return [sprintf('commodities/%s', $locationmining->getCommodity()->getId()), sprintf(
-                        'Schiff sammelt Ressourcen (~%d %s/Tick)',
-                        $gathercount,
-                        $locationmining->getCommodity()->getName()
-                    )];
-                }
-            }
+        $miningqueue = $spacecraft->getMiningQueue();
+        $module = $spacecraft->getSpacecraftSystem(SpacecraftSystemTypeEnum::BUSSARD_COLLECTOR)->getModule();
+        if ($miningqueue === null || $module === null) {
+            return null;
         }
 
-        return null;
+        $locationmining = $miningqueue->getLocationMining();
+        $gathercount = $module->getFactionId() == null ? 100 : 200;
+
+        return [sprintf('commodities/%s', $locationmining->getCommodity()->getId()), sprintf(
+            'Schiff sammelt Ressourcen (~%d %s/Tick)',
+            $gathercount,
+            $locationmining->getCommodity()->getName()
+        )];
+    }
+
+    /** @return array<string>|null */
+    private function getForPassiveTakeover(SpacecraftWrapperInterface $wrapper): ?array
+    {
+        $takeover = $wrapper->get()->getTakeoverPassive();
+        if ($takeover === null) {
+            return null;
+        }
+
+        $sourceUserNamePlainText = $this->bbCodeParser->parse($takeover->getSourceSpacecraft()->getUser()->getName())->getAsText();
+        return ['buttons/untake2', sprintf(
+            'Schiff wird von Spieler "%s" übernommen (noch %d Runden)',
+            $sourceUserNamePlainText,
+            $wrapper->getTakeoverTicksLeft($takeover)
+        )];
     }
 }
