6df63c4183deb782a965f640a506749b1738e1a3
g5bot <jadari@web.de>
2025-06-29 09:32:44 +0200

    auto delete sqlite init-migration and db

--- generate-migrations.sh
+++ generate-migrations.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+rm -rf src/Migrations/Sqlite/*
+rm -rf inttest.sqlite
+
 php vendor/bin/doctrine-migrations diff --stage=dev --configuration="config/migrations/development.php" --namespace="Stu\Migrations\Pgsql" --allow-empty-diff --no-interaction
 php vendor/bin/doctrine-migrations diff --stage=inttest --configuration="config/migrations/development.php" --namespace="Stu\Migrations\Sqlite" --allow-empty-diff --no-interaction --from-empty-schema
