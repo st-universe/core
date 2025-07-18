c982c24988add951971d1a7820fc20e897ba83bf
g5bot <jadari@web.de>
2025-07-18 20:56:52 +0200

    reload system settings popup after action

--- src/Module/Spacecraft/View/ShowSystemSettings/SubspaceSensorSystemSettings.php
+++ src/Module/Spacecraft/View/ShowSystemSettings/SubspaceSensorSystemSettings.php
@@ -5,13 +5,16 @@ declare(strict_types=1);
 namespace Stu\Module\Spacecraft\View\ShowSystemSettings;
 
 use Stu\Component\Spacecraft\System\SpacecraftSystemTypeEnum;
+use Stu\Component\Spacecraft\System\SpacecraftSystemWrapperFactoryInterface;
 use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Spacecraft\Lib\SpacecraftWrapperInterface;
 
 
 class SubspaceSensorSystemSettings implements SystemSettingsProviderInterface
 {
-    public function __construct() {}
+    public function __construct(
+        private readonly SpacecraftSystemWrapperFactoryInterface $spacecraftSystemWrapperFactory
+    ) {}
 
     public function setTemplateVariables(
         SpacecraftSystemTypeEnum $systemType,
@@ -19,6 +22,11 @@ class SubspaceSensorSystemSettings implements SystemSettingsProviderInterface
         GameControllerInterface $game
     ): void {
 
-        $game->setMacroInAjaxWindow('html/ship/subspacescanner.twig');
+        $game->setMacroInAjaxWindow('html/spacecraft/system/subspaceScanner.twig');
+
+        $game->setTemplateVar(
+            'systemWrapper',
+            $this->spacecraftSystemWrapperFactory->create($wrapper->get(), $systemType)
+        );
     }
 }
