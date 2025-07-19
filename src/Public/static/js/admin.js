ae4b6c1a292e3f6276ad272b4ac1b426aa7db839
g5bot <jadari@web.de>
2025-07-19 21:46:44 +0200

    tidy up js files

--- src/Public/static/js/admin.js
+++ src/Public/static/js/admin.js
@@ -3,20 +3,3 @@ function showUserLock(userid) {
 	openPJsWin('elt', 1);
 	ajax_update('elt', '/admin/?SHOW_USER_LOCK=1&id=' + userid);
 }
-
-function registerSystemEditorNavKeys(previousId, currentId, nextId) {
-	document.addEventListener("keydown", (event) => {
-		if (event.key === "ArrowLeft" && previousId > 0) {
-			document.location.href = '?SHOW_SYSTEM=1&systemid=' + previousId;
-		}
-		if (event.key === "ArrowRight" && nextId > 0) {
-			document.location.href = '?SHOW_SYSTEM=1&systemid=' + nextId;
-		}
-		if (event.key === "ArrowUp") {
-			document.location.href = '?REGENERATE_SYSTEM=1&systemid=' + currentId;
-		}
-		if (event.key === "ArrowDown") {
-			document.location.href = '?SHOW_SYSTEM=1&systemid=' + currentId;
-		}
-	});
-}
