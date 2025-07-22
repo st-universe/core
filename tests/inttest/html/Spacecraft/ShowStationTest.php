377d723021e7ef700cf98710976af8deaace8317
g5bot <jadari@web.de>
2025-07-22 12:20:46 +0200

    stub components

--- tests/inttest/html/Spacecraft/ShowStationTest.php
+++ tests/inttest/html/Spacecraft/ShowStationTest.php
@@ -5,12 +5,24 @@ declare(strict_types=1);
 namespace Stu\Html\Spacecraft;
 
 use Mockery;
+use Stu\Module\Game\Component\GameComponentEnum;
 use Stu\Module\Spacecraft\View\ShowSpacecraft\ShowSpacecraft;
 use Stu\Orm\Repository\AnomalyRepositoryInterface;
+use Stu\StuMocks;
 use Stu\TwigTestCase;
 
 class ShowStationTest extends TwigTestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        StuMocks::get()->registerStubbedComponent(GameComponentEnum::COLONIES)
+            ->registerStubbedComponent(GameComponentEnum::NAVIGATION)
+            ->registerStubbedComponent(GameComponentEnum::PM)
+            ->registerStubbedComponent(GameComponentEnum::RESEARCH)
+            ->registerStubbedComponent(GameComponentEnum::SERVERTIME_AND_VERSION)
+            ->registerStubbedComponent(GameComponentEnum::USER);
+    }
+
     public function testHandle(): void
     {
         $anomalyRepositoryMock = $this->mock(AnomalyRepositoryInterface::class);
