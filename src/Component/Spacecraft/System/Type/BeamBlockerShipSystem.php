6b6ffa6d387f6748ffde6a993338aa941b6c05db
Huxinator <nicotintel@gmail.com>
2025-08-15 18:29:20 +0200

    test

--- src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
+++ src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
@@ -28,6 +28,7 @@ final class BeamBlockerShipSystem extends AbstractSpacecraftSystemType implement
     #[Override]
     public function getDefaultMode(): SpacecraftSystemModeEnum
     {
+        $testvalue = 0;
         return SpacecraftSystemModeEnum::MODE_ALWAYS_ON;
     }
 
