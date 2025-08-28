467f7ecb0c8c97563eefd59896b86c18da799aa5
g5bot <jadari@web.de>
2025-08-28 21:54:59 +0200

    fix test

--- tests/inttest/Tick/SpacecraftTickTest.php
+++ tests/inttest/Tick/SpacecraftTickTest.php
@@ -4,14 +4,22 @@ declare(strict_types=1);
 
 namespace Stu\Tick;
 
+use Mockery;
 use Stu\ActionTestCase;
 use Stu\Config\Init;
 use Stu\Module\Tick\Spacecraft\SpacecraftTickManagerInterface;
+use Stu\Orm\Repository\ColonyShipRepairRepositoryInterface;
 
 class SpacecraftTickTest extends ActionTestCase
 {
     public function testExecution(): void
     {
+        $colonyshipRepairRepoMock = $this->mock(ColonyShipRepairRepositoryInterface::class);
+        $colonyshipRepairRepoMock->shouldReceive('getMostRecentJobs')
+            ->with(Mockery::any())
+            ->andReturn([]);
+        $this->getContainer()->setAdditionalService(ColonyShipRepairRepositoryInterface::class, $colonyshipRepairRepoMock);
+
         Init::getContainer()
             ->get(SpacecraftTickManagerInterface::class)
             ->work(false);
