5605687aef4390ad9d566d54f0df43e11be716ff
Huxinator <nicotintel@gmail.com>
2025-08-28 18:01:23 +0200

    Revert "test"
    This reverts commit 921bf46c3447eb81d9fe88ccf36b472fc13bfeb4.

--- src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
+++ src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
@@ -30,7 +30,6 @@ final class BeamBlockerShipSystem extends AbstractSpacecraftSystemType implement
     {
         return SpacecraftSystemModeEnum::MODE_ALWAYS_ON;
     }
-    //tesct
 
     #[Override]
     public function getEnergyUsageForActivation(): int
