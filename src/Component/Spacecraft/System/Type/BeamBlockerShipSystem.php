ad30d57a394a58f221286a62dd6cbd307bbf172f
Huxinator <nicotintel@gmail.com>
2025-08-28 17:04:32 +0200

    test

--- src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
+++ src/Component/Spacecraft/System/Type/BeamBlockerShipSystem.php
@@ -31,6 +31,8 @@ final class BeamBlockerShipSystem extends AbstractSpacecraftSystemType implement
         return SpacecraftSystemModeEnum::MODE_ALWAYS_ON;
     }
 
+    //test
+
     #[Override]
     public function getEnergyUsageForActivation(): int
     {
