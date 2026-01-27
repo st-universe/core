95b21f61d2bfe2d3f99655b54df2dac880868190
Huxinator <nicotintel@gmail.com>
2026-01-27 12:07:59 +0100

    fix ticket 948

--- src/Module/Communication/View/ShowQuestList/ShowQuestList.php
+++ src/Module/Communication/View/ShowQuestList/ShowQuestList.php
@@ -8,6 +8,11 @@ use Stu\Module\Control\GameControllerInterface;
 use Stu\Module\Control\ViewControllerInterface;
 use Stu\Orm\Repository\NPCQuestRepositoryInterface;
 use Stu\Orm\Repository\NPCQuestUserRepositoryInterface;
+use Stu\Orm\Repository\SpacecraftBuildplanRepositoryInterface;
+use Stu\Orm\Repository\CommodityRepositoryInterface;
+use Stu\Orm\Entity\NPCQuest;
+use Stu\Orm\Entity\SpacecraftBuildplan;
+use Stu\Orm\Entity\Commodity;
 use Stu\Component\Quest\QuestUserModeEnum;
 
 final class ShowQuestList implements ViewControllerInterface
@@ -16,7 +21,9 @@ final class ShowQuestList implements ViewControllerInterface
 
     public function __construct(
         private NPCQuestRepositoryInterface $npcQuestRepository,
-        private NPCQuestUserRepositoryInterface $npcQuestUserRepository
+        private NPCQuestUserRepositoryInterface $npcQuestUserRepository,
+        private SpacecraftBuildplanRepositoryInterface $spacecraftBuildplanRepository,
+        private CommodityRepositoryInterface $commodityRepository
     ) {}
 
     #[\Override]
@@ -39,19 +46,19 @@ final class ShowQuestList implements ViewControllerInterface
 
         foreach ($allQuests as $quest) {
             $isParticipant = in_array($quest->getId(), $userQuestIds);
-            
+
             $canSeeFactions = $quest->getFactions();
             $secretFactions = $quest->getSecret();
-            
+
             $canSeeQuest = false;
             if ($canSeeFactions === null || in_array($userFactionId, $canSeeFactions)) {
                 $canSeeQuest = true;
             }
-            
+
             if ($secretFactions !== null && !in_array($userFactionId, $secretFactions)) {
                 $canSeeQuest = false;
             }
-            
+
             if ($isParticipant) {
                 $canSeeQuest = true;
             }
@@ -79,5 +86,53 @@ final class ShowQuestList implements ViewControllerInterface
         $game->setTemplateVar('PARTICIPATED_QUESTS', $participatedQuests);
         $game->setTemplateVar('OTHER_ENDED_QUESTS', $otherEndedQuests);
         $game->setTemplateVar('USER_QUEST_IDS', $userQuestIds);
+        $game->setTemplateVar('BUILDPLANS', $this->loadAllBuildplans($allQuests));
+        $game->setTemplateVar('COMMODITIES', $this->loadAllCommodities($allQuests));
+    }
+
+    /**
+     * @param array<int, NPCQuest> $quests
+     * @return array<int, SpacecraftBuildplan>
+     */
+    private function loadAllBuildplans(array $quests): array
+    {
+        $buildplans = [];
+        foreach ($quests as $quest) {
+            $spacecrafts = $quest->getSpacecrafts();
+            if ($spacecrafts) {
+                foreach ($spacecrafts as $buildplanId => $amount) {
+                    if (!isset($buildplans[$buildplanId])) {
+                        $buildplan = $this->spacecraftBuildplanRepository->find($buildplanId);
+                        if ($buildplan !== null) {
+                            $buildplans[$buildplanId] = $buildplan;
+                        }
+                    }
+                }
+            }
+        }
+        return $buildplans;
+    }
+
+    /**
+     * @param array<int, NPCQuest> $quests
+     * @return array<int, Commodity>
+     */
+    private function loadAllCommodities(array $quests): array
+    {
+        $commodities = [];
+        foreach ($quests as $quest) {
+            $commodityReward = $quest->getCommodityReward();
+            if ($commodityReward) {
+                foreach ($commodityReward as $commodityId => $amount) {
+                    if (!isset($commodities[$commodityId])) {
+                        $commodity = $this->commodityRepository->find($commodityId);
+                        if ($commodity !== null) {
+                            $commodities[$commodityId] = $commodity;
+                        }
+                    }
+                }
+            }
+        }
+        return $commodities;
     }
 }
