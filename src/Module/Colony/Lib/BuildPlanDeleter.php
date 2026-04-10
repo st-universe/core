93a0b4fa381ef75962561d85a101f98824e04f9a
Huxinator <nicotintel@gmail.com>
2026-04-10 12:52:12 +0200

    fix ticket 1030

--- src/Module/Colony/Lib/BuildPlanDeleter.php
+++ src/Module/Colony/Lib/BuildPlanDeleter.php
@@ -7,6 +7,7 @@ namespace Stu\Module\Colony\Lib;
 use Stu\Orm\Entity\SpacecraftBuildplan;
 use Stu\Orm\Repository\BuildplanModuleRepositoryInterface;
 use Stu\Orm\Repository\ColonyShipQueueRepositoryInterface;
+use Stu\Orm\Repository\ShipyardShipQueueRepositoryInterface;
 use Stu\Orm\Repository\SpacecraftBuildplanRepositoryInterface;
 
 /**
@@ -14,7 +15,12 @@ use Stu\Orm\Repository\SpacecraftBuildplanRepositoryInterface;
  */
 final class BuildPlanDeleter implements BuildPlanDeleterInterface
 {
-    public function __construct(private SpacecraftBuildplanRepositoryInterface $spacecraftBuildplanRepository, private BuildplanModuleRepositoryInterface $buildplanModuleRepository, private ColonyShipQueueRepositoryInterface $colonyShipQueueRepository) {}
+    public function __construct(
+        private SpacecraftBuildplanRepositoryInterface $spacecraftBuildplanRepository,
+        private BuildplanModuleRepositoryInterface $buildplanModuleRepository,
+        private ColonyShipQueueRepositoryInterface $colonyShipQueueRepository,
+        private ShipyardShipQueueRepositoryInterface $shipyardShipQueueRepository
+    ) {}
 
     #[\Override]
     public function delete(SpacecraftBuildplan $spacecraftBuildplan): void
@@ -27,8 +33,17 @@ final class BuildPlanDeleter implements BuildPlanDeleterInterface
     public function isDeletable(
         SpacecraftBuildplan $spacecraftBuildplan
     ): bool {
-        $queuedShipsCount = $this->colonyShipQueueRepository->getCountByBuildplan($spacecraftBuildplan->getId());
+        if ($spacecraftBuildplan->getSpacecraftCount() > 0) {
+            return false;
+        }
 
-        return $spacecraftBuildplan->getSpacecraftCount() === 0 && $queuedShipsCount === 0;
+        $buildplanId = $spacecraftBuildplan->getId();
+        $queuedShipsCount = $this->colonyShipQueueRepository->getCountByBuildplan($buildplanId);
+
+        if ($queuedShipsCount > 0) {
+            return false;
+        }
+
+        return $this->shipyardShipQueueRepository->getCountByBuildplan($buildplanId) === 0;
     }
 }
