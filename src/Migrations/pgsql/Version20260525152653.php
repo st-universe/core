ab940e46b7b07153d58922d83f89dc5689adddc0
Nico Tintel <nicotintel@gmail.com>
2026-05-25 18:30:35 +0200

    fix ticket 916

---
+++ src/Migrations/pgsql/Version20260525152653.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Migrations\Pgsql;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260525152653 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stu_buildplan ADD npc_gift BOOLEAN DEFAULT false');
+        $this->addSql('ALTER TABLE stu_buildplan ADD count INT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stu_buildplan DROP npc_gift');
+        $this->addSql('ALTER TABLE stu_buildplan DROP count');
+    }
+}
