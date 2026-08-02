02334334349b64e7a3e91aa6726ede09dcb0cc78
Nico Tintel <nicotintel@gmail.com>
2026-08-02 17:04:19 +0200

    testfix

--- tests/inttest/Config/InitTest.php
+++ tests/inttest/Config/InitTest.php
@@ -4,27 +4,47 @@ declare(strict_types=1);
 
 namespace Stu\Config;
 
-use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use ReflectionClass;
 use Stu\StuTestCase;
 
-/**
- * Avoid global settings to cause trouble within other tests
- */
-#[RunTestsInSeparateProcesses]
 class InitTest extends StuTestCase
 {
     public function testInitCallsGivenCallable(): void
     {
-        error_reporting(0);
-
         $output = 'some-output';
+        $initContainer = $this->getStaticProperty(Init::class, 'CONTAINER');
+        $configFiles = $this->getStaticProperty(ConfigFileSetup::class, 'configFiles');
+        $errorReporting = error_reporting();
+        $includePath = get_include_path();
+        $timezone = date_default_timezone_get();
 
-        static::expectOutputString($output);
+        try {
+            $this->setStaticProperty(Init::class, 'CONTAINER', null);
+            error_reporting(0);
 
-        $app = function () use ($output): void {
-            echo $output;
-        };
+            static::expectOutputString($output);
 
-        Init::run($app, false);
+            Init::run(static function () use ($output): void {
+                echo $output;
+            }, false);
+        } finally {
+            $this->setStaticProperty(Init::class, 'CONTAINER', $initContainer);
+            $this->setStaticProperty(ConfigFileSetup::class, 'configFiles', $configFiles);
+            error_reporting($errorReporting);
+            set_include_path($includePath);
+            date_default_timezone_set($timezone);
+        }
+    }
+
+    /** @param class-string $className */
+    private function getStaticProperty(string $className, string $propertyName): mixed
+    {
+        return (new ReflectionClass($className))->getProperty($propertyName)->getValue();
+    }
+
+    /** @param class-string $className */
+    private function setStaticProperty(string $className, string $propertyName, mixed $value): void
+    {
+        (new ReflectionClass($className))->getProperty($propertyName)->setValue(null, $value);
     }
 }
