22b953ab4e7d7a927bbbdf82d2f7a4aa256b90b8
Nico Tintel <nicotintel@gmail.com>
2026-09-01 21:33:33 +0200

    try fix

--- src/Module/Tick/Spacecraft/SpacecraftTickManager.php
+++ src/Module/Tick/Spacecraft/SpacecraftTickManager.php
@@ -5,18 +5,22 @@ declare(strict_types=1);
 namespace Stu\Module\Tick\Spacecraft;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Stu\Component\Game\SemaphoreConstants;
 use Stu\Module\Config\StuConfigInterface;
+use Stu\Module\Control\SemaphoreUtilInterface;
 use Stu\Module\Logging\LogTypeEnum;
 use Stu\Module\Logging\StuLogger;
 use Stu\Module\Tick\Lock\LockManagerInterface;
 use Stu\Module\Tick\Lock\LockTypeEnum;
 use Stu\Module\Tick\Spacecraft\ManagerComponent\ManagerComponentInterface;
 use Stu\Orm\Repository\UserRepositoryInterface;
+use Throwable;
 
 class SpacecraftTickManager implements SpacecraftTickManagerInterface
 {
     /** @param array<ManagerComponentInterface> $components */
     public function __construct(
+        private SemaphoreUtilInterface $semaphoreUtil,
         private UserRepositoryInterface $userRepository,
         private LockManagerInterface $lockManager,
         private StuConfigInterface $config,
@@ -27,9 +31,14 @@ class SpacecraftTickManager implements SpacecraftTickManagerInterface
     #[\Override]
     public function work(bool $doCommit = false): void
     {
-        $this->setLock(1);
+        $mainSema = $this->semaphoreUtil->acquireSemaphore(SemaphoreConstants::MAIN_SHIP_SEMAPHORE_KEY);
+        $lockSet = false;
 
         try {
+            $this->setLock(1);
+            $lockSet = true;
+            $this->lockAllUsersForUpdate();
+
             foreach ($this->components as $component) {
                 $startTime = microtime(true);
                 $component->work();
@@ -42,14 +51,24 @@ class SpacecraftTickManager implements SpacecraftTickManagerInterface
                 $this->entityManager->flush();
                 $this->entityManager->commit();
             }
+        } catch (Throwable $e) {
+            $this->semaphoreUtil->releaseSemaphore($mainSema);
+
+            throw $e;
         } finally {
-            $this->clearLock(1);
+            if ($lockSet) {
+                $this->clearLock(1);
+            }
         }
     }
 
     private function setLock(int $batchGroupId): void
     {
         $this->lockManager->setLock($batchGroupId, LockTypeEnum::SHIP_GROUP);
+    }
+
+    private function lockAllUsersForUpdate(): void
+    {
         if (!$this->config->getDbSettings()->useSqlite()) {
             $this->userRepository->lockAllUsersForUpdate();
         }
--- tests/unit/Module/Tick/Spacecraft/SpacecraftTickManagerTest.php
+++ tests/unit/Module/Tick/Spacecraft/SpacecraftTickManagerTest.php
@@ -6,8 +6,11 @@ namespace Stu\Module\Tick\Spacecraft;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Mockery\MockInterface;
+use RuntimeException;
+use Stu\Component\Game\SemaphoreConstants;
 use Stu\Module\Config\Model\DbSettingsInterface;
 use Stu\Module\Config\StuConfigInterface;
+use Stu\Module\Control\SemaphoreUtilInterface;
 use Stu\Module\Tick\Lock\LockManagerInterface;
 use Stu\Module\Tick\Lock\LockTypeEnum;
 use Stu\Orm\Repository\UserRepositoryInterface;
@@ -15,6 +18,7 @@ use Stu\StuTestCase;
 
 class SpacecraftTickManagerTest extends StuTestCase
 {
+    private SemaphoreUtilInterface&MockInterface $semaphoreUtil;
     private UserRepositoryInterface&MockInterface $userRepository;
     private LockManagerInterface&MockInterface $lockManager;
     private StuConfigInterface&MockInterface $config;
@@ -23,11 +27,13 @@ class SpacecraftTickManagerTest extends StuTestCase
     #[\Override]
     protected function setUp(): void
     {
+        $this->semaphoreUtil = $this->mock(SemaphoreUtilInterface::class);
         $this->userRepository = $this->mock(UserRepositoryInterface::class);
         $this->lockManager = $this->mock(LockManagerInterface::class);
         $this->config = $this->mock(StuConfigInterface::class);
 
         $this->subject = new SpacecraftTickManager(
+            $this->semaphoreUtil,
             $this->userRepository,
             $this->lockManager,
             $this->config,
@@ -36,10 +42,13 @@ class SpacecraftTickManagerTest extends StuTestCase
         );
     }
 
-    public function testWorkLocksAllUsersOnNonSqliteDatabase(): void
+    public function testWorkAcquiresMainSemaphoreAndLocksAllUsersOnNonSqliteDatabase(): void
     {
         $dbSettings = $this->mock(DbSettingsInterface::class);
 
+        $this->semaphoreUtil->shouldReceive('acquireSemaphore')
+            ->with(SemaphoreConstants::MAIN_SHIP_SEMAPHORE_KEY)
+            ->once();
         $this->lockManager->shouldReceive('setLock')
             ->with(1, LockTypeEnum::SHIP_GROUP)
             ->once();
@@ -57,6 +66,43 @@ class SpacecraftTickManagerTest extends StuTestCase
         $this->userRepository->shouldReceive('lockAllUsersForUpdate')
             ->withNoArgs()
             ->once();
+
+        $this->subject->work();
+    }
+
+    public function testWorkReleasesSemaphoreAndClearsLockWhenUserLockingFails(): void
+    {
+        $semaphore = 42;
+        $dbSettings = $this->mock(DbSettingsInterface::class);
+
+        $this->semaphoreUtil->shouldReceive('acquireSemaphore')
+            ->with(SemaphoreConstants::MAIN_SHIP_SEMAPHORE_KEY)
+            ->once()
+            ->andReturn($semaphore);
+        $this->semaphoreUtil->shouldReceive('releaseSemaphore')
+            ->with($semaphore)
+            ->once();
+        $this->lockManager->shouldReceive('setLock')
+            ->with(1, LockTypeEnum::SHIP_GROUP)
+            ->once();
+        $this->lockManager->shouldReceive('clearLock')
+            ->with(1, LockTypeEnum::SHIP_GROUP)
+            ->once();
+        $this->config->shouldReceive('getDbSettings')
+            ->withNoArgs()
+            ->once()
+            ->andReturn($dbSettings);
+        $dbSettings->shouldReceive('useSqlite')
+            ->withNoArgs()
+            ->once()
+            ->andReturn(false);
+        $this->userRepository->shouldReceive('lockAllUsersForUpdate')
+            ->withNoArgs()
+            ->once()
+            ->andThrow(new RuntimeException('database lock failed'));
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('database lock failed');
 
         $this->subject->work();
     }
