9f7e4ec1e896c7464df95ef55652ab0f71857fea
g5bot <jadari@web.de>
2026-09-19 21:46:08 +0200

    feat: refactor session handling to GameRunner for improved modularity

---
+++ src/Config/GameRunner.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Stu\Config;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerInterface;
+use Stu\Component\Game\ModuleEnum;
+use Stu\Module\Control\GameControllerInterface;
+
+final class GameRunner
+{
+
+    public static function runModule(ModuleEnum $module): void
+    {
+        @session_start();
+
+        Init::run(function (ContainerInterface $dic) use ($module): void {
+            $dic->get(EntityManagerInterface::class)
+                ->wrapInTransaction(
+                    fn() => $dic->get(GameControllerInterface::class)->main(
+                        $module
+                    )
+                );
+        });
+    }
+}
--- src/Public/alliance.php
+++ src/Public/alliance.php
@@ -2,26 +2,7 @@
 
 declare(strict_types=1);
 
-use Doctrine\ORM\EntityManagerInterface;
-use Psr\Container\ContainerInterface;
 use Stu\Component\Game\ModuleEnum;
-use Stu\Config\Init;
-use Stu\Module\Control\GameControllerInterface;
+use Stu\Config\GameRunner;
 
-/**
- * @deprecated Session handling should be part of the application
- */
-@session_start();
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-
-Init::run(function (ContainerInterface $dic): void {
-    $em = $dic->get(EntityManagerInterface::class);
-    $em->beginTransaction();
-
-    $dic->get(GameControllerInterface::class)->main(
-        ModuleEnum::ALLIANCE
-    );
-
-    $em->commit();
-});
+GameRunner::runModule(ModuleEnum::ALLIANCE);
