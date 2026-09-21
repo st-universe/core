3bef589668980ac7e986eb551b12c6273a6bb3d2
Nico Tintel <nicotintel@gmail.com>
2026-09-21 06:15:21 +0200

    fix composer autoload for alliance.php

--- src/Public/alliance.php
+++ src/Public/alliance.php
@@ -5,4 +5,6 @@ declare(strict_types=1);
 use Stu\Component\Game\ModuleEnum;
 use Stu\Config\GameRunner;
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
 GameRunner::runModule(ModuleEnum::ALLIANCE);
