34a75229f6c4e8448c5f27c499cba45baec7daeb
Nico Tintel <nicotintel@gmail.com>
2026-09-26 09:41:50 +0200

    fix ticket 1120

--- src/Lib/Interaction/InteractionCheckType.php
+++ src/Lib/Interaction/InteractionCheckType.php
@@ -17,8 +17,9 @@ enum InteractionCheckType
     case EXPECT_SOURCE_TACHYON;
     case EXPECT_SOURCE_UNTRACTORED;
     case EXPECT_SOURCE_UNBLOCKED;
+    case EXPECT_SPACECRAFT_BEAMABLE;
 
-    // TARGET
+        // TARGET
     case EXPECT_TARGET_NOT_NPC;
     case EXPECT_TARGET_UNSHIELDED;
     case EXPECT_TARGET_UNCLOAKED;
@@ -38,14 +39,15 @@ enum InteractionCheckType
             self::EXPECT_SOURCE_UNWARPED => 'Der Warpantrieb ist aktiviert',
             self::EXPECT_SOURCE_ENABLED => sprintf('%s ist kampfunfähig', $placeholder),
             self::EXPECT_SOURCE_UNTRACTORED => 'Das Schiff wird von einem Traktorstrahl gehalten',
-            self::EXPECT_SOURCE_UNBLOCKED => 'Die Kolonie wird blockiert.',
+            self::EXPECT_SOURCE_UNBLOCKED => 'Die Kolonie wird blockiert',
+            self::EXPECT_SPACECRAFT_BEAMABLE => 'Der Beamblocker verhindert das Beamen',
             self::EXPECT_TARGET_NOT_NPC => 'Aktion nicht möglich, der Spieler ist NPC!',
             self::EXPECT_TARGET_UNSHIELDED => 'Das Ziel hat die Schilde aktiviert',
             self::EXPECT_TARGET_UNWARPED => 'Das Ziel hat den Warpantrieb aktiviert',
             self::EXPECT_TARGET_ALSO_IN_FINISHED_WEB => 'Das Ziel ist nicht mit im Energienetz gefangen',
             self::EXPECT_TARGET_ON_SAME_SIDE_OF_FINISHED_WEB => 'Das Ziel ist nicht auf derselben Seite des Energienetzes',
             self::EXPECT_TARGET_DOCKED_OR_NO_ION_STORM => 'Beamen nicht möglich während eines Ionensturms',
-            self::EXPECT_TARGET_UNBLOCKED => 'Das Ziel wird blockiert.',
+            self::EXPECT_TARGET_UNBLOCKED => 'Das Ziel wird blockiert',
             self::EXPECT_SOURCE_TACHYON => throw new SanityCheckException('Tried to interact with cloaked entity without active tachyon'),
             self::EXPECT_TARGET_SAME_USER => throw new SanityCheckException('Tried to interact with entity of other user'),
             self::EXPECT_TARGET_UNCLOAKED => throw new SanityCheckException('Tried to interact with cloaked entity')
--- src/Module/Game/Action/Transfer/Transfer.php
+++ src/Module/Game/Action/Transfer/Transfer.php
@@ -88,6 +88,7 @@ final class Transfer implements ActionControllerInterface
                 InteractionCheckType::EXPECT_SOURCE_UNWARPED,
                 InteractionCheckType::EXPECT_SOURCE_UNSHIELDED,
                 InteractionCheckType::EXPECT_SOURCE_UNBLOCKED,
+                InteractionCheckType::EXPECT_SPACECRAFT_BEAMABLE,
                 InteractionCheckType::EXPECT_TARGET_DOCKED_OR_NO_ION_STORM,
                 InteractionCheckType::EXPECT_TARGET_UNWARPED,
                 InteractionCheckType::EXPECT_TARGET_UNCLOAKED,
